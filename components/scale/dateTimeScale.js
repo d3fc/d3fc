c8cbcc7bf65bb7fb35c323fec2504cfe3afe0e41
Mark Jose <mjose@scottlogic.co.uk>
2014-12-11 08:59:03 +0000

    Changes as per Colins comments

--- components/scale/dateTimeScale.js
+++ components/scale/dateTimeScale.js
@@ -2,7 +2,7 @@
     'use strict';
 
     /**
-    * This component provides a scale primarily used on the X axis of financial charts which is based on a time scale. 
+    * This component provides a scale primarily used on the X axis of financial charts and implements a time scale. 
     * It allows for the removal of time periods where the market may be closed, namely weekends. 
     * This scale also contains an option to pixel align when calculating the screen pixel from the real value. 
     * This generally produces crisper graphics.
@@ -19,10 +19,10 @@
     * Constructs a new instance of the dateTime scale component.
     * 
     * @memberof fc.scale.dateTime
-    * @param {linear} used in the copy constructor to copy the base linear scale between the original and the copy.
-    * @param {baseDomain} used in the copy constructor to copy the base domain (Max and Min) between the original and the copy.
-    * @param {alignPixels} used in the copy constructor to copy the pixel alignment option between the original and the copy.
-    * @param {hideWeekends} used in the copy constructor to copy the hide weekends option between the original and the copy.
+    * @param {d3.scale.linear} linear used in the copy constructor to copy the base linear scale between the original and the copy.
+    * @param {array[2]} baseDomain used in the copy constructor to copy the base domain (Max and Min) between the original and the copy.
+    * @param {boolean} alignPixels used in the copy constructor to copy the pixel alignment option between the original and the copy.
+    * @param {boolean} hideWeekends used in the copy constructor to copy the hide weekends option between the original and the copy.
     */
     function dateTimeScale(linear, baseDomain, alignPixels, hideWeekends) {
 
@@ -37,7 +37,7 @@
         * Used to scale a value from domain space to pixel space. This function is used primarily to position elements on the X axis.
         * 
         * @memberof fc.scale.dateTime
-        * @param {x} the real world domain value to be scaled.
+        * @param {object} x the real world domain value to be scaled.
         * @returns the converted value in pixel space. This value is also pixel aligned if the relevant options are set.
         */
         function scale(x) {
@@ -57,7 +57,7 @@
         * Used to set or get the domain for this scale. The domain is the range of real world values denoted by this scale (Max. and Min.).
         * 
         * @memberof fc.scale.dateTime
-        * @param {domain} the real world domain value as an array of 2 decimal numbers, Min and Max respectively.
+        * @param {array[2]} domain the real world domain value as an array of 2 date objects, Min and Max respectively.
         * @returns the current domain is no arguments are passed.
         */
         scale.domain = function (domain) {
@@ -78,7 +78,7 @@
         * Used to scale a value from pixel space to domain space. This function is the inverse of the `scale` function.
         * 
         * @memberof fc.scale.dateTime
-        * @param {pixel} the pixel value to be scaled.
+        * @param {decimal} pixel the pixel value to be scaled.
         * @returns the converted value in real world space. In most cases this value will only be acurate to the precision of the pixel width of the scale.
         */
        scale.invert = function (pixel) {
@@ -86,7 +86,8 @@
         };
 
         /**
-        * Used to create a copy of the current scale.
+        * Used to create a copy of the current scale. When scales are added to D3 axes the scales are copied rather than a reference being stored. 
+        * This function facilities a deep copy.
         * 
         * @memberof fc.scale.dateTime
         * @returns the copy.
@@ -97,7 +98,7 @@
 
         /**
         * Used to get an array of tick mark locations which can be used to display labels and tick marks on the associated axis.
-        * Ticks will be aligned to the nearest date time boundary. Boundarie are listed below:
+        * Ticks will be aligned to the nearest date time boundary. Boundaries are listed below:
         * + Year
         * + 8 Month
         * + 4 Month
@@ -115,7 +116,7 @@
         * + Second
         * 
         * @memberof fc.scale.dateTime
-        * @param {n} the number of ticks to try and display within the scale domain. (This value is used as  a guide for a best fit approach)
+        * @param {integer} n the number of ticks to try and display within the scale domain. (This value is used as  a guide for a best fit approach)
         * @returns an array of values denoting real world positions within the scale. These can be converted to pixel locations using the `scale` function.
         */
         scale.ticks = function (n) {
@@ -133,85 +134,85 @@
 
                 // Determine sensible date division starting from the largest time period down
                 if (((offset.getFullYear() - 1970) * 12) + offset.getMonth() >= 8) {
-                    start = findYearStart(start);
+                    start = getYearStart(start);
                     stepFunction = function(d) {
                         d.setFullYear(d.getFullYear() + 1);
                         return d;
                     };
                 } else if (((offset.getFullYear() - 1970) * 12) + offset.getMonth() >= 4) { // 8 Months
-                    start = findMonthStart(start);
+                    start = getMonthStart(start);
                     stepFunction = function(d) {
                         d.setMonth(d.getMonth() + 8);
                         return d;
                     };
                 } else if (((offset.getFullYear() - 1970) * 12) + offset.getMonth() >= 2) { // 4 Months
-                    start = findMonthStart(start);
+                    start = getMonthStart(start);
                     stepFunction = function(d) {
                         d.setMonth(d.getMonth() + 4);
                         return d;
                     };
                 } else if (((offset.getFullYear() - 1970) * 12) + offset.getMonth() >= 1) { // 2 Months
-                    start = findMonthStart(start);
+                    start = getMonthStart(start);
                     stepFunction = function(d) {
                         d.setMonth(d.getMonth() + 2);
                         return d;
                     };
                 } else if (offsetMilli >= 604800000) { // Months
-                    start = findMonthStart(start);
+                    start = getMonthStart(start);
                     stepFunction = function(d) {
                         d.setMonth(d.getMonth() + 1);
                         return d;
                     };
                 } else if (offsetMilli >= 86400000) { // 7 Days
-                    start = findWeekStart(start);
+                    start = getWeekStart(start);
                     stepFunction = function(d) {
                         d.setDate(d.getDate() + 7);
                         return d;
                     };
                 } else if (offsetMilli >= 43200000) { // Days
-                    start = findDayStart(start);
+                    start = getDayStart(start);
                     stepFunction = function(d) {
                         d.setDate(d.getDate() + 1);
                         return d;
                     };
                 } else if (offsetMilli >= 28800000) { // 12 Hours
-                    start = findHourStart(start, 12);
+                    start = getHourStart(start, 12);
                     stepFunction = function(d) {
                         d.setHours(d.getHours() + 12);
                         return d;
                     };
                 } else if (offsetMilli >= 14400000) { // 8 Hours
-                    start = findHourStart(start, 8);
+                    start = getHourStart(start, 8);
                     stepFunction = function(d) {
                         d.setHours(d.getHours() + 8);
                         return d;
                     };
                 } else if (offsetMilli >= 7200000) { // 4 Hours
-                    start = findHourStart(start, 4);
+                    start = getHourStart(start, 4);
                     stepFunction = function(d) {
                         d.setHours(d.getHours() + 4);
                         return d;
                     };
                 } else if (offsetMilli >= 3600000) { // 2 Hours
-                    start = findHourStart(start, 4);
+                    start = getHourStart(start, 4);
                     stepFunction = function(d) {
                         d.setHours(d.getHours() + 2);
                         return d;
                     };
                 } else if (offsetMilli >= 900000) { // Hours
-                    start = findHourStart(start, 0);
+                    start = getHourStart(start, 0);
                     stepFunction = function(d) {
                         d.setHours(d.getHours() + 1);
                         return d;
                     };
                 } else if (offsetMilli >= 60000) { // 15 Minutes
-                    start = findMinuteStart(start, 15);
+                    start = getMinuteStart(start, 15);
                     stepFunction = function(d) {
                         d.setMinutes(d.getMinutes() + 15);
                         return d;
                     };
                 } else if (offsetMilli >= 1000) { // Minutes
-                    start = findMinuteStart(start, 0);
+                    start = getMinuteStart(start, 0);
                     stepFunction = function(d) {
                         d.setMinutes(d.getMinutes() + 1);
                         return d;
@@ -231,11 +232,11 @@
         };
 
         /**
-        * Used to get or set the callback function used to format the data label for the associated axis tick label.
+        * Used to set the callback function used to format the data label for the associated axis tick label.
         * 
         * @memberof fc.scale.dateTime
-        * @param {count} 
-        * @param {f} 
+        * @param {integer} count 
+        * @param {decimal} f 
         * @returns a function which returns the formatting function for the individual data item.
         */
         scale.tickFormat = function (count, f) {
@@ -248,7 +249,7 @@
         * Used to get or set the option to hide weekends. Not showing weekends is common practice on financial charts.
         * 
         * @memberof fc.scale.dateTime
-        * @param {value} if set to `true` weekends will not be shown. If no value argument is passed the current setting will be returned.
+        * @param {boolean} value if set to `true` weekends will not be shown. If no value argument is passed the current setting will be returned.
         */
         scale.hideWeekends = function (value) {
             if (!arguments.length) {
@@ -262,7 +263,7 @@
         * Used to get or set the option to align ticks to pixel columns. Pixel aligning yields crisper chart graphics.
         * 
         * @memberof fc.scale.dateTime
-        * @param {value} if set to `true` values will be pixel aligned. If no value argument is passed the current setting will be returned.
+        * @param {boolean} value if set to `true` values will be pixel aligned. If no value argument is passed the current setting will be returned.
         */
         scale.alignPixels = function (value) {
             if (!arguments.length) {
@@ -319,26 +320,26 @@
             return date;
         }
 
-        function findMinuteStart(d, offset) {
+        function getMinuteStart(d, offset) {
             d = offset > 0 ?
                 new Date(d.getFullYear(), d.getMonth(), d.getDate(), d.getHours(), offset, 0) :
                 new Date(d.getFullYear(), d.getMonth(), d.getDate(), d.getHours(), d.getMinutes(), 0);
             return d;
         }
 
-        function findHourStart(d, offset) {
+        function getHourStart(d, offset) {
             d = offset > 0 ?
                 new Date(d.getFullYear(), d.getMonth(), d.getDate(), offset, 0, 0) :
                 new Date(d.getFullYear(), d.getMonth(), d.getDate(), d.getHours(), 0, 0);
             return d;
         }
 
-        function findDayStart(d) {
+        function getDayStart(d) {
             d = new Date(d.getFullYear(), d.getMonth(), d.getDate(), 0, 0, 0);
             return d;
         }
 
-        function findWeekStart(d) {
+        function getWeekStart(d) {
             d = new Date(d.getFullYear(), d.getMonth(), d.getDate(), 0, 0, 0);
             while (d.getDay() > 0) {
                 d.setDate(d.getDate() - 1);
@@ -346,12 +347,12 @@
             return d;
         }
 
-        function findMonthStart(d) {
+        function getMonthStart(d) {
             d = new Date(d.getFullYear(), d.getMonth(), 1, 0, 0, 0);
             return d;
         }
 
-        function findYearStart(d) {
+        function getYearStart(d) {
             d = new Date(d.getFullYear(), 0, 1, 0, 0, 0);
             return d;
         }
