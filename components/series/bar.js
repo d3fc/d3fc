9d368f48a1238416855bfdcd8960adf98ccd2c28
Colin Eberhardt <ceberhardt@scottlogic.co.uk>
2014-12-11 11:12:05 +0000

    Volume series is now a more generic bar series.css
     + Assimilated much of our CSS wrt. up / down days

---
+++ components/series/bar.js
@@ -0,0 +1,90 @@
+(function(d3, fc) {
+    'use strict';
+
+    fc.series.bar = function() {
+
+        var xScale = d3.time.scale(),
+            yScale = d3.scale.linear(),
+            barWidth = 5,
+            yValue = fc.utilities.valueAccessor('volume'),
+            classForBar = function(d) { return ''; };
+
+        var bar = function(selection) {
+            var series, container;
+
+            selection.each(function(data) {
+
+                // add a 'root' g element on the first enter selection. This ensures
+                // that it is just added once
+                container = d3.select(this)
+                    .selectAll('.bar-series')
+                    .data([data]);
+                container.enter()
+                    .append('g')
+                    .classed('bar-series', true);
+
+                // create a data-join for each rect element
+                series = container
+                    .selectAll('rect')
+                    .data(data);
+
+                // enter
+                series.enter()
+                    .append('rect');
+
+                // exit
+                series.exit()
+                    .remove();
+
+                // update
+                series.attr('x', function(d) { return xScale(d.date) - (barWidth / 2.0); })
+                    .attr('y', function(d) { return yScale(yValue(d)); })
+                    .attr('width', barWidth)
+                    .attr('height', function(d) { return yScale(0) - yScale(yValue(d)); })
+                    .attr('class', classForBar);
+            });
+        };
+
+        bar.xScale = function(value) {
+            if (!arguments.length) {
+                return xScale;
+            }
+            xScale = value;
+            return bar;
+        };
+
+        bar.yScale = function(value) {
+            if (!arguments.length) {
+                return yScale;
+            }
+            yScale = value;
+            return bar;
+        };
+
+        bar.barWidth = function(value) {
+            if (!arguments.length) {
+                return barWidth;
+            }
+            barWidth = value;
+            return bar;
+        };
+
+        bar.yValue = function(value) {
+            if (!arguments.length) {
+                return yValue;
+            }
+            yValue = value;
+            return bar;
+        };
+
+        bar.classForBar = function(value) {
+            if (!arguments.length) {
+                return classForBar;
+            }
+            classForBar = value;
+            return bar;
+        };
+
+        return bar;
+    };
+}(d3, fc));
