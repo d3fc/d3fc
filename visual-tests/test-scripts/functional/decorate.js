d80b26277f3d44b59ec673f14436a8cc14391baf
Colin Eberhardt <ceberhardt@scottlogic.co.uk>
2015-01-09 17:07:45 +0000

    Decorate rolled out to candlestick and ohlc

--- visual-tests/test-scripts/functional/decorate.js
+++ visual-tests/test-scripts/functional/decorate.js
@@ -1,9 +1,12 @@
 (function(d3, fc) {
     'use strict';
 
-    var generator = fc.utilities.dataGenerator()
-        .seedDate(new Date(2014, 1, 1));
-    var data = generator.generate(10);
+    var data = fc.utilities.dataGenerator().randomSeed(12)
+        .seedDate(new Date(2014, 1, 1)).generate(20);
+    var data2 = fc.utilities.dataGenerator().randomSeed(123)
+        .seedDate(new Date(2014, 1, 1)).generate(20);
+    var data3 = fc.utilities.dataGenerator().randomSeed(1234)
+        .seedDate(new Date(2014, 1, 1)).generate(20);
 
     var chartLayout = fc.utilities.chartLayout();
     var chartBuilder = fc.utilities.chartBuilder(chartLayout);
@@ -18,7 +21,7 @@
 
     // Create scale for y axis
     var priceScale = fc.scale.linear()
-        .domain(fc.utilities.extent(data, ['high', 'low']))
+        .domain([90, 110])
         .nice();
 
     // Create the axes
@@ -32,24 +35,50 @@
         .orient('left')
         .ticks(5);
 
-    var color = d3.scale.ordinal()
-        .range(['#98abc5', '#8a89a6', '#7b6888', '#6b486b', '#a05d56']);
+    var color = d3.scale.category10();
 
     var bar = fc.series.bar()
         .decorate(function(sel) {
             sel.attr('fill', function(d, i) { return color(i); });
         })
+        .yValue(function(d) { return d.low - 0.2; })
         .xScale(dateScale)
         .yScale(priceScale);
 
-    // add the components to the chart
+    var candlestick = fc.series.candlestick()
+        .decorate(function(sel) {
+            sel.select('rect').attr('fill', function(d, i) { return color(i); });
+        })
+        .xScale(dateScale)
+        .yScale(priceScale);
+
+    var ohlc = fc.series.ohlc()
+        .decorate(function(sel) {
+            sel.attr('stroke', function(d, i) { return color(i); })
+                .attr('class', '');
+        })
+        .xScale(dateScale)
+        .yScale(priceScale);
+
+    // letting chart builder manage the axes
     chartBuilder.setAxis('bottom', dateAxis);
     chartBuilder.setAxis('left', priceAxis);
-    chartBuilder.addToPlotArea([bar]);
-
-    chartBuilder.setData(data);
-
     chartBuilder.render();
 
+    // manually managing series in order to join with different datasets
+    chartLayout.getPlotArea()
+        .append('g')
+        .datum(data)
+        .call(bar);
+
+    chartLayout.getPlotArea()
+        .append('g')
+        .datum(data2)
+        .call(ohlc);
+
+    chartLayout.getPlotArea()
+        .append('g')
+        .datum(data3)
+        .call(candlestick);
 
 })(d3, fc);
