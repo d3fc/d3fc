23d1dd263d6bcd2e205cff2f8202f48cd2c618ae
James Leftley <jleftley@scottlogic.com>
2015-01-14 14:57:18 +0000

    Update Simple Data Join to support transitions

--- components/utilities/simpleDataJoin.js
+++ components/utilities/simpleDataJoin.js
@@ -1,19 +1,21 @@
 (function(d3, fc) {
     'use strict';
 
-    fc.utilities.simpleDataJoin = function(parent, className, data) {
+    fc.utilities.simpleDataJoin = function(parent, className, data, dataKey) {
         var updateSelection = parent.selectAll('g.' + className)
-            .data(data);
+            .data(data, dataKey || fc.utilities.fn.identity);
 
         var enterSelection = updateSelection.enter()
             .append('g')
             .classed(className, true);
 
-        updateSelection.exit()
+        var exitSelection = d3.transition(updateSelection.exit())
             .remove();
 
-        updateSelection.enter = d3.functor(enterSelection);
+        updateSelection = d3.transition(updateSelection);
 
+        updateSelection.enter = d3.functor(enterSelection);
+        updateSelection.exit = d3.functor(exitSelection);
         return updateSelection;
     };
 }(d3, fc));
