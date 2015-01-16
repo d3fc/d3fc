d92eabe07e0fa249043d1871d484ae058a3ec971
Chris Price <price.c@gmail.com>
2015-01-16 17:40:41 +0000

    Change candlestick to use enter/update/exit

---
+++ visual-tests/test-scripts/series/candlestick.js
@@ -0,0 +1,50 @@
+(function(d3, fc) {
+    'use strict';
+
+    var data = fc.utilities.dataGenerator()
+        .seedDate(new Date(2014, 1, 1))
+        .generate(50);
+
+    var chartLayout = fc.utilities.chartLayout();
+    var chartBuilder = fc.utilities.chartBuilder(chartLayout);
+
+    d3.select('#candlestick')
+        .call(chartBuilder);
+
+    // Create scale for x axis
+    var dateScale = fc.scale.dateTime()
+        .domain(fc.utilities.extent(data, 'date'))
+        .nice();
+
+    // Create scale for y axis
+    var priceScale = fc.scale.linear()
+        .domain(fc.utilities.extent(data, ['high', 'low']))
+        .nice();
+
+    // Create the axes
+    var dateAxis = d3.svg.axis()
+        .scale(dateScale)
+        .orient('bottom')
+        .ticks(5);
+
+    var priceAxis = d3.svg.axis()
+        .scale(priceScale)
+        .orient('left')
+        .ticks(5);
+
+    var candlestick = fc.series.candlestick()
+        .xScale(dateScale)
+        .yScale(priceScale);
+
+    // add the components to the chart
+    chartBuilder.setAxis('bottom', dateAxis);
+    chartBuilder.setAxis('left', priceAxis);
+    chartBuilder.addToPlotArea([candlestick]);
+
+    // associate the data
+    chartBuilder.setData(data);
+
+    // draw stuff!
+    chartBuilder.render();
+
+})(d3, fc);
