1220c357f9a257c365e3ab33cda3146350df00f1
Colin Eberhardt <colin.eberhardt@gmail.com>
2015-01-16 22:28:59 +0000

    Utilised the new property syntax
    Added key function to simple data join

--- components/series/stackedBar.js
+++ components/series/stackedBar.js
@@ -3,15 +3,8 @@
 
     fc.series.stackedBar = function() {
 
-        var xScale = d3.time.scale(),
-            yScale = d3.scale.linear(),
-            barWidth = 20.0,
-            yValueKeys = [],
-            visitor = function(selection, data) { },
-            xValueKey = 'date';
-
         var stackedBar = function(selection) {
-            var series, container;
+            var container;
 
             // takes an object with values associated with each property, and
             // converts it into an array of values. Each value has the xValue associated
@@ -30,9 +23,9 @@
             function objectDatapointToArray(obj) {
                 var values = [];
                 var yTotal = 0;
-                var xVal = obj[xValueKey];
+                var xVal = obj[stackedBar.xValueKey.value];
                 for (var propertyName in obj) {
-                    if (obj.hasOwnProperty(propertyName) && propertyName !== xValueKey) { // be quiet JSHint!
+                    if (obj.hasOwnProperty(propertyName) && propertyName !== stackedBar.xValueKey.value) {
                         var previous = yTotal;
                         yTotal += +obj[propertyName];
                         values.push({
@@ -57,85 +50,50 @@
                     .append('g')
                     .classed('stacked-bar-series', true);
 
-                // create a data-join for each item in the array
-                series = container
-                    .selectAll('g')
-                    .data(data);
+                var keyFunction = function(d) {
+                    return d[stackedBar.xValueKey.value];
+                };
+                var g = fc.utilities.simpleDataJoin(container, 'stacked-bar', data, keyFunction);
 
-                // enter
-                series.enter()
-                    .append('g');
-
-                // exit
-                series.exit()
-                    .remove();
 
                 // create a join for each bar
-                var bar = series.selectAll('rect')
+                var bar = g.selectAll('rect')
                     .data(function(d) { return objectDatapointToArray(d); })
                     .enter()
                     .append('rect');
 
+                // compute the bar width from the x values
+                var xValues = data.map(function(d) {
+                    return stackedBar.xScale.value(d[stackedBar.xValueKey.value]);
+                });
+                var width = stackedBar.barWidth.value(xValues);
+
                 // update
                 bar.attr('x', function(d) {
-                        return xScale(d.xValue) + xScale.rangeBand() / 2 - barWidth / 2; }
+                        return stackedBar.xScale.value(d.xValue) - width / 2; }
                     )
                     .attr('y', function(d) {
-                        return yScale(d.currentValue); }
+                        return stackedBar.yScale.value(d.currentValue); }
                     )
-                    .attr('width', barWidth)
+                    .attr('width', width)
                     .attr('height', function(d) {
-                        return yScale(d.previousValue) - yScale(d.currentValue);
+                        return stackedBar.yScale.value(d.previousValue) - stackedBar.yScale.value(d.currentValue);
                     });
 
-                bar.each(function(d, i) {
-                    var selection = d3.select(this);
-                    visitor(selection, d);
-                });
+                stackedBar.decorate.value(bar);
 
             });
         };
 
-        stackedBar.xScale = function(value) {
-            if (!arguments.length) {
-                return xScale;
-            }
-            xScale = value;
-            return stackedBar;
-        };
+        stackedBar.decorate = fc.utilities.property(fc.utilities.fn.noop);
 
-        stackedBar.visitor = function(value) {
-            if (!arguments.length) {
-                return visitor;
-            }
-            visitor = value;
-            return stackedBar;
-        };
+        stackedBar.barWidth = fc.utilities.functorProperty(fc.utilities.fractionalBarWidth(0.75));
 
-        stackedBar.yScale = function(value) {
-            if (!arguments.length) {
-                return yScale;
-            }
-            yScale = value;
-            return stackedBar;
-        };
+        stackedBar.xScale = fc.utilities.property(d3.time.scale());
 
+        stackedBar.yScale = fc.utilities.property(d3.scale.linear());
 
-        stackedBar.yValueKeys = function(value) {
-            if (!arguments.length) {
-                return yValueKeys;
-            }
-            yValueKeys = value;
-            return stackedBar;
-        };
-
-        stackedBar.xValueKey = function(value) {
-            if (!arguments.length) {
-                return xValueKey;
-            }
-            xValueKey = value;
-            return stackedBar;
-        };
+        stackedBar.xValueKey = fc.utilities.property('name');
 
         return stackedBar;
     };
--- visual-tests/test-scripts/series/stacked-bar.js
+++ visual-tests/test-scripts/series/stacked-bar.js
@@ -1,64 +1,60 @@
 (function(d3, fc) {
     'use strict';
 
-var chartLayout = fc.utilities.chartLayout();
+    var chartLayout = fc.utilities.chartLayout();
 
-// Setup the chart
-var chart = d3.select('#stacked-bar')
-    .call(chartLayout);
+    // Setup the chart
+    var chart = d3.select('#stacked-bar')
+        .call(chartLayout);
 
-d3.csv("stackedBarData.csv", function(error, data) {
+    d3.csv('stackedBarData.csv', function(error, data) {
 
-  var series = Object.getOwnPropertyNames(data[0]).slice(1);
-  var categories = data.slice(1).map(function(d) { return d.State; });
-  var yMax = d3.max(data, function(d) {
-    var total = 0
-    for (var prop in d) {
-      if (prop!="State") {
-        total += +d[prop]
-      }
-    }
-    return total
-  });
+        var categories = data.slice(1).map(function(d) { return d.State; });
+        var yMax = d3.max(data, function(d) {
+            var total = 0;
+            for (var prop in d) {
+                if (prop !== 'State') {
+                    total += +d[prop];
+                }
+            }
+            return total;
+        });
 
-  // create scales
-  var x = d3.scale.ordinal()
-      .domain(categories)
-      .rangeBands([0, chartLayout.getPlotAreaWidth()]);
+        // create scales
+        var x = d3.scale.ordinal()
+            .domain(categories)
+            .rangePoints([0, chartLayout.getPlotAreaWidth()], 1);
 
-  var color = d3.scale.ordinal()
-    .domain(series)
-    .range(["#98abc5", "#8a89a6", "#7b6888", "#6b486b", "#a05d56", "#d0743c", "#ff8c00"]);
+        var color = d3.scale.category10();
 
-  var y = d3.scale.linear()
-    .domain([0, yMax])
-    .range([chartLayout.getPlotAreaHeight(), 0]);
+        var y = d3.scale.linear()
+          .domain([0, yMax])
+          .range([chartLayout.getPlotAreaHeight(), 0]);
 
-  // add axes
-  var bottomAxis = d3.svg.axis()
-      .scale(x)
-      .orient('bottom');
-  chartLayout.getAxisContainer('bottom').call(bottomAxis);
+        // add axes
+        var bottomAxis = d3.svg.axis()
+            .scale(x)
+            .orient('bottom');
+        chartLayout.getAxisContainer('bottom').call(bottomAxis);
 
-  var rightAxis = d3.svg.axis()
-      .scale(y)
-      .orient('right')
-      .ticks(5);
-  chartLayout.getAxisContainer('right').call(rightAxis);
+        var rightAxis = d3.svg.axis()
+            .scale(y)
+            .orient('right')
+            .ticks(5);
+        chartLayout.getAxisContainer('right').call(rightAxis);
 
 
-  var volume = fc.series.stackedBar()
-    .xScale(x)
-    .yValueKeys(series)
-    .xValueKey("State")
-    .yScale(y)
-    .visitor(function(s, d) {
-      s.attr("fill", color(d.name));
+        var stack = fc.series.stackedBar()
+          .xScale(x)
+          .xValueKey('State')
+          .yScale(y)
+          .decorate(function(sel) {
+              sel.attr('fill', function(d, i) { return color(i); });
+          });
+
+        chartLayout.getPlotArea(chart)
+          .datum(data.slice(1))
+          .call(stack);
     });
-
-  chartLayout.getPlotArea(chart)
-    .datum(data.slice(1))
-    .call(volume)
-});
 
 })(d3, fc);
