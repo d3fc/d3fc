60d4d10e417ed5549a000fc7c23b74fc102e84f4
Chris Price <price.c@gmail.com>
2015-01-18 17:24:23 +0000

    Update line series to use properties/decorate pattern

--- components/series/line.js
+++ components/series/line.js
@@ -3,110 +3,66 @@
 
     fc.series.line = function() {
 
-        var xValue = fc.utilities.valueAccessor('date'),
-            yValue = fc.utilities.valueAccessor('close'),
-            xScale = fc.scale.dateTime(),
-            yScale = fc.scale.linear(),
-            underFill = true,
-            area = d3.svg.area(),
-            d3line = d3.svg.line(),
-            css = 'line-series';
+        // convenience functions that return the x & y screen coords for a given point
+        var x = function(d) { return line.xScale.value(line.xValue.value(d)); };
+        var y = function(d) { return line.yScale.value(line.yValue.value(d)); };
 
         var line = function(selection) {
-            var container, areapath, linepath;
+
             selection.each(function(data) {
 
-                if (underFill) {
-                    area.x(function(d) { return xScale(xValue(d)); })
-                        .y0(yScale(0))
-                        .y1(function(d) { return yScale(yValue(d)); });
-                }
-
-                d3line
-                    .x(function(d) { return xScale(xValue(d)); })
-                    .y(function(d) { return yScale(yValue(d)); });
-
-                // add a 'root' g element on the first enter selection. This ensures
-                // that it is just added once
-                container = d3.select(this)
-                    .selectAll('.' + css)
+                var g = d3.select(this)
+                    .selectAll('.line-series')
                     .data([data]);
-                container.enter()
+
+                g.enter()
                     .append('g')
-                    .classed(css, true);
+                    .classed('line-series', true);
 
-                areapath = container
-                        .selectAll('.area')
-                        .data([data]);
-                // enter
-                areapath.enter()
-                    .append('path');
-                // update
-                areapath
-                    .classed('area', true)
-                    .attr('d', function(d) {
-                        return underFill ? area(d) : null;
-                    });
-                // exit
-                areapath.exit()
+                g.exit()
                     .remove();
 
+                var areaPath = g.selectAll('.area')
+                        .data(line.underFill.value.apply(this, arguments) ? [data] : []);
 
-                linepath = container
-                    .selectAll('.line')
+                areaPath.enter()
+                    .append('path')
+                    .classed('area', true);
+
+                var areaData = d3.svg.area()
+                    .x(x)
+                    .y0(line.yScale.value(0))
+                    .y1(y);
+                areaPath.attr('d', areaData);
+
+                areaPath.exit()
+                    .remove();
+
+                var linePath = g.selectAll('.line')
                     .data([data]);
-                // enter
-                linepath.enter()
-                    .append('path');
-                // update
-                linepath
-                    .classed('line', true)
-                    .attr('d', d3line);
-                // exit
-                linepath.exit()
+
+                linePath.enter()
+                    .append('path')
+                    .classed('line', true);
+
+                var lineData = d3.svg.line()
+                    .x(x)
+                    .y(y);
+                linePath.attr('d', lineData);
+
+                linePath.exit()
                     .remove();
+
+                line.decorate.value(g);
             });
         };
 
-        line.xValue = function(value) {
-            if (!arguments.length) {
-                return xValue;
-            }
-            xValue = value;
-            return line;
-        };
-
-        line.yValue = function(value) {
-            if (!arguments.length) {
-                return yValue;
-            }
-            yValue = value;
-            return line;
-        };
-
-        line.xScale = function(value) {
-            if (!arguments.length) {
-                return xScale;
-            }
-            xScale = value;
-            return line;
-        };
-
-        line.yScale = function(value) {
-            if (!arguments.length) {
-                return yScale;
-            }
-            yScale = value;
-            return line;
-        };
-
-        line.underFill = function(value) {
-            if (!arguments.length) {
-                return underFill;
-            }
-            underFill = value;
-            return line;
-        };
+        line.decorate = fc.utilities.property(fc.utilities.fn.noop);
+        line.xScale = fc.utilities.property(d3.time.scale());
+        line.yScale = fc.utilities.property(d3.scale.linear());
+        line.yValue = fc.utilities.property(fc.utilities.valueAccessor('close'));
+        line.xValue = fc.utilities.property(fc.utilities.valueAccessor('date'));
+        line.underFill = fc.utilities.functorProperty(true);
 
         return line;
     };
