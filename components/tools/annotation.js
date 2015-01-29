42656ad147a323a1242fd9bde13ee49edede7caf
James Leftley <jleftley@scottlogic.com>
2015-01-29 16:34:17 +0000

    Add key function to Annotation

--- components/tools/annotation.js
+++ components/tools/annotation.js
@@ -11,7 +11,7 @@
                 var container = d3.select(this);
 
                 // Create a group for each annotation
-                var g = fc.utilities.simpleDataJoin(container, 'annotation', data, annotation.yValue.value);
+                var g = fc.utilities.simpleDataJoin(container, 'annotation', data, annotation.keyFunction.value);
 
                 // Added the required elements - each annotation consists of a line and text label
                 var enter = g.enter();
@@ -39,6 +39,7 @@
         annotation.xScale = fc.utilities.property(d3.time.scale());
         annotation.yScale = fc.utilities.property(d3.scale.linear());
         annotation.yValue = fc.utilities.functorProperty(fc.utilities.fn.identity);
+        annotation.keyFunction = fc.utilities.functorProperty(function(d, i) { return i; });
         annotation.label = fc.utilities.functorProperty(annotation.yValue.value);
         annotation.padding = fc.utilities.functorProperty(2);
         annotation.decorate = fc.utilities.property(fc.utilities.fn.noop);
