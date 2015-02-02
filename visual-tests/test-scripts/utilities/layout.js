55a3eb9cf4d179ac30a0628b6738b9c50e74df9b
Colin Eberhardt <colin.eberhardt@gmail.com>
2015-02-02 21:35:14 +0000

    Fixes #276
    (and re-instates the file that I deleted by mistake - I really should double-check the visual tests before committing each time!)

---
+++ visual-tests/test-scripts/utilities/layout.js
@@ -0,0 +1,58 @@
+(function(d3, fc) {
+    'use strict';
+
+    var layout = fc.utilities.layout();
+
+    var chart = d3.select('#layout-test');
+    chart.call(layout);
+
+    var plotArea = chart.select('.plotArea');
+    var width = plotArea.attr('layout-width'),
+        height = plotArea.attr('layout-height');
+
+    var data = fc.utilities.dataGenerator()
+        .seedDate(new Date(2014, 1, 1))
+        .randomSeed('12345')
+        .generate(50);
+
+    // Calculate the scale domain
+    var day = 8.64e7, // One day in milliseconds
+        dateFrom = new Date(d3.min(data, function(d) { return d.date; }).getTime() - day),
+        dateTo = new Date(d3.max(data, function(d) { return d.date; }).getTime() + day),
+        priceFrom = d3.min(data, function(d) { return d.low; }),
+        priceTo = d3.max(data, function(d) { return d.high; });
+
+    // Create scale for x axis
+    var dateScale = fc.scale.dateTime()
+        .domain([dateFrom, dateTo])
+        .range([0, width])
+        .nice();
+
+    // Create scale for y axis
+    var priceScale = fc.scale.linear()
+        .domain([priceFrom, priceTo])
+        .range([height, 0])
+        .nice();
+
+    // Create the axes
+    var dateAxis = d3.svg.axis()
+        .scale(dateScale)
+        .orient('bottom')
+        .ticks(5);
+
+    var priceAxis = d3.svg.axis()
+        .scale(priceScale)
+        .orient('right')
+        .ticks(5);
+
+    // Create the line series
+    var line = fc.series.line()
+        .xScale(dateScale)
+        .yScale(priceScale)
+        .yValue(function(d) { return d.open; });
+
+
+    plotArea.datum(data).call(line);
+    chart.select('.axis.right').call(priceAxis);
+    chart.select('.axis.bottom').call(dateAxis);
+})(d3, fc);
