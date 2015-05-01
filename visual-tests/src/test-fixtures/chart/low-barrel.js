7b07bc0f9a6ad4523660ae25b84c07f280497764
Chris Price <price.c@gmail.com>
2015-05-01 13:14:28 +0100

    Update low-barrel example

--- visual-tests/src/test-fixtures/chart/low-barrel.js
+++ visual-tests/src/test-fixtures/chart/low-barrel.js
@@ -1,177 +1,172 @@
 (function(d3, fc) {
     'use strict';
 
-    var tooltip = function() {
-
-        var tooltip = function(selection) {
-
-            var container = selection.enter()
-                .append('g')
-                .attr('class', 'info');
-
-            container.append('rect')
-                .attr({
-                    width: 130,
-                    height: 76,
-                    fill: 'white'
-                });
-
-            container.append('text');
-
-            container = selection.select('g.info')
-                .attr('transform', function(d) {
-                    var dx = Number(d.x);
-                    var x = dx < 150 ? dx + 10 : dx - 150 + 10;
-                    return 'translate(' + x + ',' + 10 + ')';
-                });
-
-            var tspan = container.select('text')
-                .selectAll('tspan')
-                .data(tooltip.items.value);
-
-            tspan.enter()
-                .append('tspan')
-                .attr('x', 4)
-                .attr('dy', 12);
-
-            tspan.text(function(d) {
-                return d(container.datum().datum);
-            });
-        };
-
-        function format(type, value) {
-            return tooltip.formatters.value[type](value);
-        }
-
-        tooltip.items = fc.utilities.property([
-            function(d) { return format('date', d.date); },
-            function(d) { return 'Open: ' + format('price', d.open); },
-            function(d) { return 'High: ' + format('price', d.high); },
-            function(d) { return 'Low: ' + format('price', d.low); },
-            function(d) { return 'Close: ' + format('price', d.close); },
-            function(d) { return 'Volume: ' + format('volume', d.volume); }
-        ]);
-        tooltip.formatters = fc.utilities.property({
-            date: d3.time.format('%A, %b %e, %Y'),
-            price: d3.format('.2f'),
-            volume: d3.format('0,5p')
-        });
-
-        return tooltip;
-    };
-
     var dataGenerator = fc.utilities.dataGenerator()
         .startDate(new Date(2014, 1, 1));
 
     var container = d3.select('#low-barrel')
         .datum(dataGenerator(250));
 
-    var mainContainer = container.select('.main-row>td.chart')
-        .append('svg')
-        .style({
-            width: '100%',
-            height: '100%'
-        });
+    function mainChart(selection) {
 
-    var volumeContainer = container.select('.volume-row>td.chart')
-        .append('svg')
-        .style({
-            width: '100%',
-            height: '100%'
-        });
+        var data = selection.datum();
 
-    var navigatorContainer = container.select('.navigator-row>td.chart')
-        .append('svg')
-        .style({
-            width: '100%',
-            height: '100%'
-        });
-
-    function mainChart(data) {
         var gridlines = fc.scale.gridlines()
             .yTicks(3);
 
         var candlestick = fc.series.candlestick();
 
         var crosshairs = fc.tools.crosshairs()
-            .decorate(tooltip())
-            .snap(fc.utilities.seriesPointSnap(candlestick, data));
-        // .on('tracking.link', render);
+            .decorate(fc.tooltip())
+            .snap(fc.utilities.seriesPointSnap(candlestick, data))
+            .on('trackingmove.link', render);
 
         var multi = fc.series.multi()
-            .series([gridlines, candlestick, crosshairs]);
+            .series([gridlines, candlestick, crosshairs])
+            .mapping(function(data, series) {
+                switch (series) {
+                    case crosshairs:
+                        return data.crosshairs;
+                    default:
+                        return data;
+                }
+            });
 
         var chart = fc.charts.linearTimeSeries()
-            .yDomain(fc.utilities.extent(data, 'high', 'low'))
+            .xDomain(data.dateDomain)
+            .xTicks(0)
+            .yDomain(fc.utilities.extent(data, ['high', 'low']))
+            .yNice()
             .yTicks(3)
             .plotArea(multi);
 
-        return chart;
+        selection.call(chart);
     }
 
-    function volumeChart(data) {
+    function volumeChart(selection) {
+
+        var data = selection.datum();
+
         var gridlines = fc.scale.gridlines()
             .yTicks(2);
 
         var bar = fc.series.bar()
             .yValue(function(d) { return d.volume; });
 
+        var crosshairs = fc.tools.crosshairs()
+            .snap(fc.utilities.seriesPointSnap(bar, data))
+            .on('trackingmove.link', render);
+
         var multi = fc.series.multi()
-            .series([gridlines, bar]);
+            .series([gridlines, bar, crosshairs])
+            .mapping(function(data, series) {
+                switch (series) {
+                    case crosshairs:
+                        return data.crosshairs;
+                    default:
+                        return data;
+                }
+            });
 
         var chart = fc.charts.linearTimeSeries()
+            .xDomain(data.dateDomain)
             .yDomain(fc.utilities.extent(data, 'volume'))
+            .yNice()
             .yTicks(2)
             .plotArea(multi);
 
-        return chart;
+        selection.call(chart);
     }
 
-    function navigatorChart(data) {
+    function navigatorChart(selection) {
+
+        var data = selection.datum();
+
+        var yDomain = fc.utilities.extent(data, 'close');
+
+        var chart = fc.charts.linearTimeSeries()
+            .xDomain(fc.utilities.extent(data, 'date'))
+            .yDomain(yDomain)
+            .yNice()
+            .xTicks(3)
+            .yTicks(0);
+
         var gridlines = fc.scale.gridlines()
             .xTicks(3)
             .yTicks(0);
 
         var line = fc.series.line();
+
         var area = fc.series.area();
+
+        var brush = d3.svg.brush()
+            .on('brush', function() {
+                var domain = [brush.extent()[0][0], brush.extent()[1][0]];
+                // Scales with a domain delta of 0 === NaN
+                if (domain[0] - domain[1] !== 0) {
+                    data.dateDomain = domain;
+                    render();
+                }
+            });
+
         var multi = fc.series.multi()
-            .series([gridlines, line, area]);
+            .series([gridlines, line, area, brush])
+            .mapping(function(data, series) {
+                // Need to set the extent AFTER the scales
+                // are set AND their ranges defined
+                if (series === brush) {
+                    brush.extent([
+                        [data.dateDomain[0], chart.yDomain()[0]],
+                        [data.dateDomain[1], chart.yDomain()[1]]
+                    ]);
+                }
+                return data;
+            });
 
-        var chart = fc.charts.linearTimeSeries()
-            .xDomain(fc.utilities.extent(data, 'date'))
-            .yDomain(fc.utilities.extent(data, 'close'))
-            .xTicks(3)
-            .yTicks(0)
-            .plotArea(multi);
+        chart.plotArea(multi);
 
-        return chart;
+        selection.call(chart);
     }
 
-    function render(dateDomain) {
-        // Calculate visible data to use when calculating scale domains
+    function render() {
         var data = container.datum();
+
+        // Enhance data with interactive state
+        if (data.crosshairs == null) {
+            data.crosshairs = [];
+        }
+        if (data.dateDomain == null) {
+            var maxDate = fc.utilities.extent(container.datum(), 'date')[1];
+            var dateScale = d3.time.scale()
+                .domain([maxDate - 50 * 24 * 60 * 60 * 1000, maxDate])
+                .nice();
+            data.dateDomain = dateScale.domain();
+        }
+
+        // Calculate visible data for main/volume charts
         var bisector = d3.bisector(function(d) { return d.date; });
         var visibleData = data.slice(
-            bisector.left(data, dateDomain[0]),
-            bisector.right(data, dateDomain[1])
+            // Pad and clamp the bisector values to ensure extents can be calculated
+            Math.max(0, bisector.left(data, data.dateDomain[0]) - 1),
+            Math.min(bisector.right(data, data.dateDomain[1]) + 1, data.length)
         );
+        visibleData.dateDomain = data.dateDomain;
+        visibleData.crosshairs = data.crosshairs;
 
-        var main = mainChart(visibleData)
-            .xDomain(dateDomain);
-        mainContainer.call(main);
+        container.select('svg.main')
+            .datum(visibleData)
+            .call(mainChart);
 
-        var vol = volumeChart(visibleData)
-            .xDomain(dateDomain);
-        volumeContainer.call(vol);
+        container.select('svg.volume')
+            .datum(visibleData)
+            .call(volumeChart);
 
-        var nav = navigatorChart(data);
-        navigatorContainer.call(nav);
+        container.select('svg.navigator')
+            .call(navigatorChart);
+
+        // TODO: Add pan/zoom behaviour
     }
 
-    var maxDate = fc.utilities.extent(container.datum(), 'date')[1];
-    var dateScale = d3.time.scale()
-        .domain([maxDate - 50 * 24 * 60 * 60 * 1000, maxDate])
-        .nice();
-    render(dateScale.domain());
+    render();
 
 })(d3, fc);
