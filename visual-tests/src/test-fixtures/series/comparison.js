9a7c94544cfc2407d3603a9a487f3b8183450782
Jan Akerman <jakerman@scottlogic.co.uk>
2015-05-08 12:14:48 +0100

    WIP: Update visual test

--- visual-tests/src/test-fixtures/series/comparison.js
+++ visual-tests/src/test-fixtures/series/comparison.js
@@ -1,19 +1,19 @@
 (function(d3, fc) {
     'use strict';
 
+    function renderData(data) {
+        d3.select('#comparison')
+            .append('svg')
+            .datum(data)
+            .call(chart);
+    }
+
     var data = [
         fc.utilities.dataGenerator().startDate(new Date(2014, 1, 1))(50),
         fc.utilities.dataGenerator().startDate(new Date(2013, 12, 15))(50)
     ];
 
-    var chartLayout = fc.test.chartLayout();
-    var chartBuilder = fc.test.chartBuilder(chartLayout);
     var zoom = d3.behavior.zoom();
-
-    d3.select('#comparison')
-        .call(chartBuilder);
-
-    chartLayout.getChartArea().call(zoom);
 
     var findIndex = function(data, item, field) {
         // Find insertion point for item in seriesData.
@@ -23,67 +23,40 @@
             }).left;
         return bisect(data, item);
     };
-
-    // Create scale for x axis
-    var dateScale = fc.scale.dateTime()
-        .domain(fc.utilities.extent(data, 'date'))
-        .nice();
-
-    // Create scale for y axis
-    var priceScale = d3.scale.linear()
-        .domain(fc.utilities.extent(data, ['high', 'low']))
-        .nice();
-
-    // Create the axes
-    var dateAxis = d3.svg.axis()
-        .scale(dateScale)
-        .orient('bottom')
-        .ticks(5);
-
-    var priceAxis = d3.svg.axis()
-        .scale(priceScale)
-        .orient('left')
-        .ticks(5);
-
     var percentageChange = fc.math.percentageChange()
         .inputValue(function(d) { return d.close; })
         .outputValue(function(d, value) { d.percentageChange = value; });
 
     data.forEach(percentageChange);
 
-    priceScale.domain(fc.utilities.extent(data, ['percentageChange']))
-        .nice();
+    var chart = fc.charts.linearTimeSeries()
+        .xDomain(fc.utilities.extent(data, 'date'))
+        .xNice()
+        .xTicks(5)
+        .yDomain(fc.utilities.extent(data, ['percentageChange']))
+        .yNice()
+        .yTicks(5);
 
     var line = fc.series.line()
-        .xScale(dateScale)
-        .yScale(priceScale)
         .xValue(function(d) { return d.date; })
         .yValue(function(d) { return d.percentageChange; });
 
-    // add the components to the chart
-    chartBuilder.setAxis('bottom', dateAxis);
-    chartBuilder.setAxis('left', priceAxis);
-    chartBuilder.addToPlotArea([
-        function(selection) {
-            selection.each(function(data) {
-                var container = d3.select(this);
-                fc.utilities.simpleDataJoin(container, 'comparison-line', data)
-                    .call(line);
-            });
-        }
-    ]);
+    var multi = fc.series.multi()
+        .series([line, line])
+        .mapping(function(data, series, i) {
+            return data[i];
+        });
 
-    // associate the data
-    chartBuilder.setData(data);
-    // draw stuff!
-    chartBuilder.render();
+    chart.plotArea(multi);
+
+    renderData(data);
 
     // zoom stuff!
     function zoomed() {
         var comparisonData = [];
         data.forEach(function(d) {
-            var leftIndex = findIndex(d, dateScale.domain()[0], 'date'),
-                rightIndex = findIndex(d, dateScale.domain()[1], 'date');
+            var leftIndex = findIndex(d, chart.xDomain()[0], 'date'),
+                rightIndex = findIndex(d, chart.xDomain()[1], 'date');
             if (leftIndex !== 0) {
                 leftIndex -= 1; // Try to base from the data point one before the LHS of the date axis.
             }
@@ -91,11 +64,14 @@
             comparisonData.push(d.slice(leftIndex, rightIndex + 1));
             return percentageChange(d);
         });
-        chartBuilder.setData(data);
-        priceScale.domain(fc.utilities.extent(comparisonData, ['percentageChange'])).nice();
-        chartBuilder.render();
+        chart.xDomain(fc.utilities.extent(comparisonData, ['percentageChange']))
+            .nice();
+        renderData(data);
     }
 
-    zoom.x(dateScale).on('zoom', zoomed).scaleExtent([0.5, 3]);
+    zoom.x(chart.xScale()).on('zoom', zoomed).scaleExtent([0.5, 3]);
+
+
+
 
 })(d3, fc);
