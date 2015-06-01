60a826f1b27559db09257ea890a6aaf6f2c737be
Colin Eberhardt <colin.eberhardt@gmail.com>
2015-06-01 08:27:14 +0100

    Permit nested multi-series
    Fixes #393

--- src/series/multi.js
+++ src/series/multi.js
@@ -14,7 +14,13 @@
 
                 var container = d3.select(this);
 
+                // in order to support nested multi-series, this selector
+                // is filtered to only return immediate children of the container
+                var self = this;
                 var g = container.selectAll('g.multi-outer')
+                    .filter(function() {
+                        return this.parentNode === self;
+                    })
                     .data(series);
 
                 g.enter()
