8e92d46d0002585002b069a1b3319a8fd6cb2fe1
Chris Price <price.c@gmail.com>
2015-06-22 10:23:14 +0100

    Revert "Removed version number from fc.js"
    This reverts commit 5db890e9d903843b8c6eb394650867a3e42f7b31.
    
    The release process has changed so we can now do this reliably.
    
    Conflicts:
    	src/fc.js

--- src/fc.js
+++ src/fc.js
@@ -1,6 +1,9 @@
 (function() {
     'use strict';
 
+    // Needs to be defined like this so that the grunt task can update it
+    var version = '0.2.0';
+
     // Crazyness to get a strict mode compliant reference to the global object
     var global = null;
     /* jshint ignore:start */
@@ -22,6 +25,7 @@
         series: {},
         svg: {},
         tool: {},
-        util: {}
+        util: {},
+        version: version
     };
 }());
