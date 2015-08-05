c5fbc1efc785cb151b778d333f02a247dc431ecb
Chris Price <price.c@gmail.com>
2015-08-05 15:18:48 +0100

    Add low-barrel and yahoo-finance-chart examples

---
+++ site/src/examples/yahoo-finance-chart/index.js
@@ -0,0 +1,298 @@
+d3.json('yahoo.json', function(error, json) {
+    var chartData = json.chart.result[0];
+    var quoteData = chartData.indicators.quote[0];
+    var data = chartData.timestamp.map(function(d, i) {
+        var date = new Date(d * 1000 - 5 * 1000 * 60 * 60);
+        return {
+            date: date,
+            high: quoteData.high[i],
+            low: quoteData.low[i],
+            open: quoteData.open[i],
+            close: quoteData.close[i],
+            volume: quoteData.volume[i]
+        };
+    });
+
+    // select a subset of data
+    data = data.slice(0, 600);
+    // filter out any data that is > 2 hours outside of trading
+    data = data.filter(function(d) {
+        return d.date.getHours() > 7 && d.date.getHours() < 19;
+    });
+    renderChart(data);
+});
+
+var yAxisWidth = 40,
+    xAxisHeight = 20,
+    calloutLeftMargin = 10,
+    calloutHeight = 14,
+    calloutWidth = yAxisWidth - calloutLeftMargin;
+
+var dateFormat = d3.time.format('%a %H:%M%p');
+var priceFormat = d3.format('.2f');
+var volumeFormat = d3.format('s');
+
+function calloutPathData(width, height) {
+    var h2 = height / 2;
+    return [
+        [0, 0],
+        [h2, -h2],
+        [width, -h2],
+        [width, h2],
+        [h2, h2],
+        [0, 0]
+    ];
+}
+
+function addCallout(sel) {
+    sel.enter()
+        .select('.right-handle')
+        .classed('callout', true)
+        .insert('path', ':first-child')
+        .attr('transform', 'translate(' + calloutLeftMargin + ', 0)')
+        .attr('d', d3.svg.area()(calloutPathData(calloutWidth, calloutHeight)));
+
+    sel.select('text')
+        .attr('transform', 'translate(' + yAxisWidth + ', ' + (calloutHeight / 4) + ')')
+        .attr('x', 0)
+        .attr('y', 0);
+}
+
+function addXCallout(sel) {
+    sel.enter()
+        .select('.top-handle')
+        .select('text')
+        .remove();
+
+    var xLabelContainer = sel.enter()
+        .select('.bottom-handle');
+
+    xLabelContainer.classed('callout', true)
+        .append('rect')
+        .attr('transform', 'translate(-40, 0)')
+        .attr('width', 80)
+        .attr('height', xAxisHeight);
+
+    xLabelContainer.append('text')
+        .attr('y', xAxisHeight / 2)
+        .text(function(d) { return dateFormat(d.x); });
+}
+
+var legend = fc.chart.legend()
+    .items([
+        ['open', function(d) { return priceFormat(d.open); }],
+        ['high', function(d) { return priceFormat(d.high); }],
+        ['low', function(d) { return priceFormat(d.low); }],
+        ['close', function(d) { return priceFormat(d.close); }],
+        ['volume', function(d) { return volumeFormat(d.volume); }]
+    ]);
+
+function renderLegend(datapoint) {
+    d3.select('#legend')
+        .data([datapoint])
+        .call(legend);
+}
+
+function renderChart(data) {
+
+  data.crosshair = [];
+
+  // add a moving average to the data
+  var movingAverage = fc.indicator.algorithm.exponentialMovingAverage()
+        .value(function(d) { return d.close; })
+        .windowSize(20);
+
+  movingAverage(data);
+
+  // add a volume series container to the layout
+  var container = d3.select('#time-series');
+  var volumeContainer = container.selectAll('g.volume')
+        .data([data]);
+  volumeContainer.enter()
+      .append('g')
+      .attr({
+          'class': 'volume',
+      })
+      .layout({
+          position: 'absolute',
+          top: 150,
+          bottom: xAxisHeight,
+          right: yAxisWidth,
+          left: 0
+      });
+
+  var layout = fc.layout();
+  container.layout();
+
+  var volumeScale = d3.scale.linear()
+    .domain([0, d3.max(data, function (d) { return Number(d.volume); })])
+    .range([volumeContainer.layout('height'), 0]);
+
+  var discontinuity = fc.scale.discontinuity.tradedHours()
+          .trades(data.map(function(d) { return d.date; }));
+
+  // add a time series components
+  var chart = fc.chart.linearTimeSeries()
+        .xDomain(fc.util.extent(data, 'date'))
+        .xDiscontinuityProvider(discontinuity)
+        .yDomain(fc.util.extent(data, ['open', 'close']))
+        .xTickFormat(dateFormat)
+        .yTickFormat(priceFormat)
+        .yTicks(5)
+        .yNice(5)
+        .yOrient('right')
+        .yTickSize(yAxisWidth, 0)
+        .xTickSize(xAxisHeight)
+        .xTicks(3);
+
+  // create the line annotations
+  var emaClose = fc.annotation.line()
+    .value(function(d) { return d.exponentialMovingAverage; })
+    .label(function(d) { return priceFormat(d.exponentialMovingAverage); })
+    .decorate(function(sel) {
+      addCallout(sel);
+      sel.enter().classed('ema', true);
+    });
+
+  var lastClose = fc.annotation.line()
+      .value(function(d) { return d.close; })
+      .label(function(d) { return priceFormat(d.close); })
+      .decorate(function(sel) {
+        addCallout(sel);
+        sel.enter().classed('close', true);
+      });
+
+  // create the series
+  var area = fc.series.area()
+        .y0Value(chart.yDomain()[0])
+        .yValue(function(d) { return d.open; });
+
+  var line = fc.series.line()
+        .yValue(function(d) { return d.open; });
+
+  var emaLine = fc.series.line()
+        .yValue(function(d) { return d.exponentialMovingAverage; })
+        .decorate(function(g) {
+          g.classed('ema', true);
+        });
+
+  var gridlines = fc.annotation.gridline()
+        .yTicks(5)
+        .xTicks(0);
+
+  // add a crosshair
+  var crosshair = fc.tool.crosshair()
+            .snap(fc.util.seriesPointSnapXOnly(line, data))
+            .xLabel(function(d) { return dateFormat(d.datum.date); })
+            .yLabel(function(d) { return priceFormat(d.datum.close); })
+            .decorate(function(sel) {
+                sel.enter().select('circle').attr('r', 3);
+                addCallout(sel);
+                addXCallout(sel);
+            })
+            .on('trackingmove', function(crosshairData) {
+                renderLegend(crosshairData[0].datum);
+            })
+            .on('trackingend', function() {
+                renderLegend(data[data.length - 1]);
+            });
+
+  // add vertical lines and bands
+  var tradingHourMarkers = discontinuity.orderedExtents()
+    .map(function(extent) {
+        var open = d3.time.day.floor(extent.start);
+        var close = d3.time.day.floor(extent.start);
+        open.setHours(9);
+        open.setMinutes(30);
+        close.setHours(16);
+        return [{
+                date: extent.start,
+                type: '#preMarket-1-1072d'
+            }, {
+                date: open,
+                type: '#activeDay-1-1072d'
+            }, {
+                date: close,
+                type: '#postMarket-1-1072d'
+            }];
+    });
+
+  var verticalLineData = [].concat.apply([], tradingHourMarkers);
+  var verticalBandData = d3.pairs(tradingHourMarkers);
+
+  function verticalLineData() {
+      return [].concat.apply([], tradingHours());
+  }
+
+  var verticalLines = fc.annotation.line()
+      .orient('vertical')
+      .value(function(d) { return d.date; })
+      .label(function(d) { return ''; })
+      .decorate(function(sel) {
+          sel.enter()
+              .select('.bottom-handle')
+              .append('use')
+              .attr('transform', 'translate(0, -15)')
+              .attr('xlink:href', function(d) { return d.type; });
+      });
+
+  var verticalBands = fc.annotation.band()
+      .x0(function(d) { return d[0][2].date; })
+      .x1(function(d) { return d[1][1].date; });
+
+  // combine the series with a 'multi'
+  var multi = fc.series.multi()
+        .series([gridlines, area, emaLine, line, emaClose, lastClose, crosshair,
+            verticalLines, verticalBands])
+        .mapping(function(series) {
+            switch (series) {
+              case emaClose:
+              case lastClose:
+                return [data[data.length - 1]];
+              case crosshair:
+                return data.crosshair;
+              case verticalLines:
+                return verticalLineData;
+              case verticalBands:
+                return verticalBandData;
+              default:
+                return data;
+            }
+        });
+
+  chart.plotArea(multi);
+
+  // render the chart
+  d3.select('#time-series')
+        .datum(data)
+        .call(chart);
+
+  // create a volume series and render
+  var volume = fc.series.bar()
+      .xScale(chart.xScale())
+      .yScale(volumeScale)
+      .yValue(function(d) { return d.volume; })
+      .decorate(function(sel) {
+        sel.select('path')
+          .style('stroke', function(d, i) {
+            return d.close > d.open ? 'red' : 'green';
+          });
+      });
+
+  volumeContainer
+      .datum(data)
+      .call(volume);
+
+  // render the legend
+  renderLegend(data[data.length - 1]);
+
+  // customise the D3 axis
+  d3.selectAll('.y-axis text')
+      .style('text-anchor', 'end')
+      .attr('transform', 'translate(-3, -8)');
+
+  d3.selectAll('.x-axis text')
+      .attr('dy', undefined)
+      .style({'text-anchor': 'start', 'dominant-baseline': 'central'})
+      .attr('transform', 'translate(3, -' + (xAxisHeight / 2 + 3) + ' )');
+}
---
+++ site/src/examples/yahoo-finance-chart/index.md
@@ -0,0 +1,39 @@
+---
+layout: example
+title: Yahoo Finance Chart
+namespace: examples
+---
+<style>@import "index.css";</style>
+
+<div style='margin-right: 40px;position:relative'>
+  <div id="legend"></div>
+  <svg id='time-series' style='height: 300px; width: 100%;  overflow: visible'>
+    <defs>
+      <linearGradient id="area-gradient"
+                      x1="0%" y1="0%"
+                      x2="0%" y2="100%">
+         <stop offset="0%" stop-opacity="0.3" stop-color="#fff" />
+        <stop offset="100%" stop-opacity="0" stop-color="#1a9af9" />
+      </linearGradient>
+      <g id="activeDay-1-1072d" transform="translate(-9.5-9.5)">
+          <circle cx="9.5" cy="9.5" r="9.5" fill="#fff"/>
+          <path d="M19 9.5c0 5.2-4.3 9.5-9.5 9.5S0 14.7 0 9.5 4.3 0 9.5 0 19 4.3 19 9.5zm-.9 0c0-.4-.3-.7-.7-.7H16c-.4 0-.7.3-.7.7 0 .4.3.7.7.7h1.4c.3 0 .7-.3.7-.7zm-2.5-5c.3-.3.3-.7 0-1-.3-.3-.7-.3-1 0l-1 1c-.3.3-.3.7 0 1 .3.3.7.3 1 0l1-1zm-1.8 5c0-2.4-1.9-4.3-4.3-4.3S5.2 7.1 5.2 9.5s1.9 4.3 4.3 4.3 4.3-1.9 4.3-4.3zm-3.6-6.4V1.7c0-.4-.3-.7-.7-.7-.4 0-.7.3-.7.7v1.4c0 .4.3.7.7.7.4 0 .7-.3.7-.7zM5.5 5.5c.3-.3.3-.7 0-1l-1-1c-.3-.3-.7-.3-1 0s-.3.7 0 1l1 1c.2.2.7.2 1 0zM3.1 8.8H1.6c-.4 0-.7.3-.7.7 0 .4.3.7.7.7H3c.4 0 .7-.3.7-.7.1-.4-.2-.7-.6-.7zm.3 5.7c-.3.3-.3.7 0 1 .3.3.7.3 1 0l1-1c.3-.3.3-.7 0-1-.3-.3-.7-.3-1 0l-1 1zm5.4 1.4v1.4c0 .4.3.7.7.7.4 0 .7-.3.7-.7v-1.4c0-.4-.3-.7-.7-.7-.4 0-.7.3-.7.7zm4.7-2.4c-.3.3-.3.7 0 1l1 1c.3.3.7.3 1 0 .3-.3.3-.7 0-1l-1-1c-.2-.2-.7-.2-1 0z"/>
+      </g>
+      <g id="postMarket-1-1072d" transform="translate(-9.5-9.5)">
+          <circle cx="9.5" cy="9.5" r="9.5" fill="#fff"/>
+          <path d="M9.5 19c5.2 0 9.5-4.3 9.5-9.5S14.7 0 9.5 0 0 4.3 0 9.5 4.3 19 9.5 19zM7.9 1.7c.6-.2 1.3-.3 2-.3C14.4 1.4 18 5 18 9.5s-3.6 8.1-8.1 8.1c-.7 0-1.4-.1-2-.3 3.5-.9 6.1-4.1 6.1-7.8 0-3.8-2.6-6.9-6.1-7.8z" fill-rule="evenodd" clip-rule="evenodd"/>
+      </g>
+      <g id="preMarket-1-1072d" transform="translate(-9.5-9.5)">
+          <circle cx="9.5" cy="9.5" r="9.5" fill="#fff"/>
+          <path d="M19 9.5c0 5.2-4.3 9.5-9.5 9.5S0 14.7 0 9.5 4.3 0 9.5 0 19 4.3 19 9.5zM14.3 14.3l-3.1-3.7c.2-.3.4-.7.4-1.1 0-.9-.6-1.7-1.5-1.9V2.3c0-.3-.2-.5-.5-.5s-.6.3-.6.5v5.2c-.9.2-1.5 1-1.5 1.9 0 1.1.9 2 2 2 .3 0 .6-.1.9-.2l3.2 3.7c.1.1.2.2.4.2.1 0 .2 0 .3-.1.1-.2.2-.5 0-.7z"/>
+      </g>
+    </defs>
+  </svg>
+</div>
+<script src="tradedHours.js"></script>
+<script src="index.js"></script>
+
+This example shows how d3fc components can be used to recreate the complex [Yahoo Finance Chart](http://finance.yahoo.com/echarts?s=yhoo+Interactive#{"showEma":true,"emaColors":"#cc0000","emaPeriods":"50","emaWidths":"1","emaGhosting":"0","range":"5d","allowChartStacking":true})
+
+<p>This example makes use of a number of components, including the legend, crosshair, annotations and bands. It also includes a custom discontinuity provider for rendering an axis which excludes non-trading hours.</p>
+<p>For a detailed overview of how this chart was implemented, pop over to the Scott Logic blog which covers the details via a two-part blog series (<a href="http://blog.scottlogic.com/2015/07/08/yahoo-finance-chart.html">part one</a>, <a href="http://blog.scottlogic.com/2015/07/22/yahoo-finance-chart-part-two.html">part two</a>).</p>
