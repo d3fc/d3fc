9b1002a283cf1ffe39bd4c0997ea002774bfd131
Colin Eberhardt <colin.eberhardt@gmail.com>
2015-10-18 20:39:32 +0100

    Added an ohlc base series

--- src/series/candlestick.js
+++ src/series/candlestick.js
@@ -1,27 +1,19 @@
 import d3 from 'd3';
 import dataJoinUtil from '../util/dataJoin';
-import fractionalBarWidth from '../util/fractionalBarWidth';
 import {noop} from '../util/fn';
 import candlestickSvg from '../svg/candlestick';
+import ohlcBase from './ohlcBase';
+import {rebindAll} from '../util/rebind';
 
 export default function() {
 
     var decorate = noop,
-        xScale = d3.time.scale(),
-        yScale = d3.scale.linear(),
-        xValue = function(d, i) { return d.date; },
-        yOpenValue = function(d, i) { return d.open; },
-        yHighValue = function(d, i) { return d.high; },
-        yLowValue = function(d, i) { return d.low; },
-        yCloseValue = function(d, i) { return d.close; },
-        barWidth = fractionalBarWidth(0.75);
+        base = ohlcBase();
 
     var dataJoin = dataJoinUtil()
         .selector('g.candlestick')
         .element('g')
         .attr('class', 'candlestick');
-
-    var xValueScaled = function(d, i) { return xScale(xValue(d, i)); };
 
     var candlestick = function(selection) {
 
@@ -33,30 +25,21 @@ export default function() {
                 .append('path');
 
             var pathGenerator = candlestickSvg()
-                    .width(barWidth(data.map(xValueScaled)));
+                    .width(base.computeWidth(data));
 
             g.each(function(d, i) {
 
-                var yCloseRaw = yCloseValue(d, i),
-                    yOpenRaw = yOpenValue(d, i),
-                    x = xValueScaled(d, i),
-                    yOpen = yScale(yOpenRaw),
-                    yHigh = yScale(yHighValue(d, i)),
-                    yLow = yScale(yLowValue(d, i)),
-                    yClose = yScale(yCloseRaw);
+                var values = base.computeValues(d, i);
 
                 var g = d3.select(this)
-                    .classed({
-                        'up': yCloseRaw > yOpenRaw,
-                        'down': yCloseRaw < yOpenRaw
-                    })
-                    .attr('transform', 'translate(' + x + ', ' + yHigh + ')');
+                    .classed(values.classed, true)
+                    .attr('transform', 'translate(' + values.x + ', ' + values.yHigh + ')');
 
                 pathGenerator.x(d3.functor(0))
-                    .open(function() { return yOpen - yHigh; })
-                    .high(function() { return yHigh - yHigh; })
-                    .low(function() { return yLow - yHigh; })
-                    .close(function() { return yClose - yHigh; });
+                    .open(function() { return values.yOpen - values.yHigh; })
+                    .high(function() { return values.yHigh - values.yHigh; })
+                    .low(function() { return values.yLow - values.yHigh; })
+                    .close(function() { return values.yClose - values.yHigh; });
 
                 g.select('path')
                     .attr('d', pathGenerator([d]));
@@ -73,64 +56,9 @@ export default function() {
         decorate = x;
         return candlestick;
     };
-    candlestick.xScale = function(x) {
-        if (!arguments.length) {
-            return xScale;
-        }
-        xScale = x;
-        return candlestick;
-    };
-    candlestick.yScale = function(x) {
-        if (!arguments.length) {
-            return yScale;
-        }
-        yScale = x;
-        return candlestick;
-    };
-    candlestick.xValue = function(x) {
-        if (!arguments.length) {
-            return xValue;
-        }
-        xValue = x;
-        return candlestick;
-    };
-    candlestick.yOpenValue = function(x) {
-        if (!arguments.length) {
-            return yOpenValue;
-        }
-        yOpenValue = x;
-        return candlestick;
-    };
-    candlestick.yHighValue = function(x) {
-        if (!arguments.length) {
-            return yHighValue;
-        }
-        yHighValue = x;
-        return candlestick;
-    };
-    candlestick.yLowValue = function(x) {
-        if (!arguments.length) {
-            return yLowValue;
-        }
-        yLowValue = x;
-        return candlestick;
-    };
-    candlestick.yValue = candlestick.yCloseValue = function(x) {
-        if (!arguments.length) {
-            return yCloseValue;
-        }
-        yCloseValue = x;
-        return candlestick;
-    };
-    candlestick.barWidth = function(x) {
-        if (!arguments.length) {
-            return barWidth;
-        }
-        barWidth = d3.functor(x);
-        return candlestick;
-    };
 
     d3.rebind(candlestick, dataJoin, 'key');
+    rebindAll(candlestick, base);
 
     return candlestick;
 
---
+++ src/series/ohlcBase.js
@@ -0,0 +1,108 @@
+import d3 from 'd3';
+import fractionalBarWidth from '../util/fractionalBarWidth';
+
+export default function() {
+
+    var xScale = d3.time.scale(),
+        yScale = d3.scale.linear(),
+        xValue = function(d, i) { return d.date; },
+        yOpenValue = function(d, i) { return d.open; },
+        yHighValue = function(d, i) { return d.high; },
+        yLowValue = function(d, i) { return d.low; },
+        yCloseValue = function(d, i) { return d.close; },
+        barWidth = fractionalBarWidth(0.75),
+        xValueScaled = function(d, i) {
+            return xScale(xValue(d, i));
+        };
+
+    function base() { }
+
+    base.computeWidth = function(data) {
+        return barWidth(data.map(xValueScaled));
+    };
+
+    base.computeValues = function(d, i) {
+        var yCloseRaw = yCloseValue(d, i);
+        var yOpenRaw = yOpenValue(d, i);
+        var x = xValueScaled(d, i);
+        var yOpen = yScale(yOpenRaw);
+        var yHigh = yScale(yHighValue(d, i));
+        var yLow = yScale(yLowValue(d, i));
+        var yClose = yScale(yCloseRaw);
+        var classed = '';
+        if (yCloseRaw > yOpenRaw) {
+            classed = 'up';
+        }
+        if (yCloseRaw < yOpenRaw) {
+            classed = 'down';
+        }
+
+        return {
+            x: x,
+            yOpen: yOpen,
+            yHigh: yHigh,
+            yLow: yLow,
+            yClose: yClose,
+            classed: classed
+        };
+    };
+
+    base.xScale = function(x) {
+        if (!arguments.length) {
+            return xScale;
+        }
+        xScale = x;
+        return base;
+    };
+    base.yScale = function(x) {
+        if (!arguments.length) {
+            return yScale;
+        }
+        yScale = x;
+        return base;
+    };
+    base.xValue = function(x) {
+        if (!arguments.length) {
+            return xValue;
+        }
+        xValue = x;
+        return base;
+    };
+    base.yOpenValue = function(x) {
+        if (!arguments.length) {
+            return yOpenValue;
+        }
+        yOpenValue = x;
+        return base;
+    };
+    base.yHighValue = function(x) {
+        if (!arguments.length) {
+            return yHighValue;
+        }
+        yHighValue = x;
+        return base;
+    };
+    base.yLowValue = function(x) {
+        if (!arguments.length) {
+            return yLowValue;
+        }
+        yLowValue = x;
+        return base;
+    };
+    base.yValue = base.yCloseValue = function(x) {
+        if (!arguments.length) {
+            return yCloseValue;
+        }
+        yCloseValue = x;
+        return base;
+    };
+    base.barWidth = function(x) {
+        if (!arguments.length) {
+            return barWidth;
+        }
+        barWidth = d3.functor(x);
+        return base;
+    };
+
+    return base;
+}
