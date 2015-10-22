d99520d11b883154489ee3c967eb760094706eb9
bjedrzejewski <bjedrzejewski@scottlogic.com>
2015-10-22 14:46:42 +0100

    Fixing eslint violations

--- src/indicator/renderer/stochasticOscillator.js
+++ src/indicator/renderer/stochasticOscillator.js
@@ -4,7 +4,7 @@ import multiSeries from '../../series/multi';
 import {noop} from '../../util/fn';
 import lineSeries from '../../series/line';
 
-export default function () {
+export default function() {
 
     var xScale = d3.time.scale(),
         yScale = d3.scale.linear(),
@@ -15,21 +15,21 @@ export default function () {
 
     var annotations = lineAnnotation();
     var dLine = lineSeries()
-        .yValue(function (d, i) {
+        .yValue(function(d, i) {
             return d.stochastic.d;
         });
 
     var kLine = lineSeries()
-        .yValue(function (d, i) {
+        .yValue(function(d, i) {
             return d.stochastic.k;
         });
 
-    var stochastic = function (selection) {
+    var stochastic = function(selection) {
 
         multi.xScale(xScale)
             .yScale(yScale)
             .series([annotations, dLine, kLine])
-            .mapping(function (series) {
+            .mapping(function(series) {
                 if (series === annotations) {
                     return [
                         upperValue,
@@ -38,9 +38,9 @@ export default function () {
                 }
                 return this;
             })
-            .decorate(function (g, data, index) {
+            .decorate(function(g, data, index) {
                 g.enter()
-                    .attr('class', function (d, i) {
+                    .attr('class', function(d, i) {
                         return 'multi ' + ['annotations', 'stochastic-d', 'stochastic-k'][i];
                     });
                 decorate(g, data, index);
@@ -49,35 +49,35 @@ export default function () {
         selection.call(multi);
     };
 
-    stochastic.xScale = function (x) {
+    stochastic.xScale = function(x) {
         if (!arguments.length) {
             return xScale;
         }
         xScale = x;
         return stochastic;
     };
-    stochastic.yScale = function (x) {
+    stochastic.yScale = function(x) {
         if (!arguments.length) {
             return yScale;
         }
         yScale = x;
         return stochastic;
     };
-    stochastic.upperValue = function (x) {
+    stochastic.upperValue = function(x) {
         if (!arguments.length) {
             return upperValue;
         }
         upperValue = x;
         return stochastic;
     };
-    stochastic.lowerValue = function (x) {
+    stochastic.lowerValue = function(x) {
         if (!arguments.length) {
             return lowerValue;
         }
         lowerValue = x;
         return stochastic;
     };
-    stochastic.decorate = function (x) {
+    stochastic.decorate = function(x) {
         if (!arguments.length) {
             return decorate;
         }
