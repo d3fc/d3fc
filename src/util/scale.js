eae5243bb2e56856cb9afcd016a39bc6a042e143
Colin Eberhardt <colin.eberhardt@gmail.com>
2015-11-04 10:24:27 +0000

    Fixed an issue where ordinal scale ranges were inverted

--- src/util/scale.js
+++ src/util/scale.js
@@ -5,8 +5,30 @@ export function isOrdinal(scale) {
 // ordinal axes have a rangeExtent function, this adds any padding that
 // was applied to the range. This functions returns the rangeExtent
 // if present, or range otherwise
+///
+// NOTE: d3 uses very similar logic here:
+// https://github.com/mbostock/d3/blob/5b981a18db32938206b3579248c47205ecc94123/src/scale/scale.js#L8
 export function range(scale) {
-    return isOrdinal(scale) ? scale.rangeExtent() : scale.range();
+    // for non ordinal, simply return the range
+    if (!isOrdinal(scale)) {
+        return scale.range();
+    }
+
+    // For ordinal, use the rangeExtent. However, rangeExtent always provides
+    // a non inverted range (i.e. extent[0] < extent[1]) regardless of the
+    // range set on the scale. The logic below detects the inverted case.
+    //
+    // The d3 code that tackles the same issue doesn't have to deal with the inverted case.
+    var range = scale.range();
+    var extent = scale.rangeExtent();
+    if (range.length <= 1) {
+        // we cannot detect the inverted case if the range (and domain) has
+        // a single item in it.
+        return extent;
+    } else {
+        var inverted = range[0] > range[1];
+        return inverted ? [extent[1], extent[0]] : extent;
+    }
 }
 
 // Ordinal and quantitative scales have different methods for setting the range. This
