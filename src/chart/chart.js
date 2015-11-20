8325d8e429a9d91b8715675a09db12f86e952f2f
Colin Eberhardt <colin.eberhardt@gmail.com>
2015-11-20 10:02:01 +0000

    Removed HTML legend component

--- src/chart/chart.js
+++ src/chart/chart.js
@@ -1,12 +1,10 @@
 import cartesian from './cartesian';
-import legend from './legend';
 import tooltip from './tooltip';
 import sparkline from './sparkline';
 import smallMultiples from './smallMultiples';
 
 export default {
     cartesian: cartesian,
-    legend: legend,
     sparkline: sparkline,
     tooltip: tooltip,
     smallMultiples: smallMultiples
