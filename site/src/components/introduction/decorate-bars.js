75baec7f3797772247970f7e3842764880ae5d93
James Leftley <jleftley@scottlogic.com>
2016-02-17 22:02:26 +0000

    Fix site bar decorate example - specify correct accessors and pad x-domain

--- site/src/components/introduction/decorate-bars.js
+++ site/src/components/introduction/decorate-bars.js
@@ -8,7 +8,7 @@ var dataGenerator = fc.data.random.financial()
 var data = dataGenerator(20);
 
 var xScale = fc.scale.dateTime()
-    .domain(fc.util.extent().fields('date')(data))
+    .domain(fc.util.extent().fields('date').pad(0.1)(data))
     .range([0, width]);
 
 var yScale = d3.scale.linear()
@@ -21,6 +21,8 @@ var color = d3.scale.category10();
 var bar = fc.series.bar()
     .xScale(xScale)
     .yScale(yScale)
+    .xValue(function(d) { return d.date; })
+    .yValue(function(d) { return d.close; })
     .decorate(function(s) {
         s.enter()
           .select('.bar > path')
