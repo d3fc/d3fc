427c8bb50102cec7db879ac7aa08e08b55583e73
Colin Eberhardt <colin.eberhardt@gmail.com>
2016-02-18 17:54:32 +0000

    Improved error reporting

--- site/build/handlebars-helpers/hyperlink.js
+++ site/build/handlebars-helpers/hyperlink.js
@@ -5,11 +5,22 @@ function register(handlebars) {
         var matches = pages.filter(function(pageMetadata) {
             return pageMetadata.page.destination.endsWith('/' + link);
         });
+
+        if (!context.hash.title) {
+            throw new Error('A title was not provided for the hyperlink within page ' + context.data.root.page.path);
+        }
+
         if (matches.length === 1) {
             var title = context.hash.title;
             return '<a href=\"' + matches[0].page.destination + '\">' + title + '</a>';
         } else {
-            throw new Error('Unable to locate link ' + link + ' within page ' + context.data.root.page.path);
+            if (matches.length === 0) {
+                throw new Error('Unable to locate a page with the name ' + link + ' hyperlinked within page ' + context.data.root.page.path);
+            }
+            if (matches.length > 1) {
+                throw new Error('Multiple pages end with the name ' + link +
+                    ', expand the path in order to make the link un-ambiguous, hyperlinked within page ' + context.data.root.page.path);
+            }
         }
     });
 }
