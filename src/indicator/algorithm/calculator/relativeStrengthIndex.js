762cedcb1e2d532c5565aad90b484edaec20f812
jdhodges431 <jdhodges431@gmail.com>
2016-02-29 11:47:10 +0000

    Removed retained state between RSI calc calls. Added test to verify fix.

--- src/indicator/algorithm/calculator/relativeStrengthIndex.js
+++ src/indicator/algorithm/calculator/relativeStrengthIndex.js
@@ -3,53 +3,52 @@ import _slidingWindow from './slidingWindow';
 
 export default function() {
 
-    var closeValue = function(d, i) { return d.close; },
+    var windowSize = 14,
+        closeValue = function(d, i) { return d.close; },
         wildersSmoothing = function(values, prevAvg) {
-            var result = prevAvg + ((values[values.length - 1] - prevAvg) / values.length);
-            return result;
+            return prevAvg + ((values[values.length - 1] - prevAvg) / values.length);
         },
-        sum = function(a, b) { return a + b; },
-        prevClose,
-        prevDownChangesAvg,
-        prevUpChangesAvg;
-
-    var slidingWindow = _slidingWindow()
-        .windowSize(14)
-        .accumulator(function(values) {
-            var closes = values.map(closeValue);
-
-            if (!prevClose) {
-                prevClose = closes[0];
-                return undefined;
-            }
-
-            var downChanges = [];
-            var upChanges = [];
-
-            closes.forEach(function(close) {
-                var downChange = prevClose > close ? prevClose - close : 0;
-                var upChange = prevClose < close ? close - prevClose : 0;
-
-                downChanges.push(downChange);
-                upChanges.push(upChange);
-
-                prevClose = close;
-            });
-
-            var downChangesAvg = prevDownChangesAvg ? wildersSmoothing(downChanges, prevDownChangesAvg) :
-                downChanges.reduce(sum) / closes.length;
-
-            var upChangesAvg = prevUpChangesAvg ? wildersSmoothing(upChanges, prevUpChangesAvg) :
-                upChanges.reduce(sum) / closes.length;
-
-            prevDownChangesAvg = downChangesAvg;
-            prevUpChangesAvg = upChangesAvg;
-
-            var rs = upChangesAvg / downChangesAvg;
-            return 100 - (100 / (1 + rs));
-        });
+        sum = function(a, b) { return a + b; };
 
     var rsi = function(data) {
+        var prevClose, prevDownChangesAvg, prevUpChangesAvg;
+
+        var slidingWindow = _slidingWindow()
+            .windowSize(windowSize)
+            .accumulator(function(values) {
+                var closes = values.map(closeValue);
+
+                if (!prevClose) {
+                    prevClose = closes[0];
+                    return undefined;
+                }
+
+                var downChanges = [];
+                var upChanges = [];
+
+                closes.forEach(function(close) {
+                    var downChange = prevClose > close ? prevClose - close : 0;
+                    var upChange = prevClose < close ? close - prevClose : 0;
+
+                    downChanges.push(downChange);
+                    upChanges.push(upChange);
+
+                    prevClose = close;
+                });
+
+                var downChangesAvg = prevDownChangesAvg ? wildersSmoothing(downChanges, prevDownChangesAvg) :
+                downChanges.reduce(sum) / closes.length;
+
+                var upChangesAvg = prevUpChangesAvg ? wildersSmoothing(upChanges, prevUpChangesAvg) :
+                upChanges.reduce(sum) / closes.length;
+
+                prevDownChangesAvg = downChangesAvg;
+                prevUpChangesAvg = upChangesAvg;
+
+                var rs = upChangesAvg / downChangesAvg;
+                return 100 - (100 / (1 + rs));
+            });
+
         return slidingWindow(data);
     };
 
@@ -61,7 +60,13 @@ export default function() {
         return rsi;
     };
 
-    d3.rebind(rsi, slidingWindow, 'windowSize');
+    rsi.windowSize = function(x) {
+        if (!arguments.length) {
+            return windowSize;
+        }
+        windowSize = x;
+        return rsi;
+    };
 
     return rsi;
 }
