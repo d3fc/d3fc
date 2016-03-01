cc7263221f4399e5f1d9d06c7b6772388840f320
Ali Sheehan-Dare <alisd23@live.co.uk>
2016-03-01 09:53:36 +0000

    Add minified files

--- packages/d3fc-shape/build/d3fc-path.js
+++ packages/d3fc-shape/build/d3fc-path.js
@@ -112,8 +112,116 @@ function ohlc () {
     return ohlc;
 }
 
+// Renders a bar series as an SVG path based on the given array of datapoints. Each
+// bar has a fixed width, whilst the x, y and height are obtained from each data
+// point via the supplied accessor functions.
+function bar () {
+
+    var x = function x(d, i) {
+        return d.x;
+    },
+        y = function y(d, i) {
+        return d.y;
+    },
+        horizontalAlign = 'center',
+        verticalAlign = 'center',
+        height = function height(d, i) {
+        return d.height;
+    },
+        width = d3.functor(3);
+
+    var bar = function bar(context, data, index) {
+
+        data.forEach(function (d, i) {
+            var xValue = x.call(this, d, index || i),
+                yValue = y.call(this, d, index || i),
+                barHeight = height.call(this, d, index || i),
+                barWidth = width.call(this, d, index || i);
+
+            var horizontalOffset;
+            switch (horizontalAlign) {
+                case 'left':
+                    horizontalOffset = barWidth;
+                    break;
+                case 'right':
+                    horizontalOffset = 0;
+                    break;
+                case 'center':
+                    horizontalOffset = barWidth / 2;
+                    break;
+                default:
+                    throw new Error('Invalid horizontal alignment ' + horizontalAlign);
+            }
+
+            var verticalOffset;
+            switch (verticalAlign) {
+                case 'bottom':
+                    verticalOffset = -barHeight;
+                    break;
+                case 'top':
+                    verticalOffset = 0;
+                    break;
+                case 'center':
+                    verticalOffset = barHeight / 2;
+                    break;
+                default:
+                    throw new Error('Invalid vertical alignment ' + verticalAlign);
+            }
+
+            context.rect(xValue - horizontalOffset, yValue - verticalOffset, barWidth, barHeight);
+        }, this);
+
+        return context.toString();
+    };
+
+    bar.x = function (_x) {
+        if (!arguments.length) {
+            return x;
+        }
+        x = d3.functor(_x);
+        return bar;
+    };
+    bar.y = function (_x) {
+        if (!arguments.length) {
+            return y;
+        }
+        y = d3.functor(_x);
+        return bar;
+    };
+    bar.width = function (_x) {
+        if (!arguments.length) {
+            return width;
+        }
+        width = d3.functor(_x);
+        return bar;
+    };
+    bar.horizontalAlign = function (_x) {
+        if (!arguments.length) {
+            return horizontalAlign;
+        }
+        horizontalAlign = _x;
+        return bar;
+    };
+    bar.height = function (_x) {
+        if (!arguments.length) {
+            return height;
+        }
+        height = d3.functor(_x);
+        return bar;
+    };
+    bar.verticalAlign = function (_x) {
+        if (!arguments.length) {
+            return verticalAlign;
+        }
+        verticalAlign = _x;
+        return bar;
+    };
+    return bar;
+}
+
 var index = {
-  ohlc: ohlc
+  ohlc: ohlc,
+  bar: bar
 };
 
 module.exports = index;
