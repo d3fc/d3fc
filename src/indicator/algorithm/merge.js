50cc4643bc599475b2fd2744e61fbccb9fe2923f
jdhodges431 <jdhodges431@gmail.com>
2016-04-25 10:50:38 +0100

    feat: allow functional usage of merge

--- src/indicator/algorithm/merge.js
+++ src/indicator/algorithm/merge.js
@@ -2,8 +2,7 @@ import calculator from './calculator/slidingWindow';
 import d3 from 'd3';
 import {noop} from '../../util/fn';
 
-// applies an algorithm to an array, merging the result back into
-// the source array using the given merge function.
+// applies an algorithm to an array and merges the result using the given merge function.
 export default function() {
 
     var merge = noop,
@@ -11,8 +10,8 @@ export default function() {
 
     var mergeCompute = function(data) {
         return d3.zip(data, algorithm(data))
-            .forEach(function(tuple) {
-                merge(tuple[0], tuple[1]);
+            .map(function(tuple) {
+                return merge(tuple[0], tuple[1]);
             });
     };
 
