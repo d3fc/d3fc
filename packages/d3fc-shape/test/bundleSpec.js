656dfbac756f1a8908f464649851af4ebccf0b01
Colin Eberhardt <colin.eberhardt@gmail.com>
2016-07-15 08:31:07 +0100

    test: added a test to verify the bundle

---
+++ packages/d3fc-shape/test/bundleSpec.js
@@ -0,0 +1,21 @@
+import jsdom from 'jsdom';
+
+describe('bundle', function() {
+    it('should corectly wire-up all the dependencies via their UMD-exposed globals', function(done) {
+        jsdom.env({
+            html: '<html></html>',
+            virtualConsole: jsdom.createVirtualConsole().sendTo(console),
+            scripts: [
+                './node_modules/d3-path/build/d3-path.js',
+                './build/d3fc-shape.js'
+            ],
+            done: (_, win) => {
+                const shape = win.fc.bar();
+                const data = [{x: 10, y: 10, height: 22}];
+                const result = shape(data);
+                expect(result).not.toBeUndefined();
+                done();
+            }
+        });
+    });
+});
