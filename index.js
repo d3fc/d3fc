92f9ea6735b93e761597ce6fe1fb90eedef44640
Ali Sheehan-Dare <alisd23@live.co.uk>
2016-07-25 11:35:32 +0100

    chore/more-setup

---
+++ index.js
@@ -0,0 +1,12 @@
+// Can' use export * from 'package' with rollup for some reason at the moment
+// See https://github.com/rollup/rollup/issues/791
+export * from './node_modules/d3fc-technical-indicator/index';
+export * from './node_modules/d3fc-discontinuous-scale/index';
+export * from './node_modules/d3fc-extent/index';
+export * from './node_modules/d3fc-random-data/index';
+export * from './node_modules/d3fc-financial-feed/index';
+export * from './node_modules/d3fc-sample/index';
+export * from './node_modules/d3fc-rebind/index';
+export * from './node_modules/d3fc-shape/index';
+export * from './node_modules/d3fc-label-layout/index';
+export * from './node_modules/d3fc-data-join/index';
---
+++ rollup.config.js
@@ -0,0 +1,15 @@
+import babel from 'rollup-plugin-babel';
+
+export default {
+    entry: 'index.js',
+    moduleName: 'fc',
+    format: 'umd',
+    plugins: [
+        babel({
+            babelrc: false,
+            presets: ['es2015-rollup']
+        })
+    ],
+    dest: 'build/d3fc.js',
+    globals: { }
+};
