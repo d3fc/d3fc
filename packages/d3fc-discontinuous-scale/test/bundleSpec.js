c87e9eb7f3f13d30b48b908eaf0eaae1fe3c9991
Chris Price <price.c@gmail.com>
2016-08-11 16:51:59 +0100

    chore: integrate d3fc-scripts (#8)

--- packages/d3fc-discontinuous-scale/test/bundleSpec.js
+++ packages/d3fc-discontinuous-scale/test/bundleSpec.js
@@ -7,12 +7,12 @@ describe('bundle', function() {
             virtualConsole: jsdom.createVirtualConsole().sendTo(console),
             scripts: [
                 // transitive dependencies
-                './node_modules/d3-scale/node_modules/d3-array/build/d3-array.js',
-                './node_modules/d3-scale/node_modules/d3-collection/build/d3-collection.js',
-                './node_modules/d3-scale/node_modules/d3-color/build/d3-color.js',
-                './node_modules/d3-scale/node_modules/d3-format/build/d3-format.js',
-                './node_modules/d3-scale/node_modules/d3-interpolate/build/d3-interpolate.js',
-                './node_modules/d3-scale/node_modules/d3-time-format/build/d3-time-format.js',
+                './node_modules/d3-array/build/d3-array.js',
+                './node_modules/d3-collection/build/d3-collection.js',
+                './node_modules/d3-color/build/d3-color.js',
+                './node_modules/d3-format/build/d3-format.js',
+                './node_modules/d3-interpolate/build/d3-interpolate.js',
+                './node_modules/d3-time-format/build/d3-time-format.js',
                 // direct dependencies
                 './node_modules/d3-scale/build/d3-scale.js',
                 './node_modules/d3-time/build/d3-time.js',
