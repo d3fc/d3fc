0881c943974fa4d90cfe97b64ce9cfa6a4e566d3
Chris Price <price.c@gmail.com>
2016-08-12 14:05:28 +0100

    refactor: remove duplicated code

--- packages/d3fc-rebind/src/rebindAll.js
+++ packages/d3fc-rebind/src/rebindAll.js
@@ -1,19 +1,10 @@
+import createReboundMethod from './createReboundMethod';
+
 const createTransform = (transforms) =>
     (name) => transforms.reduce(
         (name, fn) => name && fn(name),
         name
     );
-
-const createReboundMethod = (target, source, name) => {
-    const method = source[name];
-    if (typeof method !== 'function') {
-        throw new Error(`Attempt to rebind ${name} which isn't a function on the source object`);
-    }
-    return (...args) => {
-        var value = method.apply(source, args);
-        return value === source ? target : value;
-    };
-};
 
 export default (target, source, ...transforms) => {
     const transform = createTransform(transforms);
