cae9bba024644b42ab2c1e4c86c1bee376611090
Ali Sheehan-Dare <alisd23@live.co.uk>
2016-09-14 15:26:03 +0100

    feat: add node resolution

--- packages/d3fc-scripts/scripts/bundle.js
+++ packages/d3fc-scripts/scripts/bundle.js
@@ -1,15 +1,34 @@
 var babel = require('rollup-plugin-babel');
 var fs = require('fs');
-var globals = require('./util/globals');
 var mkdirp = require('mkdirp');
 var packageJson = JSON.parse(fs.readFileSync('package.json', 'utf8'));
 var rimraf = require('rimraf');
 var rollup = require('rollup');
 var uglifyJS = require('uglify-js');
+var nodeResolve = require('rollup-plugin-node-resolve');
+var commandLineArgs = require('command-line-args');
 
 rimraf.sync('build');
 
 mkdirp.sync('build');
+
+var optionDefinitions = [
+  { name: 'include-d3fc', type: Boolean, defaultOption: false }
+];
+var options = commandLineArgs(optionDefinitions);
+
+var external = function(key) {
+    return (key.indexOf('d3-') === 0) ||
+      (!options['include-d3fc'] && key.indexOf('d3fc-') === 0);
+};
+var globals = function(key) {
+    if (key.indexOf('d3-') === 0) {
+        return 'd3';
+    }
+    if (key.indexOf('d3fc-') === 0) {
+        return 'fc';
+    }
+};
 
 module.exports = rollup.rollup({
     entry: 'index.js',
@@ -17,14 +36,16 @@ module.exports = rollup.rollup({
         babel({
             babelrc: false,
             presets: ['es2015-rollup']
-        })
-    ]
+        }),
+        nodeResolve({ jsnext: true, main: true })
+    ],
+    external: external
 })
   .then(bundle =>
       bundle.write({
           dest: `build/${packageJson.name}.js`,
           format: 'umd',
-          globals: globals(),
+          globals: globals,
           moduleName: 'fc'
       })
   )
