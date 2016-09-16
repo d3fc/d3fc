347019b055456059444a5668c4f0adde43e01f35
Ali Sheehan-Dare <alisd23@live.co.uk>
2016-09-16 10:22:00 +0100

    feat: add examples folder to eslint glob (#18)

--- packages/d3fc-scripts/scripts/test.js
+++ packages/d3fc-scripts/scripts/test.js
@@ -5,7 +5,7 @@ var path = require('path');
 var linter = new ESLint({
     configFile: path.join(__dirname, '..', '.eslintrc.json')
 });
-var linterReport = linter.executeOnFiles(['index.js', 'src/**/*.js', 'test/**/*.js']);
+var linterReport = linter.executeOnFiles(['index.js', 'src/**/*.js', 'test/**/*.js', 'examples/**/*.js']);
 if (linterReport.warningCount > 0 || linterReport.errorCount > 0) {
     console.log(linter.getFormatter()(linterReport.results));
     throw new Error('ESLint errors');
