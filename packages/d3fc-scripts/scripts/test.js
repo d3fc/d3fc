dacc18a8a5b0d3869525f7bc2bf994b558de358a
Ali Sheehan-Dare <alisd23@live.co.uk>
2016-09-26 11:28:02 +0100

    feat: add required files check to test (#23)

--- packages/d3fc-scripts/scripts/test.js
+++ packages/d3fc-scripts/scripts/test.js
@@ -1,16 +1,63 @@
-var ESLint = require('eslint').CLIEngine;
-var Jasmine = require('jasmine');
-var path = require('path');
+const ESLint = require('eslint').CLIEngine;
+const Jasmine = require('jasmine');
+const path = require('path');
+const glob = require('glob');
+const fs = require('fs');
 
-var linter = new ESLint({
+// ====
+// LINTER
+// ====
+const linter = new ESLint({
     configFile: path.join(__dirname, '..', '.eslintrc.json')
 });
-var linterReport = linter.executeOnFiles(['index.js', 'src/**/*.js', 'test/**/*.js', 'examples/**/*.js']);
+const linterReport = linter.executeOnFiles(['index.js', 'src/**/*.js', 'test/**/*.js', 'examples/**/*.js']);
 if (linterReport.warningCount > 0 || linterReport.errorCount > 0) {
     console.log(linter.getFormatter()(linterReport.results));
     throw new Error('ESLint errors');
 }
 
+const required = (pathname) => path.resolve(__dirname, '../required', pathname || '');
+const root = (pathname) => path.resolve(process.cwd(), pathname);
+
+// ====
+// REQUIRED FILES CHECK
+// ====
+const globOptions = {
+    cwd: required(),
+    dot: true
+};
+
+glob.sync('*', globOptions, (err, files) => {
+    if (err) {
+        throw new Error('Required files check failed - ', err);
+    }
+    const passed = files.every(filename => {
+        try {
+            const candidateContents = fs.readFileSync(root(filename));
+            const requiredContents = fs.readFileSync(required(filename));
+
+            if (!candidateContents.equals(requiredContents)) {
+                console.log(filename, '\t\t', 'INCORRECT CONTENTS');
+                return false;
+            }
+            console.log(filename, '\t\t', 'OK');
+            return true;
+        } catch (e) {
+            console.log(filename, '\t\t', 'NO FILE');
+            return true;
+        }
+    });
+
+    if (!passed) {
+        throw new Error('Required files are not correct - check https://github.com/d3fc/d3fc-scripts/required');
+    } else {
+        console.log(`\n${files.length}/${files.length} required files OK\n`);
+    }
+});
+
+// ====
+// TESTS
+// ====
 require('./bundle')
   .then(() => {
       var jasmine = new Jasmine();
