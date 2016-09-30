88e03bc6a4a25efc616be69fa93890c7a5247b11
Chris Price <price.c@gmail.com>
2016-09-30 16:32:31 +0100

    feat: include new packages
    Also removes hack for rollup/rollup#791

--- index.js
+++ index.js
@@ -1,12 +1,15 @@
-// Can' use export * from 'package' with rollup for some reason at the moment
-// See https://github.com/rollup/rollup/issues/791
-export * from './node_modules/d3fc-technical-indicator/index';
-export * from './node_modules/d3fc-discontinuous-scale/index';
-export * from './node_modules/d3fc-extent/index';
-export * from './node_modules/d3fc-random-data/index';
-export * from './node_modules/d3fc-financial-feed/index';
-export * from './node_modules/d3fc-sample/index';
-export * from './node_modules/d3fc-rebind/index';
-export * from './node_modules/d3fc-shape/index';
-export * from './node_modules/d3fc-label-layout/index';
-export * from './node_modules/d3fc-data-join/index';
+export * from 'd3fc-technical-indicator';
+export * from 'd3fc-discontinuous-scale';
+export * from 'd3fc-extent';
+export * from 'd3fc-random-data';
+export * from 'd3fc-financial-feed';
+export * from 'd3fc-sample';
+export * from 'd3fc-rebind';
+export * from 'd3fc-shape';
+export * from 'd3fc-label-layout';
+export * from 'd3fc-data-join';
+export * from 'd3fc-series';
+export * from 'd3fc-annotation';
+export * from 'd3fc-axis';
+export * from 'd3fc-element';
+export * from 'd3fc-pointer';
