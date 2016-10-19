ea60bc6c6da02229bf45f779926743d01e4b94d5
Chris Price <price.c@gmail.com>
2016-10-19 10:26:20 +0100

    fix: don't force position: relative

--- packages/d3fc-element/src/element.js
+++ packages/d3fc-element/src/element.js
@@ -5,12 +5,11 @@ import requestRedraw from './requestRedraw';
 const init = (instance, node) => {
     instance.__node__ = node;
     // Fill parent but allow parent to shrink
-    instance.style.position = 'relative';
+    if (instance.__node__.offsetParent !== instance) {
+        instance.style.position = 'relative';
+    }
     instance.__node__.style.position = 'absolute';
-    instance.__node__.style.top = '0';
-    instance.__node__.style.right = '0';
-    instance.__node__.style.bottom = '0';
-    instance.__node__.style.left = '0';
+    instance.__node__.style.top = instance.__node__.style.right = instance.__node__.style.bottom = instance.__node__.style.left = 0;
 };
 
 export default (createNode) => class extends HTMLElement {
