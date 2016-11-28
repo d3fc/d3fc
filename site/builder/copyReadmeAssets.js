a875ba5f4f0b389faed02665fc2c6fc5fba213a3
Martinho Santos <lamppostman@gmail.com>
2016-11-28 09:22:29 +0000

    docs: copy json and csv files from d3fc.io and add them to build (#109)

--- site/builder/copyReadmeAssets.js
+++ site/builder/copyReadmeAssets.js
@@ -4,35 +4,37 @@ import glob from 'glob';
 import ensureExists from './ensureExists';
 
 const root = path.resolve(__dirname, '../../');
+const distFolder = path.resolve(root, '');
 const apiDistFolder = path.resolve(root, 'site/dist/api');
-const imageGlob = 'node_modules/d3fc-*/**/*.png';
-
+const getDataFilename = (pathname) => path.join(distFolder, pathname.replace(new RegExp('site/src/'), 'site/dist/'));
 const getImageFilename = (pathname) => path.join(apiDistFolder, pathname.replace(new RegExp('node_modules/'), ''));
 
-function createFile(path, buffer) {
-  const pathname = getImageFilename(path);
-  return ensureExists(pathname).then(() => fs.writeFile(pathname, buffer));
-}
+const globs = [
+  { src: 'node_modules/d3fc-*/**/*.png', dest: getImageFilename },
+  { src: 'site/src/examples/**/*.{csv,json}', dest: getDataFilename }
+];
 
 export default (data) =>
   new Promise((resolve, reject) => {
-    glob(imageGlob, { cwd: root }, (err, files) => {
-      if (err) {
-        console.error('Finding README Images failed - ', err);
-        reject(err);
-      }
+    globs.forEach(entry => {
+      glob(entry.src, { cwd: root }, (err, files) => {
+        if (err) {
+          console.error('Finding README Images failed - ', err);
+          reject(err);
+        }
 
-      const readPromises = files.map(filename => {
-        const filepath = path.join(root, filename);
-        return fs
-          .readFile(filepath)
-          .then(buffer => createFile(filename, buffer));
+        const readPromises = files.map(filename => {
+          const filepath = path.join(root, filename);
+          const fileDestination = entry.dest(filename);
+          return fs
+            .readFile(filepath)
+            .then(buffer => ensureExists(fileDestination).then(() => fs.writeFile(fileDestination, buffer)));
+        });
+
+        return Promise
+          .all(readPromises)
+          .then(() => resolve(data))
+          .catch(reject);
       });
-
-      return Promise
-        .all(readPromises)
-        .then(() => resolve(data))
-        .catch(reject);
     });
-
   });
