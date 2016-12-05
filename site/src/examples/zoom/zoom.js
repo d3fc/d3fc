002d88907eccd292c6b3dad0eba32d0b45458b4e
Colin Eberhardt <colin.eberhardt@gmail.com>
2016-12-06 11:12:07 +1300

    docs: changed the zoom example to use canvas (#1037)

--- site/src/examples/zoom/zoom.js
+++ site/src/examples/zoom/zoom.js
@@ -1,71 +1,50 @@
 // create some test data
-var generator = fc.randomGeometricBrownianMotion()
-    .steps(1000);
-var data = generator(10);
+var data = d3.range(10000).map(function(d) {
+  return {
+    x: Math.random(),
+    y: Math.random()
+  };
+});
 
-var yExtent = fc.extentLinear()
-  .pad([0.1, 0.1]);
+var x = d3.scaleLinear();
+var y = d3.scaleLinear();
 
-// create the scales
-var x = d3.scaleLinear()
-  .domain([0, data.length]);
-var y = d3.scaleLinear()
-  .domain(yExtent(data));
+// secondary scales for the purposes of computing the zoom
+var x2 = d3.scaleLinear();
+var y2 = d3.scaleLinear();
 
-// a secondary x-scale, which is always set to the full domain
-// extent, for the purposes of computing the zoom
-var x2 = x.copy();
-
-// create the various series
-var area = fc.seriesSvgArea()
-  .crossValue(function(d, i) { return i; })
-  .mainValue(function(d) { return d; })
-  .decorate(function(selection) {
-    selection.enter()
-      .style('fill', 'lightgreen')
-      .style('fill-opacity', 0.5);
-  });
-
-var gridlines = fc.annotationSvgGridline();
-
-var multi = fc.seriesSvgMulti()
-  .series([gridlines, area]);
+var area = fc.seriesCanvasPoint()
+  .crossValue(function(d) { return d.x; })
+  .mainValue(function(d) { return d.y; })
+  .size(4);
 
 // create a d3-zoom that handles the mouse / touch interactions
 var zoom = d3.zoom()
   .on('zoom', function() {
-    // use the rescaleX utility function to compute the new scale
-    var rescaled = d3.event.transform.rescaleX(x2);
-    // update the x scale used by the chart to use the udpated domain
-    x.domain(rescaled.domain());
+    // update the scale used by the chart to use the udpated domain
+    x.domain(d3.event.transform.rescaleX(x2).domain());
+    y.domain(d3.event.transform.rescaleY(y2).domain());
     render();
   });
 
 // the chart!
-var chart = fc.chartSvgCartesian(x, y)
-  .chartLabel('d3-zoom integration example')
-  .plotArea(multi)
+var chart = fc.chartCanvasCartesian(x, y)
+  .chartLabel('Canvas Zoom 10,000 Points')
+  .plotArea(area)
   .decorate((sel) => {
     // add the zoom interaction on the enter selection
+    // NOTE: there is a much better zoom integration being developed
+    // for d3fc!!!
     sel.enter()
       .select('.plot-area')
       .on('measure.range', () => {
         x2.range([0, d3.event.detail.width]);
+        y2.range([d3.event.detail.height, 0]);
       })
-      .call(zoom)
-      // initialise the zoom at x10
-      .call(zoom.transform, d3.zoomIdentity.scale(10));
+      .call(zoom);
   });
 
 function render() {
-  // update the y-domain based on the visible range
-  var visibleRange = data.slice(
-    Math.max(0, x.domain()[0]),
-    Math.min(x.domain()[1], data.length)
-  );
-  y.domain(yExtent(visibleRange));
-
-  // render the chart!
   d3.select('#zoom-chart')
     .datum(data)
     .call(chart);
