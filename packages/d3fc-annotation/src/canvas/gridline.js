c1980050e085c78e934b6c19ea3e84a66c793eb8
Ro4052 <Ro4052@users.noreply.github.com>
2019-03-20 12:26:08 +0000

    fix: switch decorates to match svg version

--- packages/d3fc-annotation/src/canvas/gridline.js
+++ packages/d3fc-annotation/src/canvas/gridline.js
@@ -24,7 +24,7 @@ export default () => {
             context.strokeStyle = '#bbb';
             context.fillStyle = 'transparent';
 
-            yDecorate(context);
+            xDecorate(context);
             lineData.context(context)(yScale.domain().map(d => [xScale(xTick), yScale(d)]));
 
             context.fill();
@@ -39,7 +39,7 @@ export default () => {
             context.strokeStyle = '#bbb';
             context.fillStyle = 'transparent';
 
-            xDecorate(context);
+            yDecorate(context);
             lineData.context(context)(xScale.domain().map(d => [xScale(d), yScale(yTick)]));
 
             context.fill();
