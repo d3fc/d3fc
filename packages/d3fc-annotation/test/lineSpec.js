77a1b113e83c4a641c4dda34b0ebdcc523cba4ce
Ro4052 <Ro4052@users.noreply.github.com>
2019-04-02 12:01:46 +0100

    test: added tests for line annotations

--- packages/d3fc-annotation/test/lineSpec.js
+++ packages/d3fc-annotation/test/lineSpec.js
@@ -1,46 +1,78 @@
 import { select } from 'd3-selection';
 import { scaleLinear, scaleOrdinal } from 'd3-scale';
-import annotationLine from '../src/svg/line';
+import { createCanvas } from 'canvas';
+import annotationSvgLine from '../src/svg/line';
+import annotationCanvasLine from '../src/canvas/line';
 
 describe('line', () => {
 
     let element;
+    let context;
     let container;
 
     beforeEach(() => {
         element = document.createElement('svg');
+        context = createCanvas().getContext('2d');
         container = select(element);
     });
 
-    it('should work with continuous scales', () => {
+    it('should work with continuous scales for svg', () => {
         const xScale = scaleLinear()
             .range([0, 100]);
 
         const yScale = scaleLinear()
             .range([0, 100]);
 
-        const line = annotationLine()
+        const svgLine = annotationSvgLine()
             .xScale(xScale)
             .yScale(yScale);
 
         select('svg')
             .datum([30])
-            .call(line);
+            .call(svgLine);
     });
 
-    it('should work with ordinal scales', () => {
+    it('should work with continuous scales for canvas', () => {
+        const xScale = scaleLinear()
+            .range([0, 100]);
+
+        const yScale = scaleLinear()
+            .range([0, 100]);
+
+        const canvasLine = annotationCanvasLine()
+            .xScale(xScale)
+            .yScale(yScale);
+
+        canvasLine.context(context)([30]);
+    });
+
+    it('should work with ordinal scales for svg', () => {
         const xScale = scaleOrdinal()
             .range([0, 100]);
 
         const yScale = scaleOrdinal()
             .range([0, 100]);
 
-        const line = annotationLine()
+        const svgLine = annotationSvgLine()
             .xScale(xScale)
             .yScale(yScale);
 
         select('svg')
             .datum([30])
-            .call(line);
+            .call(svgLine);
+    });
+
+    it('should work with ordinal scales for canvas', () => {
+        const xScale = scaleOrdinal()
+            .range([0, 100]);
+
+        const yScale = scaleOrdinal()
+            .range([0, 100]);
+
+        const canvasLine = annotationCanvasLine()
+            .xScale(xScale)
+            .yScale(yScale);
+
+        canvasLine.context(context)([30]);
     });
 });
