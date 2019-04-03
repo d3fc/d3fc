17230ecd26e121b30befaa5176ca54ab5c25fea0
Ro4052 <Ro4052@users.noreply.github.com>
2019-04-03 08:32:42 +0100

    test: added canvas band tests

--- packages/d3fc-annotation/test/bandSpec.js
+++ packages/d3fc-annotation/test/bandSpec.js
@@ -1,46 +1,78 @@
 import { select } from 'd3-selection';
 import { scaleLinear, scaleOrdinal } from 'd3-scale';
-import annotationBand from '../src/svg/band';
+import { createCanvas } from 'canvas';
+import annotationSvgBand from '../src/svg/band';
+import annotationCanvasBand from '../src/canvas/band';
 
 describe('band', () => {
 
     let element;
+    let context;
     let container;
 
     beforeEach(() => {
         element = document.createElement('svg');
+        context = createCanvas().getContext('2d');
         container = select(element);
     });
 
-    it('should work with continuous scales', () => {
+    it('should work with continuous scales for svg', () => {
         const xScale = scaleLinear()
             .range([0, 100]);
 
         const yScale = scaleLinear()
             .range([0, 100]);
 
-        const band = annotationBand()
+        const svgBand = annotationSvgBand()
             .xScale(xScale)
             .yScale(yScale);
 
         select('svg')
             .datum([{ from: 10, to: 90 }])
-            .call(band);
+            .call(svgBand);
     });
 
-    it('should work with ordinal scales', () => {
+    it('should work with continuous scales for canvas', () => {
+        const xScale = scaleLinear()
+            .range([0, 100]);
+
+        const yScale = scaleLinear()
+            .range([0, 100]);
+
+        const canvasBand = annotationCanvasBand()
+            .xScale(xScale)
+            .yScale(yScale);
+
+        canvasBand.context(context)([{ from: 10, to: 90 }]);
+    });
+
+    it('should work with ordinal scales for svg', () => {
         const xScale = scaleOrdinal()
             .range([0, 100]);
 
         const yScale = scaleOrdinal()
             .range([0, 100]);
 
-        const band = annotationBand()
+        const svgBand = annotationSvgBand()
             .xScale(xScale)
             .yScale(yScale);
 
         select('svg')
             .datum([{ from: 10, to: 90 }])
-            .call(band);
+            .call(svgBand);
+    });
+
+    it('should work with ordinal scales for canvas', () => {
+        const xScale = scaleOrdinal()
+            .range([0, 100]);
+
+        const yScale = scaleOrdinal()
+            .range([0, 100]);
+
+        const canvasBand = annotationCanvasBand()
+            .xScale(xScale)
+            .yScale(yScale);
+
+        canvasBand.context(context)([{ from: 10, to: 90 }]);
     });
 });
