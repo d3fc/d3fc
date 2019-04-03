4faef000b4d72fa3132b30ad4e834f2eb6dd6a8d
Ro4052 <Ro4052@users.noreply.github.com>
2019-04-03 11:39:48 +0100

    test: added canvas crosshair tests

--- packages/d3fc-annotation/test/crosshairSpec.js
+++ packages/d3fc-annotation/test/crosshairSpec.js
@@ -1,46 +1,78 @@
 import { select } from 'd3-selection';
 import { scaleLinear, scaleOrdinal } from 'd3-scale';
-import annotationCrosshair from '../src/svg/crosshair';
+import { createCanvas } from 'canvas';
+import annotationSvgCrosshair from '../src/svg/crosshair';
+import annotationCanvasCrosshair from '../src/canvas/crosshair';
 
 describe('crosshair', () => {
 
     let element;
+    let context;
     let container;
 
     beforeEach(() => {
         element = document.createElement('svg');
+        context = createCanvas().getContext('2d');
         container = select(element);
     });
 
-    it('should work with continuous scales', () => {
+    it('should work with continuous scales for svg', () => {
         const xScale = scaleLinear()
             .range([0, 100]);
 
         const yScale = scaleLinear()
             .range([0, 100]);
 
-        const crosshair = annotationCrosshair()
+        const svgCrosshair = annotationSvgCrosshair()
             .xScale(xScale)
             .yScale(yScale);
 
         select('svg')
             .datum([{ x: 50, y: 50 }])
-            .call(crosshair);
+            .call(svgCrosshair);
     });
 
-    it('should work with ordinal scales', () => {
+    it('should work with continuous scales for canvas', () => {
+        const xScale = scaleLinear()
+            .range([0, 100]);
+
+        const yScale = scaleLinear()
+            .range([0, 100]);
+
+        const canvasCrosshair = annotationCanvasCrosshair()
+            .xScale(xScale)
+            .yScale(yScale);
+
+        canvasCrosshair.context(context)([{ x: 50, y: 50 }]);
+    });
+
+    it('should work with ordinal scales for svg', () => {
         const xScale = scaleOrdinal()
             .range([0, 100]);
 
         const yScale = scaleOrdinal()
             .range([0, 100]);
 
-        const crosshair = annotationCrosshair()
+        const svgCrosshair = annotationSvgCrosshair()
             .xScale(xScale)
             .yScale(yScale);
 
         select('svg')
             .datum([{ x: 50, y: 50 }])
-            .call(crosshair);
+            .call(svgCrosshair);
+    });
+
+    it('should work with ordinal scales for canvas', () => {
+        const xScale = scaleOrdinal()
+            .range([0, 100]);
+
+        const yScale = scaleOrdinal()
+            .range([0, 100]);
+
+        const canvasCrosshair = annotationCanvasCrosshair()
+            .xScale(xScale)
+            .yScale(yScale);
+
+        canvasCrosshair.context(context)([{ x: 50, y: 50 }]);
     });
 });
