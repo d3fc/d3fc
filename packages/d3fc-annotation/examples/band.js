12100c8c69a6754b25c160449fbabd43332f4129
Ro4052 <Ro4052@users.noreply.github.com>
2019-04-03 08:28:00 +0100

    fix: example linting

--- packages/d3fc-annotation/examples/band.js
+++ packages/d3fc-annotation/examples/band.js
@@ -17,7 +17,7 @@ var horizontalCanvasBand = fc.annotationCanvasBand()
   .yScale(yScale)
   .fromValue(function(d) { return d[0]; })
   .toValue(function(d) { return d[1]; })
-  .decorate(context => { context.fillStyle = "rgba(102, 0, 204, 0.1)"; });
+  .decorate(context => { context.fillStyle = 'rgba(102, 0, 204, 0.1)'; });
 
 var verticalCanvasBand = fc.annotationCanvasBand()
   .orient('vertical')
@@ -25,7 +25,7 @@ var verticalCanvasBand = fc.annotationCanvasBand()
   .yScale(yScale)
   .fromValue(function(d) { return d[0]; })
   .toValue(function(d) { return d[1]; })
-  .decorate(context => { context.fillStyle = "rgba(0, 204, 0, 0.1)"; });
+  .decorate(context => { context.fillStyle = 'rgba(0, 204, 0, 0.1)'; });
 
 // eslint-disable-next-line no-unused-vars
 function renderComponent() {
