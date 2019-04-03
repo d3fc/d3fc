e9b338985788fbb6775d1556e461c62460ed2c25
Ro4052 <Ro4052@users.noreply.github.com>
2019-04-03 10:44:23 +0100

    feat: initial canvas crosshair annotation

--- packages/d3fc-annotation/index.js
+++ packages/d3fc-annotation/index.js
@@ -1,5 +1,6 @@
 export { default as annotationSvgBand } from './src/svg/band';
 export { default as annotationSvgCrosshair } from './src/svg/crosshair';
+export { default as annotationCanvasCrosshair } from './src/canvas/crosshair';
 export { default as annotationSvgLine } from './src/svg/line';
 export { default as annotationCanvasLine } from './src/canvas/line';
 export { default as annotationSvgGridline } from './src/svg/gridline';
