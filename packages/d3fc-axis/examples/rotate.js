c9029590ff8163c61e9a958850c9793ad9ea2aa9
Andy <alee@scottlogic.com>
2019-04-09 13:40:41 +0100

    cherry pick of axis rotation

--- packages/d3fc-axis/examples/rotate.js
+++ packages/d3fc-axis/examples/rotate.js
@@ -1,9 +1,12 @@
 var width = 400;
+var height = 80;
 var margin = 10;
 
 var scale = d3.scaleBand()
   .domain(['Carrots', 'Bananas', 'Sausages', 'Pickles', 'Aubergines', 'Artichokes', 'Spinach', 'Cucumber'])
   .range([margin, width - 40 - margin]);
+
+// Decorate
 
 var axis = fc.axisBottom(scale)
   .decorate(function(s) {
@@ -12,9 +15,82 @@ var axis = fc.axisBottom(scale)
         .attr('transform', 'rotate(45 -10 10)');
   });
 
-var svg = d3.select('body').append('svg')
-    .attr('width', width)
-    .attr('height', 80);
+var svg = d3.select('#decorateRotate').attr('width', width).attr('height', height);
 svg.append('g')
     .attr('transform', 'translate(0, 10)')
     .call(axis);
+
+// Auto
+const draw = () => {
+  const groupRect = d3.select('.ordinal-group').node().getBoundingClientRect();
+  const groupSize = {
+    width: groupRect.width - 2 * height,
+    height: groupRect.height - 2 * height
+  };
+
+  const renderAxis = (target, axis) => {
+    const side = target.attr('class');
+    const vertical = side == 'left' || side == 'right';
+  
+    scale.range([0, vertical ? groupSize.height : groupSize.width]);
+  
+    target
+      .attr('width', `${vertical ? height : groupSize.width}px`)
+      .attr('height', `${vertical ? groupSize.height : height}px`)
+
+    let axisElement = target.select('g');
+    if (!axisElement.size()) axisElement = target.append('g');
+    let backgroundElement = axisElement.select('rect');
+    if (!backgroundElement.size()) backgroundElement = axisElement.append('rect');
+
+    const size = (axis[vertical ? 'width' : 'height'])(axisElement);
+    backgroundElement
+      .attr('x', side == 'left' ? -size : 0)
+      .attr('y', side == 'top' ? -size : 0)
+      .attr('width', vertical ? size : groupSize.width)
+      .attr('height', !vertical ? size : groupSize.height)
+      .style('fill', '#eee');
+  
+    axisElement
+      .attr('transform', () => {
+        if (side == 'top') return `translate(0, ${height})`;
+        if (side == 'left') return `translate(${height}, 0)`;
+      })
+      .call(axis);
+  };
+  
+  renderAxis(d3.select('#topAuto'),
+      fc.axisOrdinalTop(scale).labelRotate('auto')
+    );
+
+  renderAxis(d3.select('#bottomAuto'),
+      fc.axisOrdinalBottom(scale).labelRotate('auto')
+    );
+
+  renderAxis(d3.select('#leftAuto'),
+      fc.axisOrdinalLeft(scale).labelRotate('auto')
+    );
+
+    renderAxis(d3.select('#rightAuto'),
+    fc.axisOrdinalRight(scale).labelRotate('auto')
+  );
+
+  renderAxis(d3.select('#topFixed'),
+      fc.axisOrdinalTop(scale).labelRotate(30)
+    );
+
+  renderAxis(d3.select('#bottomFixed'),
+      fc.axisOrdinalBottom(scale).labelRotate(30)
+    );
+
+  renderAxis(d3.select('#leftFixed'),
+      fc.axisOrdinalLeft(scale).labelRotate(30)
+    );
+
+  renderAxis(d3.select('#rightFixed'),
+    fc.axisOrdinalRight(scale).labelRotate(30)
+  );
+};
+
+draw();
+window.addEventListener('resize', () => draw());
--- packages/d3fc-axis/src/axisOrdinal.js
+++ packages/d3fc-axis/src/axisOrdinal.js
@@ -1,9 +1,12 @@
 import { dataJoin as _dataJoin } from '@d3fc/d3fc-data-join';
-import { rebindAll } from '@d3fc/d3fc-rebind';
+import { rebindAll, exclude } from '@d3fc/d3fc-rebind';
 import { axisBase } from './axisBase';
 
 const axisOrdinal = (orient, scale) => {
     let tickOffset = null;
+    let labelRotate;
+    let decorate = () => {};
+    let rotation;
 
     const step = (tick, index, ticksArray) => {
         if (scale.step) {
@@ -37,10 +40,117 @@ const axisOrdinal = (orient, scale) => {
         return { offset: [0, base.tickPadding()] };
     };
 
+    const decorateRotation = s => {
+        if (labelRotate) {
+            const { rotate, labelHeight, anchor } = rotation;
+
+            const text = s.select('text');
+            const existingTransform = text.attr('transform');
+
+            const offset = sign() * Math.floor(labelHeight / 2);
+            const offsetTransform = isVertical() ? `translate(${offset}, 0)` : `translate(0, ${offset})`;
+
+            text.style('text-anchor', anchor)
+                .attr('transform', `${existingTransform} ${offsetTransform} rotate(${rotate} 0 0)`);
+        }
+    };
+
+    const measureLabels = s => {
+        const labels = scale.domain();
+
+        // Use a test element to measure the text in the axis SVG container
+        s.attr('font-size', 10).attr('font-family', 'sans-serif');
+        const tester = s.append('text');
+        const labelHeight = tester.text('Ty').node().getBBox().height;
+        const maxWidth = Math.max(...labels.map(l => tester.text(l).node().getBBox().width));
+        tester.remove();
+
+        return {
+            labelHeight,
+            maxWidth,
+            measuredSize: labels.length * maxWidth
+        };
+    };
+
+    const calculateRotation = s => {
+        if (labelRotate) {
+            const { labelHeight, maxWidth, measuredSize } = measureLabels(s);
+
+            // The more the overlap, the more we rotate
+            let rotate;
+            if (labelRotate === 'auto') {
+                const range = scale.range()[1];
+                rotate = range < measuredSize ? 90 * Math.min(1, (measuredSize / range - 0.8) / 2) : 0;
+            } else {
+                rotate = labelRotate;
+            }
+
+            rotation = {
+                rotate: isVertical() ? Math.floor(sign() * (90 - rotate)) : Math.floor(-rotate),
+                labelHeight,
+                maxWidth,
+                anchor: rotate ? labelAnchor() : 'middle'
+            };
+        } else {
+            rotation = {
+                rotate: 0,
+                anchor: isVertical() ? labelAnchor() : 'middle'
+            };
+        }
+    };
+
+    const isVertical = () =>
+        orient === 'left' || orient === 'right';
+    const sign = () => (orient === 'top' || orient === 'left') ? -1 : 1;
+
+    const labelAnchor = () => {
+        switch (orient) {
+        case 'top':
+        case 'right':
+            return 'start';
+        default:
+            return 'end';
+        }
+    };
+
     const base = axisBase(orient, scale, {labelOffset, tickPath});
 
+    base.decorate(s => {
+        decorateRotation(s);
+        decorate(s);
+    });
+
     const axis = (selection) => {
-        return base(selection);
+        if (!rotation) calculateRotation(selection);
+        base(selection);
+        rotation = null;
+    };
+
+    const calculateSize = s => {
+        if (labelRotate) {
+            calculateRotation(s);
+            const { maxWidth, labelHeight } = rotation;
+
+            const degrees = rotation.rotate + (isVertical() ? 90 : 0);
+            const radians = degrees / 90 * Math.PI / 2;
+
+            return Math.floor(
+                    maxWidth * Math.abs(Math.sin(radians)) +
+                    labelHeight * Math.abs(Math.cos(radians)) +
+                    Math.floor(labelHeight / 2) + 2 * base.tickPadding()
+                );
+        }
+        return null;
+    };
+
+    axis.height = (selection) => {
+        if (isVertical()) return null;
+        return calculateSize(selection);
+    };
+
+    axis.width = (selection) => {
+        if (!isVertical()) return null;
+        return calculateSize(selection);
     };
 
     axis.tickOffset = (...args) => {
@@ -51,7 +161,23 @@ const axisOrdinal = (orient, scale) => {
         return axis;
     };
 
-    rebindAll(axis, base);
+    axis.labelRotate = (...args) => {
+        if (!args.length) {
+            return labelRotate;
+        }
+        labelRotate = args[0];
+        return axis;
+    };
+
+    axis.decorate = (...args) => {
+        if (!args.length) {
+            return decorate;
+        }
+        decorate = args[0];
+        return axis;
+    };
+
+    rebindAll(axis, base, exclude('decorate'));
     return axis;
 };
 
