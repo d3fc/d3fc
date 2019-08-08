ec25145a9d49919d0f92090a81f78f70026ebe10
Tom McLaughlan <tmclaughlan@scottlogic.com>
2019-08-08 11:46:33 +0100

    Add circle point shader utilities for setting common parameters such as fill and stroke.

--- packages/d3fc-webgl/index.js
+++ packages/d3fc-webgl/index.js
@@ -5,7 +5,10 @@ export { default as shaderBuilder } from './src/shaders/shaderBuilder';
 export { default as programBuilder } from './src/program/programBuilder';
 export { default as scaleMapper } from './src/scale/scaleMapper';
 
-export { default as circlePointShader } from './src/shaders/circlePointShader';
+export { default as circlePointShader } from './src/shaders/point/circle/baseShader';
+export { default as circleFill } from './src/shaders/point/circle/fill';
+export { default as circleStroke } from './src/shaders/point/circle/stroke';
+export { default as circleAntiAlias } from './src/shaders/point/circle/antialias';
 
 export { default as glDraw } from './src/glDraw';
 
