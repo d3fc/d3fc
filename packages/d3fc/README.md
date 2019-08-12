5e70a75339789f91db456d16d77c8781a2a559fd
Hanna Greaves <hgreaves@scottlogic.com>
2019-08-12 13:12:26 +0100

    fix screenshot tool

--- packages/d3fc/README.md
+++ packages/d3fc/README.md
@@ -24,7 +24,7 @@ This install dependencies, and runs `lerna bootstrap` in order to manage the cro
 When making changes to a package, you can either execute `npm run bundle` within the package folder to build just that package, or you can run the following from the project root:
 
 ~~~
-npx lerna run bundle --since --concurrency 1
+lerna run bundle --since --concurrency 1
 ~~~
 
 This uses `lerna run` to execute the `bundle` run script for all packages that have been modified (or have modified dependencies).
--- scripts/screenshot.js
+++ scripts/screenshot.js
@@ -1,62 +1,71 @@
-const webshot = require('webshot');
+const puppeteer = require('puppeteer');
+
 const http = require('http');
 const fs = require('fs');
 const path = require('path');
 const finalhandler = require('finalhandler');
 const serveStatic = require('serve-static');
 
-const bodyMargin = 8;
-const width = 500;
-const height = 250;
+const helpRequired = process.argv.indexOf('--help') !== -1 || process.argv.indexOf('-?') !== -1 || process.argv.indexOf('/?') !== -1
+
+if(helpRequired) {
+    console.log(`
+        Generates screenshots for html files located in the specified folder
+
+        Usage: screenshot [--folder targetFolder --selector domSelector]
+
+        --folder : the relative path to the folder containing html files for which to generate screenshots, defaults to 'examples'
+        --selector: the DOM selector query of elements to generate screenshots for, defaults to 'canvas, svg'
+    `)
+    return
+}
+
+const getArg = (arg) => process.argv.indexOf(arg) !== -1 ? process.argv[process.argv.indexOf(arg) + 1] : null
+const selectorArg = '--selector'
+const captureSelector = getArg(selectorArg) || 'canvas, svg'
+
+    
+const folderArg = '--folder'
+const folder = getArg(folderArg) || 'examples'
+
 const port = 8080;
 
-const config = {
-    screenSize: {
-        width: width + bodyMargin,
-        height: height + bodyMargin
-    },
-    shotSize: {
-        width: width,
-        height: height
-    },
-    captureSelector: 'canvas, svg, body',
-    errorIfJSException: true,
-    errorIfStatusIsNot200: true
-};
+const capture = (url, file, browser) =>
+    new Promise(async (resolve, reject) => {
+        const screenshot = path.resolve('screenshots', file.substr(0, file.length - 5)) + '.png'
+        const page = await browser.newPage()
+        await page.goto(url, { waitUntil: 'networkidle2'})
+        const rect = await page.evaluate((captureSelector) => {
+            const elem = document.querySelector(captureSelector)
+            if(!elem) { return null }
+            const { x, y, width, height } = elem.getBoundingClientRect()
+            return {x, y, height, width, id: elem.id }
+        }, captureSelector)
+        try {
+            await page.screenshot({path: screenshot, clip: rect})
+            console.log(`${file} ✔️`)
+            resolve()
+        } catch(e) {
+            console.log(`${file} ❌ (${e})`)
+            reject(e)
+        }
+})
 
-const takeScreenshots = () =>
-  new Promise((resolve, reject) => {
-      const dir = process.cwd()
-      fs.readdir(process.cwd() + '/examples', function(err, files) {
-          if (err) {
-              console.error(err);
-              return;
-          }
-          
+const takeScreenshots = (browser) =>
+    new Promise((resolve, reject) => {
+        const files = fs.readdirSync(`${process.cwd()}/${folder}`)
+            .filter(file => file.substr(-4) === 'html')
         const pkgInfo = require(`${process.cwd()}/package.json`)
         const name = pkgInfo.name.replace('@d3fc/', '')
-        console.log('Producing screenshots for package : ' + name)
+        console.log(`Producing ${files.length} screenshot(s) for package : ${name}`)
         const promises = files
-        .filter(file => file.substr(-4) === 'html')
-        .map(file =>
-            new Promise((resolve, reject) => {
-                const url = `http://localhost:${port}/packages/${name}/examples/${file}`;
-                const screenshot = path.resolve('screenshots', file.substr(0, file.length - 5)) + '.png';
-                webshot(url, screenshot, config, (err) => {
-                    if (err) {
-                        reject(err);
-                        console.error(`Failed to capture ${file} (${err})`);
-                        return
-                    }
-                    console.log(`${screenshot} captured\n`)
-                    resolve();
-                });
-            })
-        );
+        .map(async (file) => {
+            const url = `http://localhost:${port}/packages/${name}/examples/${file}`;
+            return capture(url, file, browser)
+        })
+        Promise.all(promises).then(resolve).catch(reject)
+    })
 
-        Promise.all(promises).then(resolve).catch(reject);
-      });
-  });
 const serve = serveStatic('../..');
 
 const server = http.createServer(function(req, res) {
@@ -64,16 +73,15 @@ const server = http.createServer(function(req, res) {
     serve(req, res, done);
 });
 
-server.listen(port, () =>
-  takeScreenshots()
-    .then(() => {
-        console.log('Screenshots complete 📷');
-        server.close();
+server.listen(port, () => {
+    puppeteer.launch({args: ['--no-sandbox', '--disable-setuid-sandbox']}).then(async(browser) => {
+        takeScreenshots(browser).then(() => {
+            console.log('Screenshots complete 📷');
+        }).catch(err => {
+                console.error('Screenshots failed ❌ ', err);
+        }).finally(() =>{
+            browser.close()
+            server.close();
+        })
     })
-    .catch(err => {
-        if (err) {
-            console.error('Screenshots failed ❌ ', err);
-        }
-        server.close();
-    })
-);
+})
