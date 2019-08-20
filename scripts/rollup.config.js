c8a76fddc1b78479bddda2d49cab07b12bbba2ce
Hanna Greaves <hgreaves@scottlogic.com>
2019-08-20 16:29:43 +0100

    You can now npm start from any package root with examples and optionally provide a -- --openFile='' flag to specify the example to use for dev mode

--- scripts/rollup.config.js
+++ scripts/rollup.config.js
@@ -1,7 +1,10 @@
 import babel from 'rollup-plugin-babel';
 import babelrc from 'babelrc-rollup';
 import nodeResolve from 'rollup-plugin-node-resolve';
+import serve from 'rollup-plugin-serve'
+import livereload from 'rollup-plugin-livereload'
 
+const devMode = process.env.BUILD === 'dev'
 var external = (key) => key.indexOf('d3-') === 0 || key.indexOf('d3fc-') === 0
 var globals = function(key) {
   if (key.indexOf('d3-') === 0) {
@@ -12,12 +15,27 @@ var globals = function(key) {
   }
 };
 
-let plugins = [
-  babel(babelrc()),
-  nodeResolve({ jsnext: true, main: true })
-];
-
 export default commandLineArgs => {
+    const page = commandLineArgs.openFile || 'index.html'
+    
+    let plugins = () => [
+      babel(babelrc()),
+      nodeResolve({ jsnext: true, main: true })
+    ];
+
+    const devPlugins = () => plugins().concat([
+      serve({
+          contentBase: ['..', '.'],
+          open: true,
+          openPage: `/examples/${page}`,
+          host: 'localhost',
+          port: 8080
+      }),
+      livereload({
+          watch: ['build', 'examples']
+      })
+    ]);
+
     const pkgInfo = require(`${process.cwd()}/package.json`)
     if(!pkgInfo) {
       throw Error('Expected build to be triggered from directory containing package.json')
@@ -27,9 +45,15 @@ export default commandLineArgs => {
       throw Error('Expected package.json to contain `name` field')
     }
     name = name.replace('@d3fc/', '')
+
+    if(devMode && !commandLineArgs.openFile) {
+      console.log('Did you know, you can specify an example to start using')
+      console.log('\x1b[30m\x1b[47m%s\x1b[0m', '\t-- --openFile="<example.html>" ?'); 
+    }
+
     return {
-      input: 'index.js',
-      plugins: plugins,
+      input: 'index.js', 
+      plugins: devMode ? devPlugins() : plugins(),
       external: external,
       output: {
           file: `build/${name}.js`,
