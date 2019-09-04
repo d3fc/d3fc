477dcc46a2bdc9a6a9a6adb5fa73f500f51870b1
Hanna Greaves <HGreaves@scottlogic.com>
2019-09-04 16:48:20 +0100

    feat: add Conventional Commit Support
    Enables Commitizen integration and a Conventional Commit lint check which is run on Travis during PRs

---
+++ scripts/commit-notice.js
@@ -0,0 +1,8 @@
+const chalk = require('chalk')
+
+console.log('+---------------------------------------------------+')
+console.log('|         ' + chalk.bold.magenta('This repo is Commitzen Friendly!') +'          |')
+console.log('| All commits should be accompanied by conventional |')
+console.log('| commit messages. You can easily generate an appr- |')
+console.log('| opriate message by instead using ' + chalk.blue('`npx git-cz`') +' :)  |')
+console.log('+---------------------------------------------------+')
