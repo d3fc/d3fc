db0019e31d53a30106ca945b005d8363dcb390f2
OliverForeman <oliverforeman@btinternet.com>
2019-10-11 12:27:21 +0100

    Added basic webgl ohlc example

---
+++ packages/d3fc-webgl/examples/ohlc.js
@@ -0,0 +1,48 @@
+const dataGenerator = fc.randomFinancial();
+let data = dataGenerator(10);
+
+let counter = 0;
+data = data.map((val) => ({
+  ...val,
+  x: counter++
+}));
+console.log('data:', data);
+
+const x = d3.scaleLinear();
+const y = d3.scaleLinear();
+
+x.domain(fc.extentLinear().accessors([d => d.x])(data));
+y.domain(fc.extentLinear().accessors([d => d.high, d => d.low])(data));
+
+const x2 = x.copy();
+const y2 = y.copy();
+
+const getWebglSeries = () => fc.seriesWebglOhlc()
+  .crossValue(d => d.x);
+
+const zoom = d3.zoom()
+  .on('zoom', () => {
+    x.domain(d3.event.transform.rescaleX(x2).domain());
+    y.domain(d3.event.transform.rescaleY(y2).domain());
+    d3.select('d3fc-group')
+      .node()
+      .requestRedraw();
+  });
+
+const decorate = (sel) => {
+  sel.enter()
+    .select('.plot-area')
+    .on('measure.range', () => {
+      x2.range([0, d3.event.detail.width]);
+      y2.range([d3.event.detail.height, 0]);
+    })
+    .call(zoom);
+};
+
+const chart = fc.chartCartesian(x, y)
+  .decorate(decorate)
+  .webglPlotArea(getWebglSeries());
+
+d3.select('#chart')
+  .datum(data)
+  .call(chart);
