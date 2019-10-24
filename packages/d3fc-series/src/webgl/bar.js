60e219d238c3d6a9185568dd378c43dac00bd096
Matt Stobbs <mstobbs@scottlogic.com>
2019-10-24 16:24:43 +0100

    Add webgl bar with example

---
+++ packages/d3fc-series/src/webgl/bar.js
@@ -0,0 +1,44 @@
+import xyBase from '../xyBase';
+import { glBar, scaleMapper } from '@d3fc/d3fc-webgl';
+import { exclude, rebind, rebindAll } from '@d3fc/d3fc-rebind';
+
+export default () => {
+    const base = xyBase();
+
+    const draw = glBar();
+
+    const bar = (data) => {
+        const filteredData = data.filter(base.defined());
+
+        const xScale = scaleMapper(base.xScale());
+        const yScale = scaleMapper(base.yScale());
+
+        const xValues = new Float32Array(filteredData.length);
+        const y0Values = new Float32Array(filteredData.length);
+        const y1Values = new Float32Array(filteredData.length);
+        const widths = new Float32Array(filteredData.length);
+        filteredData.forEach((d, i) => {
+            xValues[i] = xScale.scale(base.crossValue()(d, i));
+            y0Values[i] = xScale.scale(base.baseValue()(d, i));
+            y1Values[i] = xScale.scale(base.mainValue()(d, i));
+            widths[i] = xScale.scale(base.bandwidth()(d, i));
+        });
+
+        draw.xValues(xValues)
+            .y0Values(y0Values)
+            .y1Values(y1Values)
+            .widths(widths)
+            .xScale(xScale.glScale)
+            .yScale(yScale.glScale)
+            .decorate((program) => {
+                base.decorate()(program, filteredData, 0);
+            });
+        draw(filteredData.length);
+    };
+
+    // Rebinding methods.
+    rebindAll(bar, base, exclude('align'));
+    rebind(bar, draw, 'context');
+
+    return bar;
+};
---
+++ packages/d3fc-webgl/src/shaders/bar/shader.js
@@ -0,0 +1,20 @@
+import shaderBuilder, { vertexShaderBase, fragmentShaderBase } from '../shaderBuilder';
+
+export default () => {
+    const vertexShader = shaderBuilder(vertexShaderBase);
+    const fragmentShader = shaderBuilder(fragmentShaderBase);
+
+    vertexShader.appendHeader(`attribute float aXValue;`)
+        .appendHeader(`attribute float aYValue;`)
+        .appendHeader(`attribute float aWidthValue;`)
+        .appendHeader(`attribute float aDirection;`)
+        .appendBody(`gl_Position = vec4(aXValue, aYValue, 0, 1);`)
+        .appendBody(`vec4 origin = vec4(0.0, 0.0, 0.0, 0.0);`)
+        .appendBody(`vec4 width = vec4(aWidthValue, 0.0, 0.0, 0.0);`)
+        .appendBody(`gl_Position.x += (width.x - origin.x) / 2.0 * aDirection;`);
+
+    return {
+        vertex: () => vertexShader,
+        fragment: () => fragmentShader
+    };
+};
