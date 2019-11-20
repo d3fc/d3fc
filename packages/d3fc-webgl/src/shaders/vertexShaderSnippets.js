10019623e1a6ffbc6afd8c8f6813263d933825ac
OliverForeman <oliverforeman@btinternet.com>
2019-11-20 13:58:47 +0000

    Refactored vertex shader to remove conditional

--- packages/d3fc-webgl/src/shaders/vertexShaderSnippets.js
+++ packages/d3fc-webgl/src/shaders/vertexShaderSnippets.js
@@ -81,22 +81,47 @@ export const area = {
         attribute float aY0PrevValue;
         attribute vec3 aCorner;
         attribute float aDefined;
-        varying float vDefined;`,
+        varying float vDefined;
+        
+        float when_eq(float a, float b) {
+            return 1.0 - abs(sign(a - b));
+        }
+        
+        float when_lt(float a, float b) {
+            return max(sign(b - a), 0.0);
+        }
+        
+        float when_gt(float a, float b) {
+            return max(sign(a - b), 0.0);
+        }
+        
+        float and(float a, float b) {
+            return a * b;
+        }
+        
+        float or(float a, float b) {
+            return min(a + b, 1.0);
+        }`,
     body: `vDefined = aDefined;
         gl_Position = vec4(0, 0, 0, 1);
-        if (aCorner.z == 1.0 && ((aYPrevValue < aY0PrevValue && aYValue > aY0Value) || (aYPrevValue > aY0PrevValue && aYValue < aY0Value))) {
-            float yGradient = (aYValue - aYPrevValue) / (aXValue - aXPrevValue);
-            float yConstant = aYValue - (yGradient * aXValue);
-            
-            float y0Gradient = (aY0Value - aY0PrevValue) / (aXValue - aXPrevValue);
-            float y0Constant = aY0Value - (y0Gradient * aXValue);
-            
-            float finalXValue = (y0Constant - yConstant) / (yGradient - y0Gradient);
-            float finalYValue = (yGradient * finalXValue) + yConstant;
-            gl_Position = vec4(finalXValue, finalYValue, 0, 1);
-        } else {
-            gl_Position.x += (aCorner.x * aXValue) + ((1.0 - aCorner.x) * aXPrevValue);
-            gl_Position.y += (aCorner.x * (1.0 - aCorner.y) * aYValue) + ((1.0 - aCorner.x) * (1.0 - aCorner.y) * aYPrevValue);
-            gl_Position.y += (aCorner.x * aCorner.y * aY0Value) + ((1.0 - aCorner.x) * aCorner.y * aY0PrevValue);
-        }`
+
+        float correctCorner = when_eq(aCorner.z, 1.0);
+        float interceptY0PosGrad = and(when_lt(aYPrevValue, aY0PrevValue), when_gt(aYValue, aY0Value));
+        float interceptY0NegGrad = and(when_gt(aYPrevValue, aY0PrevValue), when_lt(aYValue, aY0Value));
+        float useIntercept = and(correctCorner, or(interceptY0PosGrad, interceptY0NegGrad));
+        
+        float yGradient = (aYValue - aYPrevValue) / (aXValue - aXPrevValue);
+        float yConstant = aYValue - (yGradient * aXValue);
+        
+        float y0Gradient = (aY0Value - aY0PrevValue) / (aXValue - aXPrevValue);
+        float y0Constant = aY0Value - (y0Gradient * aXValue);
+        
+        float interceptXValue = (y0Constant - yConstant) / (yGradient - y0Gradient);
+        float interceptYValue = (yGradient * interceptXValue) + yConstant;
+
+        gl_Position = vec4(interceptXValue * useIntercept, interceptYValue * useIntercept, 0, 1);
+        
+        gl_Position.x += ((aCorner.x * aXValue) + ((1.0 - aCorner.x) * aXPrevValue)) * (1.0 - useIntercept);
+        gl_Position.y += ((aCorner.x * (1.0 - aCorner.y) * aYValue) + ((1.0 - aCorner.x) * (1.0 - aCorner.y) * aYPrevValue)) * (1.0 - useIntercept);
+        gl_Position.y += ((aCorner.x * aCorner.y * aY0Value) + ((1.0 - aCorner.x) * aCorner.y * aY0PrevValue)) * (1.0 - useIntercept);`
 };
