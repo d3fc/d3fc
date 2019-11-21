e3b27090fd379f1c1bdde82df070ad9951432f42
OliverForeman <oliverforeman@btinternet.com>
2019-11-21 14:18:44 +0000

    Webgl Area Charts (#1338)
    * Added shaders for square shape for point series
    
    * Updated export for pointFill from circleFill
    
    * Changed circleAlias to pointAlias and circleEdge to pointEdge
    
    * Updated square fragment shader to support stroke
    
    * Added squares performance example
    
    * Added function to map d3 shape to webgl implementation
    
    * Added support for setting a single shape
    
    * Added line to EOF
    
    * Finished squares example and removed unused code
    
    * Corrected indentation
    
    * Added square base shader to exports
    
    * Moved functor function to separate file
    
    * Added webgl setup for ohlc chart
    
    * Added webgl ohlc to d3fc-series
    
    * Added basic webgl ohlc example
    
    * Updated webgl squares example to reduce redraws
    
    * Updated ohlc drawing to triangles to support width
    
    * Updated symbol mapper default case and return statement
    
    * Changed uEdgeSize to strokeWidth for consistency
    
    * Changed square shader name from baseShader to shader
    
    * Moved functor function into separate file
    
    * Updated symbol mapper
    
    * Changed strokeWidth to uStrokeWidth
    
    * Removed functor from glPoint type
    
    * Corrected scaling and changed colours to match canvas
    
    * Added time scale to gl scale mapper
    
    * Updated x and y to xValues and yValues
    
    * Moved shader code to vertexShaderSnippets
    
    * Changed more x variables to xValues
    
    * Removed unused import
    
    * Added webgl ohlc to d3fc-series ohlc examples
    
    * Changed buffer binding and lineWidth to uniform
    
    * Updated example for changes
    
    * Updated color buffer creation to pass indicator to shaders
    
    * Refactored webgl line to draw in a single pass
    
    * Refactored defined array buffer creation
    
    * Renamed lastVal to prevVal
    
    * Refactored buffer creation for defined values
    
    * Removed return from shader
    
    * Added basic functionality for webgl area chart
    
    * Updated area examples with webgl chart
    
    * Added support for varying y0 values
    
    * Updated drawing to handle crossing y0 value
    
    * Updated example chart
    
    * Updated series area to pass decorate function to glArea
    
    * Refactored vertex shader to remove conditional
    
    * Removed unnecessary lines from vertex shader
    
    * Updated area examples with a base value
    
    * Moved base accessors to inline calls
    
    * Moved corner buffer creation to xValues
    
    * Factorised position calculations to highlight control factors

--- packages/d3fc-webgl/index.js
+++ packages/d3fc-webgl/index.js
@@ -19,6 +19,7 @@ export { default as glLine } from './src/series/glLine';
 export { default as glOhlc } from './src/series/glOhlc';
 export { default as glBar } from './src/series/glBar';
 export { default as glErrorBar } from './src/series/glErrorBar';
+export { default as glArea } from './src/series/glArea';
 
 export { default as glScaleLinear } from './src/scale/glScaleLinear';
 export { default as glScaleLog } from './src/scale/glScaleLog';
