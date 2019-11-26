4309755c141bd29af425d188035e0cb74665c3e8
Chris Price <cprice@scottlogic.co.uk>
2019-11-26 10:23:16 +0000

    chore: upgrade eslint & configure for scripts

--- scripts/commit-notice.js
+++ scripts/commit-notice.js
@@ -1,8 +1,7 @@
-const chalk = require('chalk')
+const chalk = require('chalk');
 
-console.log('+---------------------------------------------------+')
-console.log('|         ' + chalk.bold.magenta('This repo is Commitzen Friendly!') +'          |')
-console.log('| All commits should be accompanied by conventional |')
-console.log('| commit messages. You can easily generate an appr- |')
-console.log('| opriate message by instead using ' + chalk.blue('`npx git-cz`') +' :)  |')
-console.log('+---------------------------------------------------+')
+console.log(
+    'All commits should be accompanied by conventional commit messages. You can easily generate an appropriate message by instead using ' +
+        chalk.blue('`npx git-cz`') +
+        '.'
+);
