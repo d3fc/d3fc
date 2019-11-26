3256bc6e26f54be9b6a5a32fe767c715f7bb710f
Chris Price <cprice@scottlogic.co.uk>
2019-11-26 10:23:16 +0000

    chore: upgrade eslint & configure for scripts

--- scripts/rollup.config.js
+++ scripts/rollup.config.js
@@ -1,47 +1,46 @@
 import babel from 'rollup-plugin-babel';
-import babelrc from 'read-babelrc-up'
+import babelrc from 'read-babelrc-up';
 import resolve from 'rollup-plugin-node-resolve';
 import minify from 'rollup-plugin-babel-minify';
 
-var external = (key) => key.indexOf('d3-') === 0 || key.indexOf('d3fc-') === 0
+var external = key => key.indexOf('d3-') === 0 || key.indexOf('d3fc-') === 0;
 var globals = function(key) {
-  if (key.indexOf('d3-') === 0) {
-      return 'd3';
-  }
-  if (key.indexOf('d3fc-') === 0) {
-      return 'fc'
-  }
+    if (key.indexOf('d3-') === 0) {
+        return 'd3';
+    }
+    if (key.indexOf('d3fc-') === 0) {
+        return 'fc';
+    }
 };
 
 export default commandLineArgs => {
-    process.env = commandLineArgs.configEnv || 'dev'
-    const shouldMinify = process.env === 'prod'
-    let plugins = [
-      babel(babelrc()),
-      resolve()
-    ];
-    if(shouldMinify) {
-      plugins.push(minify({ comments: false }))
+    process.env = commandLineArgs.configEnv || 'dev';
+    const shouldMinify = process.env === 'prod';
+    const plugins = [babel(babelrc()), resolve()];
+    if (shouldMinify) {
+        plugins.push(minify({ comments: false }));
     }
-    const pkgInfo = require(`${process.cwd()}/package.json`)
-    if(!pkgInfo) {
-      throw Error('Expected build to be triggered from directory containing package.json')
+    const pkgInfo = require(`${process.cwd()}/package.json`);
+    if (!pkgInfo) {
+        throw Error(
+            'Expected build to be triggered from directory containing package.json'
+        );
     }
-    let name = pkgInfo.name
-    if(!name) {
-      throw Error('Expected package.json to contain `name` field')
+    let name = pkgInfo.name;
+    if (!name) {
+        throw Error('Expected package.json to contain `name` field');
     }
-    name = name.replace('@d3fc/', '')
+    name = name.replace('@d3fc/', '');
     return {
-      input: 'index.js',
-      plugins: plugins,
-      external: external,
-      output: {
-          file: `build/${name}${shouldMinify ? '.min' : ''}.js`,
-          format: 'umd',
-          globals: globals,
-          extend: true,
-          name: 'fc',
-      }
-  }
+        input: 'index.js',
+        plugins: plugins,
+        external: external,
+        output: {
+            file: `build/${name}${shouldMinify ? '.min' : ''}.js`,
+            format: 'umd',
+            globals: globals,
+            extend: true,
+            name: 'fc'
+        }
+    };
 };
--- scripts/screenshot.js
+++ scripts/screenshot.js
@@ -6,9 +6,12 @@ const path = require('path');
 const finalhandler = require('finalhandler');
 const serveStatic = require('serve-static');
 
-const helpRequired = process.argv.indexOf('--help') !== -1 || process.argv.indexOf('-?') !== -1 || process.argv.indexOf('/?') !== -1
+const helpRequired =
+    process.argv.indexOf('--help') !== -1 ||
+    process.argv.indexOf('-?') !== -1 ||
+    process.argv.indexOf('/?') !== -1;
 
-if(helpRequired) {
+if (helpRequired) {
     console.log(`
         Generates screenshots for html files located in the specified folder
 
@@ -16,55 +19,62 @@ if(helpRequired) {
 
         --folder : the relative path to the folder containing html files for which to generate screenshots, defaults to 'examples'
         --selector: the DOM selector query of elements to generate screenshots for, defaults to 'canvas, svg'
-    `)
-    return
+    `);
+    process.exit(1);
 }
 
-const getArg = (arg) => process.argv.indexOf(arg) !== -1 ? process.argv[process.argv.indexOf(arg) + 1] : null
-const selectorArg = '--selector'
-const captureSelector = getArg(selectorArg) || 'canvas, svg'
+const getArg = arg =>
+    process.argv.indexOf(arg) !== -1
+        ? process.argv[process.argv.indexOf(arg) + 1]
+        : null;
+const selectorArg = '--selector';
+const captureSelector = getArg(selectorArg) || 'canvas, svg';
 
-    
-const folderArg = '--folder'
-const folder = getArg(folderArg) || 'examples'
+const folderArg = '--folder';
+const folder = getArg(folderArg) || 'examples';
 
 const port = 8080;
 
-const capture = (url, file, browser) =>
-    new Promise(async (resolve, reject) => {
-        const screenshot = path.resolve('screenshots', file.substr(0, file.length - 5)) + '.png'
-        const page = await browser.newPage()
-        await page.goto(url, { waitUntil: 'networkidle2'})
-        const rect = await page.evaluate((captureSelector) => {
-            const elem = document.querySelector(captureSelector)
-            if(!elem) { return null }
-            const { x, y, width, height } = elem.getBoundingClientRect()
-            return {x, y, height, width, id: elem.id }
-        }, captureSelector)
-        try {
-            await page.screenshot({path: screenshot, clip: rect})
-            console.log(`${file} ✔️`)
-            resolve()
-        } catch(e) {
-            console.log(`${file} ❌ (${e})`)
-            reject(e)
+const capture = (url, file, browser) => async () => {
+    const screenshot =
+        path.resolve('screenshots', file.substr(0, file.length - 5)) + '.png';
+    const page = await browser.newPage();
+    await page.goto(url, { waitUntil: 'networkidle2' });
+    const rect = await page.evaluate(captureSelector => {
+        const elem = document.querySelector(captureSelector);
+        if (!elem) {
+            return null;
         }
-})
+        const { x, y, width, height } = elem.getBoundingClientRect();
+        return { x, y, height, width, id: elem.id };
+    }, captureSelector);
+    try {
+        await page.screenshot({ path: screenshot, clip: rect });
+        console.log(`${file} ✔️`);
+    } catch (e) {
+        console.log(`${file} ❌ (${e})`);
+        throw e;
+    }
+};
 
-const takeScreenshots = (browser) =>
+const takeScreenshots = browser =>
     new Promise((resolve, reject) => {
-        const files = fs.readdirSync(`${process.cwd()}/${folder}`)
-            .filter(file => file.substr(-4) === 'html')
-        const pkgInfo = require(`${process.cwd()}/package.json`)
-        const name = pkgInfo.name.replace('@d3fc/', '')
-        console.log(`Producing ${files.length} screenshot(s) for package : ${name}`)
-        const promises = files
-        .map(async (file) => {
+        const files = fs
+            .readdirSync(`${process.cwd()}/${folder}`)
+            .filter(file => file.substr(-4) === 'html');
+        const pkgInfo = require(`${process.cwd()}/package.json`);
+        const name = pkgInfo.name.replace('@d3fc/', '');
+        console.log(
+            `Producing ${files.length} screenshot(s) for package : ${name}`
+        );
+        const promises = files.map(async file => {
             const url = `http://localhost:${port}/packages/${name}/examples/${file}`;
-            return capture(url, file, browser)
-        })
-        Promise.all(promises).then(resolve).catch(reject)
-    })
+            return capture(url, file, browser);
+        });
+        Promise.all(promises)
+            .then(resolve)
+            .catch(reject);
+    });
 
 const serve = serveStatic('../..');
 
@@ -74,14 +84,19 @@ const server = http.createServer(function(req, res) {
 });
 
 server.listen(port, () => {
-    puppeteer.launch({args: ['--no-sandbox', '--disable-setuid-sandbox']}).then(async(browser) => {
-        takeScreenshots(browser).then(() => {
-            console.log('Screenshots complete 📷');
-        }).catch(err => {
-                console.error('Screenshots failed ❌ ', err);
-        }).finally(() =>{
-            browser.close()
-            server.close();
-        })
-    })
-})
+    puppeteer
+        .launch({ args: ['--no-sandbox', '--disable-setuid-sandbox'] })
+        .then(async browser => {
+            takeScreenshots(browser)
+                .then(() => {
+                    console.log('Screenshots complete 📷');
+                })
+                .catch(err => {
+                    console.error('Screenshots failed ❌ ', err);
+                })
+                .finally(() => {
+                    browser.close();
+                    server.close();
+                });
+        });
+});
