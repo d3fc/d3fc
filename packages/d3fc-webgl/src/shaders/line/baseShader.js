97e4ebf8f815f216223e48920979e5ecd27fcbb9
Matt Stobbs <mstobbs@scottlogic.com>
2019-11-29 10:02:18 +0000

    refactor: move shader code into shader snippets

--- packages/d3fc-webgl/src/shaders/line/baseShader.js
+++ packages/d3fc-webgl/src/shaders/line/baseShader.js
@@ -2,34 +2,20 @@ import shaderBuilder, {
     vertexShaderBase,
     fragmentShaderBase
 } from '../shaderBuilder';
+import * as vertexShaderSnippets from '../vertexShaderSnippets';
+import * as fragmentShaderSnippets from '../fragmentShaderSnippets';
 
 export default () => {
     const vertexShader = shaderBuilder(vertexShaderBase);
     const fragmentShader = shaderBuilder(fragmentShaderBase);
 
     vertexShader
-        .appendHeader(`uniform float uLineWidth;`) // defines the width of the line
-        .appendHeader(`uniform vec2 uScreen;`) // the screen space canvas size (for normalizing vectors)
-        .appendHeader(`attribute vec2 aCorner;`) // defines which vertex in the line join we are considering
-        .appendHeader(`attribute float aXValue; attribute float aYValue;`) // the current vertex positions
-        .appendHeader(
-            `attribute float aNextXValue; attribute float aNextYValue;`
-        ) // the next vertex positions
-        .appendHeader(
-            `attribute float aPrevXValue; attribute float aPrevYValue;`
-        ) // the previous vertex positions
-        .appendHeader(`attribute float aDefined;`)
-        .appendHeader(`varying float vDefined;`)
-        .appendBody(`vDefined = aDefined;`)
-        .appendBody(`vec4 curr = vec4(aXValue, aYValue, 0, 1);`)
-        .appendBody(`gl_Position = curr;`)
-        .appendBody(`vec4 next = vec4(aNextXValue, aNextYValue, 0, 0);`)
-        .appendBody(`vec4 prev = vec4(aPrevXValue, aPrevYValue, 0, 0);`);
+        .appendHeader(vertexShaderSnippets.preScaleLine.header)
+        .appendBody(vertexShaderSnippets.preScaleLine.body);
 
-    fragmentShader.appendHeader(`varying float vDefined;`)
-        .appendBody(`if (vDefined < 0.5) {
-            discard;
-        }`);
+    fragmentShader
+        .appendHeader(fragmentShaderSnippets.line.header)
+        .appendBody(fragmentShaderSnippets.line.body);
 
     return {
         vertex: () => vertexShader,
