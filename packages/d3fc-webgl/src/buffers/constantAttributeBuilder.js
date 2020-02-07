89836f81ba4a1d24ab1371d4b1b1ed6aa5266684
Chris Price <cprice@scottlogic.co.uk>
2020-02-07 09:41:13 +0000

    refactor: reduce AB APIs to supported props only

---
+++ packages/d3fc-webgl/src/buffers/constantAttributeBuilder.js
@@ -0,0 +1,37 @@
+import baseAttributeBuilder from './baseAttributeBuilder';
+import { rebind } from '@d3fc/d3fc-rebind';
+
+export default () => {
+    const base = baseAttributeBuilder().divisor(1);
+    let value = null;
+
+    const build = (gl, program, name, count) => {
+        base(gl, program, name);
+
+        if (!Array.isArray(value)) {
+            throw new Error(`Expected an array, received: ${value}`);
+        }
+        if (value.length !== base.size()) {
+            throw new Error(
+                `Expected array of length: ${base.size()}, recieved array of length: ${
+                    value.length
+                }`
+            );
+        }
+        const location = gl.getAttribLocation(program, name);
+        gl[`vertexAttrib${value.length}fv`](location, value);
+        gl.disableVertexAttribArray(location);
+    };
+
+    build.value = (...args) => {
+        if (!args.length) {
+            return value;
+        }
+        value = args[0];
+        return build;
+    };
+
+    rebind(build, base, 'normalized', 'size');
+
+    return build;
+};
--- packages/d3fc-webgl/src/buffers/elementConstantAttributeBuilder.js
+++ packages/d3fc-webgl/src/buffers/elementConstantAttributeBuilder.js
@@ -1,90 +1,27 @@
 import baseAttributeBuilder from './baseAttributeBuilder';
-import { rebindAll, exclude } from '@d3fc/d3fc-rebind';
-import arrayViewFactory from './arrayViewFactory';
+import { rebind } from '@d3fc/d3fc-rebind';
+import attributeProjector from './attributeProjector';
 
 export default () => {
     const base = baseAttributeBuilder().divisor(1);
-    const factory = arrayViewFactory();
-
-    let value = (data, element) => data[element];
-    let data = null;
-
-    const project = elementCount => {
-        const componentCount = base.size();
-        const offset = base.offset();
-        const requiredLength = offset + elementCount * componentCount;
-        const projectedData = factory.type(base.type())(requiredLength);
-
-        if (componentCount > 1) {
-            for (let element = 0; element < elementCount; element++) {
-                const componentValues = value(data, element);
-                for (
-                    let component = 0;
-                    component < componentCount;
-                    component++
-                ) {
-                    projectedData[
-                        offset + element * componentCount + component
-                    ] = componentValues[component];
-                }
-            }
-        } else {
-            for (let element = 0; element < elementCount; element++) {
-                projectedData[offset + element] = value(data, element);
-            }
-        }
-
-        return projectedData;
-    };
+    const projector = attributeProjector();
 
     const build = (gl, program, name, count) => {
+        base.size(build.size()).type(build.type());
+
         base(gl, program, name);
 
-        if (!base.hasPropertyChanged()) {
+        if (!projector.dirty()) {
             return;
         }
 
-        if (typeof value === 'function') {
-            const projectedData = project(count);
-            gl.bindBuffer(gl.ARRAY_BUFFER, base.buffer());
-            gl.bufferData(gl.ARRAY_BUFFER, projectedData, gl.DYNAMIC_DRAW);
-        } else if (Array.isArray(value)) {
-            if (value.length === base.size()) {
-                const location = gl.getAttribLocation(program, name);
-                gl[`vertexAttrib${value.length}fv`](location, value);
-                gl.disableVertexAttribArray(location);
-            } else {
-                throw new Error(
-                    `Expected array of length: ${base.size()}, recieved array of length: ${
-                        value.length
-                    }`
-                );
-            }
-        } else {
-            throw new Error(`Expected function or array, received: ${value}`);
-        }
-        base.hasPropertyChanged(false);
+        const projectedData = projector(count);
+        gl.bindBuffer(gl.ARRAY_BUFFER, base.buffer());
+        gl.bufferData(gl.ARRAY_BUFFER, projectedData, gl.DYNAMIC_DRAW);
     };
 
-    build.value = (...args) => {
-        if (!args.length) {
-            return value;
-        }
-        value = args[0];
-        base.hasPropertyChanged(true);
-        return build;
-    };
-
-    build.data = (...args) => {
-        if (!args.length) {
-            return data;
-        }
-        data = args[0];
-        base.hasPropertyChanged(true);
-        return build;
-    };
-
-    rebindAll(build, base, exclude('buffer'));
+    rebind(build, base, 'normalized', 'divisor');
+    rebind(build, projector, 'data', 'value', 'size', 'type');
 
     return build;
 };
