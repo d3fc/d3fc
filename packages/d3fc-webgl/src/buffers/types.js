3900c0c1cbe3cbb98648f747d9ba45299481281c
Chris Price <cprice@scottlogic.co.uk>
2020-02-07 09:37:54 +0000

    refactor: allow arrayViewFactory.type rebinding

--- packages/d3fc-webgl/src/buffers/types.js
+++ packages/d3fc-webgl/src/buffers/types.js
@@ -22,3 +22,20 @@ export function length(type) {
             throw new Error(`Unknown type ${type}`);
     }
 }
+
+export function getArrayViewConstructor(type) {
+    switch (type) {
+        case types.BYTE:
+            return Int8Array;
+        case types.UNSIGNED_BYTE:
+            return Uint8Array;
+        case types.SHORT:
+            return Int16Array;
+        case types.UNSIGNED_SHORT:
+            return Uint16Array;
+        case types.FLOAT:
+            return Float32Array;
+        default:
+            throw new Error(`Unknown type ${type}`);
+    }
+}
