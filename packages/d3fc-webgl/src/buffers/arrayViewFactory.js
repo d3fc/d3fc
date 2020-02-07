29d5d40b5933bf7bf59ee306298f9c9bf8d36d4a
Chris Price <cprice@scottlogic.co.uk>
2020-02-07 09:37:54 +0000

    refactor: allow arrayViewFactory.type rebinding

--- packages/d3fc-webgl/src/buffers/arrayViewFactory.js
+++ packages/d3fc-webgl/src/buffers/arrayViewFactory.js
@@ -1,10 +1,11 @@
-import typeMapper from './typeMapper';
+import types, { getArrayViewConstructor } from './types';
 
 export default () => {
-    let ArrayType = Float32Array;
-    let cachedArray = new ArrayType(0);
+    let type = types.FLOAT;
+    let cachedArray = new Float32Array(0);
 
     const factory = requiredLength => {
+        const ArrayType = getArrayViewConstructor(type);
         if (cachedArray.length > requiredLength) {
             cachedArray = new ArrayType(cachedArray.buffer, 0, requiredLength);
         } else if (cachedArray.length !== requiredLength) {
@@ -15,10 +16,13 @@ export default () => {
 
     factory.type = (...args) => {
         if (!args.length) {
-            return ArrayType;
+            return type;
         }
-        ArrayType = typeMapper(args[0]);
-        cachedArray = new ArrayType(0);
+        if (type !== args[0]) {
+            type = args[0];
+            const ArrayType = getArrayViewConstructor(type);
+            cachedArray = new ArrayType(0);
+        }
         return factory;
     };
 
