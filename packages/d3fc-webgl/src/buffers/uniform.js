86ec49dbe784a9885ca12cc72b42aa906a8b75ce
Chris Price <cprice@scottlogic.co.uk>
2020-02-10 11:17:17 +0000

    refactor: rename uniformBuilder to uniform
    Also externalises the location property and updates the
    bufferBuilder -> uniform API

---
+++ packages/d3fc-webgl/src/buffers/uniform.js
@@ -0,0 +1,60 @@
+export default initialData => {
+    let location = -1;
+    let data = initialData;
+    let dirty = true;
+
+    const build = programBuilder => {
+        if (!dirty) {
+            return;
+        }
+
+        const gl = programBuilder.context();
+
+        if (Array.isArray(data)) {
+            switch (data.length) {
+                case 1:
+                    gl.uniform1fv(location, data);
+                    break;
+                case 2:
+                    gl.uniform2fv(location, data);
+                    break;
+                case 3:
+                    gl.uniform3fv(location, data);
+                    break;
+                case 4:
+                    gl.uniform4fv(location, data);
+                    break;
+                default:
+                    throw new Error(
+                        `Uniform supports up to 4 elements. ${data.length} provided.`
+                    );
+            }
+        } else {
+            gl.uniform1f(location, data);
+        }
+
+        dirty = false;
+    };
+
+    build.location = (...args) => {
+        if (!args.length) {
+            return location;
+        }
+        if (location !== args[0]) {
+            location = args[0];
+            dirty = true;
+        }
+        return build;
+    };
+
+    build.data = (...args) => {
+        if (!args.length) {
+            return data;
+        }
+        data = args[0];
+        dirty = true;
+        return build;
+    };
+
+    return build;
+};
