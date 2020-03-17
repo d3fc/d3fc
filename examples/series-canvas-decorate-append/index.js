8751199ac6cceba4f5a5b8f1abcd65b4908b7ab4
OliverForeman <oliverforeman@btinternet.com>
2020-03-17 11:30:10 +0000

    refactor: moved series-decorate-append examples to examples directory

---
+++ examples/series-canvas-decorate-append/index.js
@@ -0,0 +1,44 @@
+const data = fc.randomGeometricBrownianMotion().steps(25)(1);
+
+const container = document.querySelector('d3fc-canvas');
+const margin = 15;
+
+const extent = fc.extentLinear();
+
+const xScale = d3.scaleLinear().domain([0, data.length]);
+
+const yScale = d3.scaleLinear().domain(extent.pad([0.1, 0.1])(data));
+
+const ctx = d3
+    .select(container)
+    .select('canvas')
+    .node()
+    .getContext('2d');
+
+const series = fc
+    .seriesCanvasPoint()
+    .xScale(xScale)
+    .yScale(yScale)
+    .crossValue((_, i) => i)
+    .mainValue(d => d)
+    .context(ctx)
+    .decorate((context, datum) => {
+        context.textAlign = 'center';
+        context.fillStyle = '#000';
+        context.font = '15px Arial';
+        context.fillText(d3.format('.3f')(datum), 0, -10);
+        // reset the fill style for the point rendering
+        context.fillStyle = '#999';
+    });
+
+d3.select(container)
+    .on('draw', () => {
+        series(data);
+    })
+    .on('measure', () => {
+        const { width, height } = event.detail;
+        xScale.range([margin, width - margin * 2]);
+        yScale.range([height, 0]);
+    });
+
+container.requestRedraw();
