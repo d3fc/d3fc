6addd6da7fed4c3049ab55badeed626829ce593e
oliverforeman <oliverforeman@btinternet.com>
2020-03-31 09:00:59 +0100

    refactor: added d3fc-group elements to handle redrawing

--- examples/axis-label-offset/index.js
+++ examples/axis-label-offset/index.js
@@ -17,47 +17,42 @@ const domain = [
     'Popcorn'
 ];
 
-const draw = () => {
-    const renderAxis = (axisFactory, selector) => {
-        const container = document.querySelector(selector);
+const renderAxis = (axisFactory, selector) => {
+    const container = document.querySelector(selector);
 
-        const scale = d3.scaleBand().domain(domain);
-        const axis = axisFactory(scale);
+    const scale = d3.scaleBand().domain(domain);
+    const axis = axisFactory(scale);
 
-        d3.select(container)
-            .on('draw', () => {
-                d3.select(container)
-                    .select('svg')
-                    .call(axis);
-            })
-            .on('measure', () => {
-                const { width, height } = event.detail;
-                scale.range([
-                    0,
-                    container.getAttribute('data-orient') === 'vertical'
-                        ? height
-                        : width
-                ]);
+    d3.select(container)
+        .on('draw', () => {
+            d3.select(container)
+                .select('svg')
+                .call(axis);
+        })
+        .on('measure', () => {
+            const { width, height } = event.detail;
+            scale.range([
+                0,
+                container.getAttribute('data-orient') === 'vertical'
+                    ? height
+                    : width
+            ]);
 
-                const containerId = container.getAttribute('id');
-                const topOffset = containerId === 'top' ? 80 : 0;
-                const leftOffset = containerId === 'left' ? 160 : 0;
+            const containerId = container.getAttribute('id');
+            const topOffset = containerId === 'top' ? 80 : 0;
+            const leftOffset = containerId === 'left' ? 160 : 0;
 
-                d3.select(container)
-                    .select('svg')
-                    .attr(
-                        'viewBox',
-                        `${-leftOffset} ${-topOffset} ${width} ${height}`
-                    );
-            });
-        container.requestRedraw();
-    };
-
-    renderAxis(scale => fc.axisLabelOffset(fc.axisBottom(scale)), '#bottom');
-    renderAxis(scale => fc.axisLabelOffset(fc.axisTop(scale)), '#top');
-    renderAxis(scale => fc.axisLabelOffset(fc.axisLeft(scale)), '#left');
-    renderAxis(scale => fc.axisLabelOffset(fc.axisRight(scale)), '#right');
+            d3.select(container)
+                .select('svg')
+                .attr(
+                    'viewBox',
+                    `${-leftOffset} ${-topOffset} ${width} ${height}`
+                );
+        });
+    container.requestRedraw();
 };
 
-draw();
-window.addEventListener('resize', () => draw());
+renderAxis(scale => fc.axisLabelOffset(fc.axisBottom(scale)), '#bottom');
+renderAxis(scale => fc.axisLabelOffset(fc.axisTop(scale)), '#top');
+renderAxis(scale => fc.axisLabelOffset(fc.axisLeft(scale)), '#left');
+renderAxis(scale => fc.axisLabelOffset(fc.axisRight(scale)), '#right');
