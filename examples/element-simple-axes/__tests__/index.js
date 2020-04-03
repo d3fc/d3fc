4f7004a6f57e8bfcee9fa2ffaf10122626e0dd1f
Adil Parvez <unknown>
2020-04-03 13:30:43 +0100

    refactor: delete unnecessary examples

--- examples/element-simple-axes/__tests__/index.js
+++
@@ -1,9 +0,0 @@
-const { join } = require('path');
-
-it('should look good on the website!', async () => {
-    await page.goto('file://' + join(__dirname, '..', 'index.html'));
-    await page.screenshot({
-        path: join(__dirname, '..', 'screenshot.png'),
-        omitBackground: true
-    });
-});
