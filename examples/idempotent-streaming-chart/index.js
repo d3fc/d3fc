a4fa206c1866f0ea163a4d1b835abdf8f6235d42
Chris Price <cprice@scottlogic.co.uk>
2020-04-07 14:38:03 +0100

    docs: eslint fixes

--- examples/idempotent-streaming-chart/index.js
+++ examples/idempotent-streaming-chart/index.js
@@ -8,27 +8,34 @@ function renderChart() {
     data.shift();
 
     // Create and apply the bollinger algorithm
-    const bollingerAlgorithm = fc.indicatorBollingerBands()
-        .value(d => d.close);
+    const bollingerAlgorithm = fc.indicatorBollingerBands().value(d => d.close);
     const bollingerData = bollingerAlgorithm(data);
-    const mergedData = data.map((d, i) => ({ ...d, bollinger: bollingerData[i] }));
+    const mergedData = data.map((d, i) => ({
+        ...d,
+        bollinger: bollingerData[i]
+    }));
 
     // Offset the range to include the full bar for the latest value
     const DAY_MS = 1000 * 60 * 60 * 24;
-    const xExtent = fc.extentDate()
+    const xExtent = fc
+        .extentDate()
         .accessors([d => d.date])
         .padUnit('domain')
         .pad([DAY_MS * -bollingerAlgorithm.period()(mergedData), DAY_MS]);
 
     // ensure y extent includes the bollinger bands
-    const yExtent = fc.extentLinear()
-        .accessors([d => d.bollinger.upper, d => d.high, d => d.bollinger.lower, d => d.low]);
+    const yExtent = fc
+        .extentLinear()
+        .accessors([
+            d => d.bollinger.upper,
+            d => d.high,
+            d => d.bollinger.lower,
+            d => d.low
+        ]);
 
     // create a chart
-    const chart = fc.chartCartesian(
-        d3.scaleTime(),
-        d3.scaleLinear()
-    )
+    const chart = fc
+        .chartCartesian(d3.scaleTime(), d3.scaleLinear())
         .xDomain(xExtent(mergedData))
         .yDomain(yExtent(mergedData))
         .chartLabel('Streaming Idempotent Chart');
@@ -37,36 +44,46 @@ function renderChart() {
     const gridlines = fc.annotationSvgGridline();
     const candlestick = fc.seriesSvgCandlestick();
 
-    const bollingerBands = function () {
-        const area = fc.seriesSvgArea()
-            .mainValue((d) => d.bollinger.upper)
-            .baseValue((d) => d.bollinger.lower)
-            .crossValue((d) => d.date);;
+    const bollingerBands = function() {
+        const area = fc
+            .seriesSvgArea()
+            .mainValue(d => d.bollinger.upper)
+            .baseValue(d => d.bollinger.lower)
+            .crossValue(d => d.date);
 
-        const upperLine = fc.seriesSvgLine()
-            .mainValue((d) => d.bollinger.upper)
-            .crossValue((d) => d.date);;
+        const upperLine = fc
+            .seriesSvgLine()
+            .mainValue(d => d.bollinger.upper)
+            .crossValue(d => d.date);
 
-        const averageLine = fc.seriesSvgLine()
-            .mainValue((d) => d.bollinger.average)
-            .crossValue((d) => d.date);;
+        const averageLine = fc
+            .seriesSvgLine()
+            .mainValue(d => d.bollinger.average)
+            .crossValue(d => d.date);
 
-        const lowerLine = fc.seriesSvgLine()
-            .mainValue((d) => d.bollinger.lower)
-            .crossValue((d) => d.date);;
+        const lowerLine = fc
+            .seriesSvgLine()
+            .mainValue(d => d.bollinger.lower)
+            .crossValue(d => d.date);
 
-        const bollingerMulti = fc.seriesSvgMulti()
+        const bollingerMulti = fc
+            .seriesSvgMulti()
             .series([area, upperLine, lowerLine, averageLine])
             .decorate(g => {
-                g.enter()
-                    .attr('class', (_, i) => 'multi bollinger ' + ['area', 'upper', 'lower', 'average'][i]);
+                g.enter().attr(
+                    'class',
+                    (_, i) =>
+                        'multi bollinger ' +
+                        ['area', 'upper', 'lower', 'average'][i]
+                );
             });
 
         return bollingerMulti;
     };
 
     // add them to the chart via a multi-series
-    const multi = fc.seriesSvgMulti()
+    const multi = fc
+        .seriesSvgMulti()
         .series([gridlines, bollingerBands(), candlestick]);
 
     chart.svgPlotArea(multi);
