0efd4fdad21239be585df317b29810f1f841eaad
Chris Price <cprice@scottlogic.co.uk>
2020-04-09 10:26:30 +0100

    test: add consistent image snapshot testing

--- examples/series-webgl-area/__tests__/index.js
+++ examples/series-webgl-area/__tests__/index.js
@@ -1,19 +1,22 @@
 const { join } = require('path');
 
+it('should match the image snapshot', async () => {
+    await d3fc.loadExample(module);
+    const image = await page.screenshot();
+    expect(image).toMatchImageSnapshot();
+});
+
 it('should have consistent performance', async () => {
     await expect(async () => {
         await d3fc.loadExample(module);
         await d3fc.waitForEmptyRedrawQueue();
-        for (let i = 1; i < 6; i += 1) {
+        for (let i = 0; i < 6; i++) {
             await page.click('d3fc-canvas');
             await d3fc.waitForEmptyRedrawQueue();
         }
     }).toHaveConsistentPerformance();
 
     expect(page).not.toHaveLogs();
-
-    const image = await page.screenshot();
-    expect(image).toMatchImageSnapshot();
 });
 
 it('should look good on the website!', async () => {
--- examples/technical-indicator-macd/__tests__/index.js
+++ examples/technical-indicator-macd/__tests__/index.js
@@ -1,5 +1,11 @@
 const { join } = require('path');
 
+it('should match the image snapshot', async () => {
+    await d3fc.loadExample(module);
+    const image = await page.screenshot();
+    expect(image).toMatchImageSnapshot();
+});
+
 it('should look good on the website!', async () => {
     await d3fc.loadExample(module);
     await page.screenshot({
