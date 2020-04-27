b0bb658acaa0b8674d22ed4561c9d35498225c91
Chris Price <cprice@scottlogic.co.uk>
2020-04-27 17:43:16 +0100

    chore: remove d3fc-site package

--- packages/d3fc-site/builder/handlebars-helpers/first.js
+++
@@ -1,9 +0,0 @@
-function register(handlebars) {
-  handlebars.registerHelper('first', function(array, data) {
-    return data.fn(array[0]);
-  });
-}
-
-module.exports = {
-  register: register
-};
