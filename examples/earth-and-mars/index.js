a3f149847faacfdf8c18ffae0f71390b2267dd1d
Chris Price <cprice@scottlogic.co.uk>
2020-04-28 13:18:09 +0100

    docs: simplify earth and mars example

--- examples/earth-and-mars/index.js
+++ examples/earth-and-mars/index.js
@@ -1,206 +1,82 @@
-// Chart options.
-let options = {
-    numPoints: 100000,
-    chartType: 'WebGL',
-    enableMultiColor: false
-};
-
-function getFormValues() {
-    return {
-        numPoints: Number(document.getElementById('numPoints').value),
-        chartType: document.getElementById('chartType').value,
-        enableMultiColor: document.getElementById('enableMultiColor').checked
-    };
-}
-
-function setEnableMultiColorVisible(state) {
-    const element = document.getElementById('enableMultiColorWrapper');
-
-    if (state) {
-        element.style.display = 'block';
-    } else {
-        element.style.display = 'none';
-    }
-}
-
-// eslint-disable-next-line no-unused-vars
-function onChange() {
-    options = getFormValues();
-
-    switch (options.chartType) {
-        case 'WebGL':
-            setEnableMultiColorVisible(true);
-            break;
-        case 'Canvas':
-            setEnableMultiColorVisible(false);
-            break;
-    }
-
-    updateAndRender();
-}
-
 // Set up webworker to calculate data.
-let data = [];
-let currentData;
-
 const worker = new Worker('worker.js');
 worker.onmessage = e => {
-    data = e.data;
-    currentData = data.slice(0, options.numPoints);
+    const data = e.data;
 
-    x.domain([currentData[0].date, currentData[currentData.length - 1].date]);
-    x2 = x.copy();
+    // Draw chart.
+    const xScale = d3.scaleTime().domain([data[0].date, data[1000].date]);
+    const xScaleCopy = xScale.copy();
+    const yScale = d3.scaleLinear().domain([0, 400]);
 
-    updateAndRender();
-};
-worker.postMessage({ numPoints: options.numPoints });
+    const crossValue = d => d.date;
+    const mainValue = d => d.distance;
 
-// Draw chart.
-const x = d3.scaleTime();
-const y = d3.scaleLinear();
-
-y.domain([0, 400]);
-let x2 = x.copy();
-
-const crossValue = d => d.date;
-const mainValue = d => d.distance;
-
-const blueGradientDecorate = program => {
-    program.fragmentShader().appendBody(`
-        vec4 color = vec4(70.0/255.0, 130.0/255.0, 180.0/255.0, 1.0);
-        float colorModifier = 1.0 - gl_FragCoord.y / 421.0;
-        colorModifier = smoothstep(0.875, 1.0, colorModifier);
-        gl_FragColor = vec4(color + ((1.0 - color) * colorModifier));
-    `);
-};
-
-const webglArea = fc
-    .seriesWebglArea()
-    .mainValue(mainValue)
-    .crossValue(crossValue)
-    .defined(() => true)
-    .equals(d => d.length)
-    .decorate(blueGradientDecorate);
-
-const webglLine = fc
-    .seriesWebglLine()
-    .mainValue(mainValue)
-    .crossValue(crossValue)
-    .defined(() => true)
-    .equals(d => d.length);
-
-const webglSeries = fc.seriesWebglMulti().series([webglArea, webglLine]);
-
-const canvasArea = fc
-    .seriesCanvasArea()
-    .mainValue(mainValue)
-    .crossValue(crossValue)
-    .decorate(context => {
-        const canvasHeight = d3.select('.plot-area').node().offsetHeight;
-
-        const grd = context.createLinearGradient(
-            0,
-            canvasHeight * 0.875,
-            0,
-            canvasHeight
-        );
-        grd.addColorStop(0, 'steelblue');
-        grd.addColorStop(1, 'white');
-
-        context.fillStyle = grd;
-    });
-
-const canvasLine = fc
-    .seriesCanvasLine()
-    .mainValue(mainValue)
-    .crossValue(crossValue);
-
-const canvasSeries = fc.seriesCanvasMulti().series([canvasArea, canvasLine]);
-
-const zoom = d3.zoom().on('zoom', () => {
-    x.domain(d3.event.transform.rescaleX(x2).domain());
-    render();
-});
-
-const multiColorAreaDecorate = program => {
-    program.vertexShader().appendHeader(`varying lowp vec4 vColor;`)
-        .appendBody(`float colourModifier = smoothstep(50.0, 400.0, aMainValue);
-        vColor = (vec4(0.55, 0.65, 0.75, 1) * colourModifier) + ((1.0 - colourModifier) * vec4(0.75, 0.45, 0.45, 1));
-        float verticalFade = max(0.0, smoothstep(-1.1, -0.9, gl_Position.y) - 0.15);
-        vColor.a = vColor.a * verticalFade;
-    `);
-
-    program.fragmentShader().appendHeader(`
-        varying lowp vec4 vColor;
-    `).appendBody(`
-        gl_FragColor = vColor;
-    `);
-};
-
-const multiColorLineDecorate = program => {
-    program.vertexShader().appendHeader(`varying lowp vec4 vColor;`)
-        .appendBody(`float colourModifier = smoothstep(-0.875, 1.0, gl_Position.y);
-            vColor = (vec4(0.55, 0.65, 0.75, 1) * colourModifier) + ((1.0 - colourModifier) * vec4(0.75, 0.45, 0.45, 1));`);
-
-    program.fragmentShader().appendHeader(`varying lowp vec4 vColor;`)
-        .appendBody(`
+    const areaSeries = fc
+        .seriesWebglArea()
+        .mainValue(mainValue)
+        .crossValue(crossValue)
+        .defined(() => true)
+        .equals(d => d.length)
+        .decorate(program => {
+            program.vertexShader().appendHeader(`varying lowp vec4 vColor;`)
+                .appendBody(`float colourModifier = smoothstep(50.0, 400.0, aMainValue);
+            vColor = (vec4(0.55, 0.65, 0.75, 1) * colourModifier) + ((1.0 - colourModifier) * vec4(0.75, 0.45, 0.45, 1));
+            float verticalFade = max(0.0, smoothstep(-1.1, -0.9, gl_Position.y) - 0.15);
+            vColor.a = vColor.a * verticalFade;
+        `);
+            program.fragmentShader().appendHeader(`
+            varying lowp vec4 vColor;
+        `).appendBody(`
             gl_FragColor = vColor;
-    `);
-};
+        `);
+        });
 
-function updateChart() {
-    currentData = data.slice(0, options.numPoints);
+    const lineSeries = fc
+        .seriesWebglLine()
+        .mainValue(mainValue)
+        .crossValue(crossValue)
+        .defined(() => true)
+        .equals(d => d.length)
+        .lineWidth(2)
+        .decorate(program => {
+            program.vertexShader().appendHeader(`varying lowp vec4 vColor;`)
+                .appendBody(`float colourModifier = smoothstep(-0.875, 1.0, gl_Position.y);
+                    vColor = (vec4(0.55, 0.65, 0.75, 1) * colourModifier) + ((1.0 - colourModifier) * vec4(0.75, 0.45, 0.45, 1));`);
+            program.fragmentShader().appendHeader(`varying lowp vec4 vColor;`)
+                .appendBody(`
+                    gl_FragColor = vColor;
+            `);
+        });
 
-    webglSeries
-        .xScale()
-        .domain([
-            currentData[0].date,
-            currentData[currentData.length - 1].date
-        ]);
-    canvasSeries
-        .xScale()
-        .domain([
-            currentData[0].date,
-            currentData[currentData.length - 1].date
-        ]);
+    const multiSeries = fc.seriesWebglMulti().series([areaSeries, lineSeries]);
 
-    const [areaSeries, lineSeries] = webglSeries.series();
-    areaSeries.decorate(
-        options.enableMultiColor ? multiColorAreaDecorate : blueGradientDecorate
-    );
-    lineSeries
-        .lineWidth(options.enableMultiColor ? 2 : 1)
-        .decorate(options.enableMultiColor ? multiColorLineDecorate : () => {});
-
-    chart
-        .canvasPlotArea(options.chartType === 'Canvas' ? canvasSeries : null)
-        .webglPlotArea(options.chartType === 'WebGL' ? webglSeries : null);
-
-    zoom.transform(d3.select('.plot-area'), d3.zoomIdentity);
-}
-
-function render() {
-    d3.select('#chart')
-        .datum(currentData)
-        .call(chart);
-}
-
-function updateAndRender() {
-    updateChart();
-    render();
-}
-
-const chart = fc
-    .chartCartesian(x, y)
-    .yOrient('left')
-    .chartLabel('The distance between Earth and Mars over time')
-    .xLabel('Year')
-    .yLabel('Distance (million Km)')
-    .decorate(selection => {
-        selection
-            .select('.plot-area')
-            .on('measure.range', () => {
-                x2.range([0, d3.event.detail.width]);
-            })
-            .call(zoom);
+    const zoom = d3.zoom().on('zoom', () => {
+        xScale.domain(d3.event.transform.rescaleX(xScaleCopy).domain());
+        render();
     });
+
+    const chart = fc
+        .chartCartesian(xScale, yScale)
+        .yOrient('left')
+        .chartLabel('The distance between Earth and Mars over time')
+        .xLabel('Year')
+        .yLabel('Distance (million Km)')
+        .webglPlotArea(multiSeries)
+        .decorate(selection => {
+            selection
+                .select('.plot-area')
+                .on('measure.range', () => {
+                    xScaleCopy.range([0, d3.event.detail.width]);
+                })
+                .call(zoom);
+        });
+
+    function render() {
+        d3.select('#chart')
+            .datum(data)
+            .call(chart);
+    }
+
+    render();
+};
+worker.postMessage({ numPoints: 100000 });
