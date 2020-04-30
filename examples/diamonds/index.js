e23f65b75ee1caaf7ab57c03878f66e8dab4cb20
Chris Price <cprice@scottlogic.co.uk>
2020-04-30 13:45:58 +0100

    docs: improve example styling

--- examples/diamonds/index.js
+++ examples/diamonds/index.js
@@ -22,24 +22,46 @@ d3.tsv('diamond-data.tsv', d => ({
         .seriesWebglPoint()
         .crossValue(d => d.carat)
         .mainValue(d => d.price)
-        .size(4)
+        .size(d =>
+            Math.pow(
+                Math.max(2, xScaleCopy(d.carat + 0.01) - xScaleCopy(d.carat)),
+                2
+            )
+        )
+        .type(d3.symbolSquare)
         .defined(() => true)
         .equals(d => d.length)
         .decorate(program => {
-            fc.webglFillColor([0.0, 0.0, 0.5, 0.3])(program);
+            fc.webglFillColor([60 / 255, 180 / 255, 240 / 255, 1.0])(program);
+
+            program.fragmentShader().appendBody(`
+                if (gl_PointCoord.y > 0.6 || gl_PointCoord.y < 0.4) {
+                    discard;
+                }
+            `);
 
             const gl = program.context();
+            gl.clearColor(1.0, 1.0, 1.0, 0.0);
+            gl.clear(gl.COLOR_BUFFER_BIT);
             gl.enable(gl.BLEND);
-            gl.blendFunc(gl.SRC_ALPHA, gl.ONE);
+            gl.blendColor(0.0, 0.0, 0.0, 1.0);
+            gl.blendFuncSeparate(
+                gl.DST_COLOR,
+                gl.ZERO,
+                gl.CONSTANT_ALPHA,
+                gl.ZERO
+            );
         });
 
     const chart = fc
         .chartCartesian(xScale, yScale)
         .svgPlotArea(gridlines)
         .webglPlotArea(pointSeries)
-        .xLabel('Mass (carats)')
-        .yLabel('Price (US$)')
-        .yTickFormat(d3.format('.3s'))
+        .yOrient('left')
+        .xLabel('Carats →')
+        .yLabel('↑ Price $')
+        .xTickFormat(d3.format('.1f'))
+        .yTickFormat(d3.format('.1s'))
         .decorate(selection => {
             selection
                 .enter()
