00a527397e64e279c7d6ec7443de2805a38c6faf
Chris Price <cprice@scottlogic.co.uk>
2020-05-04 08:55:02 +0100

    docs: improve stars example aesthetics

--- examples/webgl-with-svg-overlay/__tests__/index.js
+++ examples/webgl-with-svg-overlay/__tests__/index.js
@@ -2,12 +2,14 @@ const { join } = require('path');
 
 it('should match the image snapshot', async () => {
     await d3fc.loadExample(module);
-    const image = await page.screenshot();
+    const element = await page.waitForSelector('.webgl-plot-area');
+    const image = await element.screenshot();
     expect(image).toMatchImageSnapshot();
 });
 
 it('should look good on the website!', async () => {
     await d3fc.loadExample(module);
+    await page.waitForSelector('.webgl-plot-area');
     await page.screenshot({
         path: join(__dirname, '..', 'screenshot.png'),
         omitBackground: true
