b939ac3b735a6536cdd778b8b73090b42fb01387
Chris Price <cprice@scottlogic.co.uk>
2020-05-04 10:16:43 +0100

    docs: fix determinism when generating screenshots

--- examples/annotation-gridline-transition/__tests__/index.js
+++ examples/annotation-gridline-transition/__tests__/index.js
@@ -3,10 +3,7 @@ const { join } = require('path');
 it('should match the image snapshot', async () => {
     await d3fc.loadExample(module);
     const image = await page.screenshot();
-    expect(image).toMatchImageSnapshot({
-        failureThreshold: 1,
-        failureThresholdType: 'percent'
-    });
+    expect(image).toMatchImageSnapshot();
 });
 
 it('should look good on the website!', async () => {
