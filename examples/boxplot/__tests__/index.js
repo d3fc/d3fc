e408dfccf8344eb4475d2034dc3c87ce6348e42d
Colin E <colin.eberhardt@gmail.com>
2020-05-12 22:43:51 +0100

    docs: added boxplot example

---
+++ examples/boxplot/__tests__/index.js
@@ -0,0 +1,9 @@
+it('should match the image snapshot goop', async () => {
+    await d3fc.loadExample(module);
+    await page.waitForSelector('.cartesian-chart');
+    const image = await page.screenshot({
+        omitBackground: true
+    });
+    expect(image).toMatchImageSnapshot();
+    await d3fc.saveScreenshot(module, image);
+});
