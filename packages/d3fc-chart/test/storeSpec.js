26273a01950915a687f303a36f3fa26306fda516
Martin Moxon <moxon6@googlemail.com>
2021-04-02 00:33:22 +0100

    Add d3fc-chart unit test for store

--- packages/d3fc-chart/test/storeSpec.js
+++ packages/d3fc-chart/test/storeSpec.js
@@ -33,4 +33,16 @@ describe('store', () => {
 
         expect(componentInstance.ticksValue).toEqual([22, 's']);
     });
+
+    it('should throw when target is missing required method', function() {
+        // store values for 'ticks' property
+        let myStore = store('ticks', 'anotherTicks');
+        myStore.ticks(22, 's');
+        myStore.anotherTicks(23, 't');
+
+        // re-apply these to a new component
+        const componentInstance = component();
+        
+        expect(() => myStore(componentInstance)).toThrow();
+    })
 });
