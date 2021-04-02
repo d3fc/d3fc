95115b629b945ad73a60b2b62645461413d78209
Martin Moxon <moxon6@googlemail.com>
2021-04-02 17:34:03 +0100

    Setup test-d

--- packages/d3fc-chart/src/cartesian.test-d.ts
+++ packages/d3fc-chart/src/cartesian.test-d.ts
@@ -1,25 +1,28 @@
-import d3 from 'd3';
+import * as d3 from 'd3';
 import chartCartesian from './cartesian';
 import { extentLinear, extentDate } from "../../d3fc-extent"
 import { randomFinancial } from '../../d3fc-random-data'
 import { annotationSvgGridline } from '../../d3fc-annotation';
 import { seriesSvgCandlestick, seriesSvgMulti } from '../../d3fc-series'
+import { expectType } from 'tsd';
 
-const data = randomFinancial()(50);
+it('should be able to create a chart', () => {
+    const data = randomFinancial()(50);
 
-const yExtent = extentLinear().accessors([d => d.high, d => d.low]) as any;
+    const yExtent = extentLinear().accessors([(d: any) => d.high, (d: any) => d.low]) as any;
 
-const xExtent = extentDate().accessors([d => d.date]) as any;
+    const xExtent = extentDate().accessors([(d: any) => d.date]) as any;
 
-const gridlines = annotationSvgGridline();
-const candlestick = seriesSvgCandlestick();
-const multi = (seriesSvgMulti() as any).series([gridlines, candlestick]);
+    const gridlines = annotationSvgGridline();
+    const candlestick = seriesSvgCandlestick();
+    const multi = (seriesSvgMulti() as any).series([gridlines, candlestick]);
 
-const chart = chartCartesian(d3.scaleTime(), d3.scaleLinear())
-    .yDomain(yExtent(data))
-    .xDomain(xExtent(data))
-    .svgPlotArea(multi);
+    const chart = chartCartesian(d3.scaleTime(), d3.scaleLinear())
+        .yDomain(yExtent(data))
+        .xDomain(xExtent(data))
+        .svgPlotArea(multi);
 
-d3.select('#chart')
-    .datum(data)
-    .call(chart);
+    d3.select('#chart')
+        .datum(data)
+        .call(chart);
+})
