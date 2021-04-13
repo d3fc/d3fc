cbe9ff2ee62c541e9cbd37c218430f0389361343
Martin Moxon <moxon6@googlemail.com>
2021-04-13 14:09:19 +0100

    Update definitions and tests

--- packages/d3fc-chart/src/cartesian.d.ts
+++ packages/d3fc-chart/src/cartesian.d.ts
@@ -1,4 +1,5 @@
-import { TStore, IStoreProperty } from './store'
+import type { ScaleIdentity, ScaleLinear } from 'd3-scale'
+import type { TStore, IStoreProperty } from './store'
 
 export type Functor<T> = ((...args: any[]) => T);
 type TypeOrFunctor<T> = T | Functor<T>;
@@ -8,63 +9,51 @@ type YOrient = 'left' | 'right' | 'none';
 
 interface WebglPlotArea {
     (d: any): any
-    context(canvas: HTMLCanvasElement): WebglPlotArea
-    pixelRatio(pixelRatio: number): WebglPlotArea,
-    xScale(scale: any): WebglPlotArea,
-    yScale(scale: any): WebglPlotArea,
+    context(canvas: HTMLCanvasElement): this
+    pixelRatio(pixelRatio: number): this,
+    xScale(scale: any): this,
+    yScale(scale: any): this,
 }
 
 interface CanvasPlotArea {
     (d: any): any,
-    context(canvas: HTMLCanvasElement): CanvasPlotArea,
-    xScale(scale: any): CanvasPlotArea,
-    yScale(scale: any): CanvasPlotArea,
+    context(canvas: HTMLCanvasElement): this,
+    xScale(scale: any): this,
+    yScale(scale: any): this,
 }
 
 interface SvgPlotArea {
     (d: any): any,
-    xScale(scale: any): SvgPlotArea,
-    yScale(scale: any): SvgPlotArea,
+    xScale(scale: any): this,
+    yScale(scale: any): this,
 }
 
 type Decorator = (container: d3.Selection<any, any, any, any>, data: any, index: number) => void
 
-type StoreProperties = 'tickFormat' | 'ticks' | 'tickArguments' | 'tickSize' | 'tickSizeInner' | 'tickSizeOuter' | 'tickValues' | 'tickPadding' | 'tickCenterLabel';
-type XAxisStoreProperties = `x${Capitalize<StoreProperties>}`
-type YAxisStoreProperties = `y${Capitalize<StoreProperties>}`
-
-type XAxisStore = TStore<XAxisStoreProperties>;
-type YAxisStore = TStore<YAxisStoreProperties>;
-
-type GetterSetter<TThis, TValue, TSetValue> = {
-    (): TValue
-    (value: TSetValue): TThis,
-}
-
-type CartesianChartScale<Scale, XScale, YScale, Prefix extends string> = {
-    [Property in keyof Scale as `${Prefix}${Capitalize<string & Property>}`]: Scale[Property] extends GetterSetter<any, infer U, infer V>
-    ? GetterSetter<CartesianChart<XScale, YScale>, U, V>
-    : Scale[Property]
-}
-
-type RebindAxisStore<XScale, YScale, Axis> = {
-    [Property in keyof Axis]: Axis[Property] extends IStoreProperty<Axis>
-    ? IStoreProperty<CartesianChart<XScale, YScale>>
-    : never
-}
-
-export type CartesianChartArgs<XScale, YScale> = [xScale: XScale, yScale?: YScale] | [{
+export type CartesianChartArgs<XScale, YScale> = [xScale?: XScale, yScale?: YScale] | [{
     xScale?: XScale,
     yScale?: YScale,
-    xAxis: {
+    xAxis?: {
         top: any
         bottom: any
     },
-    yAxis: {
+    yAxis?: {
         left: any
         right: any
     }
 }]
+
+type Function = (...args: any[]) => any;
+
+type PrefixProperties<T, Prefix extends string> = {
+    [Property in keyof T as `${Prefix}${Capitalize<string & Property>}`]: T[Property]
+}
+
+type AnyMethods<T> = {
+    [Property in keyof T]: T[Property] extends Function ? Function : T[Property]
+}
+
+type Store = TStore<'tickFormat' | 'ticks' | 'tickArguments' | 'tickSize' | 'tickSizeInner' | 'tickSizeOuter' | 'tickValues' | 'tickPadding' | 'tickCenterLabel'>;
 
 export type CartesianChart<XScale, YScale> = {
     (selection: d3.Selection<any, any, any, any>): void;
@@ -111,9 +100,14 @@ export type CartesianChart<XScale, YScale> = {
     yOrient(): Functor<YOrient>;
     yOrient(orient: YOrient): CartesianChart<XScale, YScale>;
 }
-    & RebindAxisStore<XScale, YScale, XAxisStore>
-    & RebindAxisStore<XScale, YScale, YAxisStore>
-    & Omit<CartesianChartScale<XScale, XScale, YScale, 'x'>, XAxisStoreProperties>
-    & Omit<CartesianChartScale<YScale, XScale, YScale, 'y'>, YAxisStoreProperties>
+    & AnyMethods<PrefixProperties<XScale, 'x'>>
+    & AnyMethods<PrefixProperties<YScale, 'y'>>
+    & AnyMethods<PrefixProperties<Store, 'x'>>
+    & AnyMethods<PrefixProperties<Store, 'y'>>
 
-export default function Cartesian<XScale, YScale>(...args: CartesianChartArgs<XScale, YScale>): CartesianChart<XScale, YScale>
+export default function Cartesian<XScale, YScale>(...args: CartesianChartArgs<XScale, YScale>):
+    CartesianChart<
+        (unknown & undefined) extends XScale ? ScaleIdentity : XScale,
+        (unknown & undefined) extends YScale ? ScaleIdentity : YScale
+    >
+
--- packages/d3fc-chart/test/cartesianSpec.ts
+++ packages/d3fc-chart/test/cartesianSpec.ts
@@ -1,7 +1,7 @@
 import * as d3 from 'd3';
 import chartCartesian, { CartesianChart } from '../src/cartesian';
 import { expectType } from 'tsd'
-import { ScaleIdentity, ScaleLinear } from 'd3';
+import { ScaleIdentity, ScaleLinear, scaleTime, scaleLinear } from 'd3';
 
 describe('chartCartesian', () => {
     it('can accept zero scale parameters', () => {
@@ -14,7 +14,43 @@ describe('chartCartesian', () => {
         expectType<CartesianChart<ScaleLinear<number, number, never>, ScaleIdentity>>(chartWithFirstParameter);
 
         const chartWithSecondParameter = chartCartesian(undefined, d3.scaleLinear());
-        expectType<CartesianChart<ScaleLinear<number, number, never>, ScaleIdentity>>(chartWithSecondParameter);
+        expectType<CartesianChart<d3.ScaleIdentity<never>, d3.ScaleLinear<number, number, never>>>(chartWithSecondParameter);
+    })
+
+    it('can accept two scale parameters', () => {
+        const chart = chartCartesian(d3.scaleLinear(), scaleTime());
+        expectType<CartesianChart<d3.ScaleLinear<number, number, never>, d3.ScaleTime<number, number, never>>>(chart);
+    })
+
+    it('can accept a configuration object with one scale', () => {
+        const chartWithYScale = chartCartesian({
+            yScale: scaleLinear()
+        });
+        expectType<CartesianChart<d3.ScaleIdentity, d3.ScaleLinear<number, number, never>>>(chartWithYScale);
+    })
+
+    it('can accept a configuration object with both scales', () => {
+        const chartWithBothScales = chartCartesian({
+            xScale: scaleLinear(),
+            yScale: scaleLinear()
+        });
+        expectType<CartesianChart<d3.ScaleLinear<number, number, never>, d3.ScaleLinear<number, number, never>>>(chartWithBothScales);
+    })
+
+    it('can accept a configuration object with optional xaxis, yaxis parameters', () => {
+        const chartWithAxes = chartCartesian({
+            xScale: scaleLinear(),
+            yScale: scaleLinear(),
+            xAxis: {
+                top: null,
+                bottom: null
+            },
+            yAxis: {
+                left: null,
+                right: null
+            }
+        });
+        expectType<CartesianChart<d3.ScaleLinear<number, number, never>, d3.ScaleLinear<number, number, never>>>(chartWithAxes);
     })
 
     it('rebinds scale methods as untyped methods', () => {
