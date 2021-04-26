02a1e97ee6629d7806c699076b9aa3102cef579d
Martin Moxon <moxon6@googlemail.com>
2021-04-27 00:45:53 +0100

    feat: tidy up changes

--- packages/d3fc-chart/index.test-d.ts
+++
@@ -1,2 +0,0 @@
-import './test/cartesian.test-d';
-import './test/canvas/cartesian.test-d';
--- packages/d3fc-chart/test-d/canvasCartesian.test-d.ts
+++ packages/d3fc-chart/test-d/canvasCartesian.test-d.ts
@@ -1,6 +1,6 @@
 import * as d3 from 'd3';
-import { chartCanvasCartesian } from '../../index';
-import { CartesianBaseChart } from '../../src/cartesianBase';
+import { chartCanvasCartesian } from '../index';
+import { CartesianBaseChart } from '../src/cartesianBase';
 import { expectType } from 'tsd';
 import { ScaleIdentity, ScaleLinear, scaleTime, scaleLinear } from 'd3';
 
