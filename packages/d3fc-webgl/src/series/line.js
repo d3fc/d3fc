f6212a35e39b4bb023b2c8be98c7a9633660aaa1
Chris Price <price.c@gmail.com>
2022-05-30 20:38:08 +0100

    feat: simplify webgl line series to use SDF (#1718)
    Co-authored-by: Chris Price <cprice@scottlogic.co.uk>
    Co-authored-by: Colin Eberhardt <colin.eberhardt@gmail.com>

--- packages/d3fc-webgl/src/series/line.js
+++ packages/d3fc-webgl/src/series/line.js
@@ -1,10 +1,12 @@
 import baseScale from '../scale/base';
 import programBuilder from '../program/programBuilder';
-import lineShader from '../shaders/line/baseShader';
+import shaderBuilder, {
+    vertexShaderBase,
+    fragmentShaderBase
+} from '../shaders/shaderBuilder';
 import drawModes from '../program/drawModes';
 import { rebind } from '@d3fc/d3fc-rebind';
 import lineWidthShader from '../shaders/lineWidth';
-import * as vertexShaderSnippets from '../shaders/vertexShaderSnippets';
 import attribute from '../buffer/attribute';
 import elementIndices from '../buffer/elementIndices';
 import types from '../buffer/types';
@@ -13,47 +15,112 @@ import rebindCurry from '../rebindCurry';
 export default () => {
     const program = programBuilder()
         .mode(drawModes.TRIANGLES)
-        .subInstanceCount(12);
+        .subInstanceCount(6);
     let xScale = baseScale();
     let yScale = baseScale();
     let decorate = () => {};
     const lineWidth = lineWidthShader();
 
+    /*
+        Line segment from a to b has vertices A, B, C, D -
+
+          A |-------| B
+            | \     |
+            |a  \  b|
+            |     \ |
+          D |-------| C
+
+        |AD| = uStrokeWidth
+        |AB| = |ab| + uStrokeWidth
+
+        Fragment shader implemented using line segment SDF 
+        simplified for starting at the origin (a) -
+        https://www.iquilezles.org/www/articles/distfunctions2d/distfunctions2d.htm
+    */
     const cornerAttribute = attribute()
         .divisor(0)
-        .size(3)
+        .size(4)
         .type(types.BYTE)
         .data([
-            [-1, 0, 0],
-            [1, 1, 0],
-            [1, -1, 1],
-            [-1, 0, 1],
-            [1, 1, 1]
+            [-1, +1, 1, 0],
+            [+1, +1, 0, 1],
+            [+1, -1, 0, 1],
+            [-1, -1, 1, 0]
         ]);
 
     program
         .buffers()
-        .elementIndices(elementIndices([0, 1, 2, 1, 2, 3, 0, 2, 3, 2, 3, 4]))
+        .elementIndices(elementIndices([0, 1, 2, 2, 3, 0]))
         .attribute('aCorner', cornerAttribute);
 
     const draw = numElements => {
-        const shaderBuilder = lineShader();
-        program
-            .vertexShader(shaderBuilder.vertex())
-            .fragmentShader(shaderBuilder.fragment());
+        const vertexShader = shaderBuilder(vertexShaderBase);
+        const fragmentShader = shaderBuilder(fragmentShaderBase);
 
-        xScale(program, 'prev', 0);
-        yScale(program, 'prev', 1);
-        xScale(program, 'curr', 0);
-        yScale(program, 'curr', 1);
-        xScale(program, 'gl_Position', 0);
-        yScale(program, 'gl_Position', 1);
-        xScale(program, 'nextNext', 0);
-        yScale(program, 'nextNext', 1);
+        program.vertexShader(vertexShader).fragmentShader(fragmentShader);
 
-        program
-            .vertexShader()
-            .appendBody(vertexShaderSnippets.postScaleLine.body);
+        vertexShader.appendHeader(`
+            attribute vec4 aCorner;
+            attribute float aCrossValue;
+            attribute float aCrossNextValue;
+            attribute float aMainValue;
+            attribute float aMainNextValue;
+            attribute float aDefined;
+            attribute float aDefinedNext;
+
+            uniform float uStrokeWidth;
+            uniform vec2 uScreen;
+
+            varying float vLength;
+            varying vec2 vPosition;
+        `);
+
+        vertexShader.appendBody(`
+            vec4 value = vec4(aCrossValue, aMainValue, 0.0, 1.0);
+            vec4 nextValue = vec4(aCrossNextValue, aMainNextValue, 0.0, 1.0);
+        `);
+
+        xScale(program, 'value', 0);
+        xScale(program, 'nextValue', 0);
+        yScale(program, 'value', 1);
+        yScale(program, 'nextValue', 1);
+
+        vertexShader.appendBody(`
+            vec2 position = aCorner[2] * value.xy + aCorner[3] * nextValue.xy;
+
+            vec2 direction = normalize((nextValue.xy - value.xy) * uScreen);
+            vec2 normal = vec2(direction.y, -direction.x);
+            vec2 padding = ((uStrokeWidth / 2.0) / (uScreen / 2.0));
+            
+            padding *= aDefined * aDefinedNext;
+            position += (aCorner[0] * direction + aCorner[1] * normal) * padding;
+
+            gl_Position = vec4(position.x, position.y, 0.0, 1.0);
+
+            vLength = length((nextValue.xy - value.xy) * (uScreen / 2.0));
+            vPosition = aCorner.xy * (uStrokeWidth / 2.0);
+            vPosition.x += aCorner[3] * vLength;
+        `);
+
+        // all fragment shader inputs are pixel denominated
+
+        fragmentShader.appendHeader(`
+            uniform float uStrokeWidth;
+            varying float vLength;
+            varying vec2 vPosition;
+
+            float canFill = 0.0;
+            float canStroke = 1.0;
+        `);
+
+        fragmentShader.appendBody(`
+            vec2 position = vPosition;
+            position.x -= clamp(position.x, 0.0, vLength);
+            float sdf = length(position) - uStrokeWidth / 2.0;
+            if (sdf > 0.5) {
+                discard;
+            }
+        `);
 
         lineWidth(program);
 
@@ -90,13 +157,6 @@ export default () => {
     rebind(draw, lineWidth, 'lineWidth');
     rebindCurry(
         draw,
-        'crossPreviousValueAttribute',
-        program.buffers(),
-        'attribute',
-        'aCrossPrevValue'
-    );
-    rebindCurry(
-        draw,
         'crossValueAttribute',
         program.buffers(),
         'attribute',
@@ -111,20 +171,6 @@ export default () => {
     );
     rebindCurry(
         draw,
-        'crossNextNextValueAttribute',
-        program.buffers(),
-        'attribute',
-        'aCrossNextNextValue'
-    );
-    rebindCurry(
-        draw,
-        'mainPreviousValueAttribute',
-        program.buffers(),
-        'attribute',
-        'aMainPrevValue'
-    );
-    rebindCurry(
-        draw,
         'mainValueAttribute',
         program.buffers(),
         'attribute',
@@ -136,13 +182,6 @@ export default () => {
         program.buffers(),
         'attribute',
         'aMainNextValue'
-    );
-    rebindCurry(
-        draw,
-        'mainNextNextValueAttribute',
-        program.buffers(),
-        'attribute',
-        'aMainNextNextValue'
     );
     rebindCurry(
         draw,
