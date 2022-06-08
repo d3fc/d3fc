2115dc50a47d0cc489ca68fe70dbe88f1bab7e63
John Cambefort <john@cardiologs.com>
2022-06-08 11:26:35 -0400

    feat(d3fc-pointer): type pointer function

---
+++ packages/d3fc-pointer/src/pointer.d.ts
@@ -0,0 +1,8 @@
+import type {Selection, BaseType} from "d3-selection"
+
+export default function Pointer <
+    GElement extends BaseType,
+    Datum,
+    PElement extends BaseType,
+    PDatum
+>(selection: Selection<GElement, Datum, PElement, PDatum>): void;
