7d6fcd070e53fcf401c5d06c42d5902c8b5f3efd
murcikan@scottlogic.com <murcikan@scottlogic.com>
2022-06-21 14:14:11 +0100

    chore: fixed linting errors

--- examples/discontinuous-week-axis/index.js
+++ examples/discontinuous-week-axis/index.js
@@ -1,19 +1,34 @@
 const checkbox = document.getElementById('skip');
 
 // define non-trading time ranges for any day of the week
-const nonTradingHoursPattern =
-{
-    Monday: [["07:45", "08:30"], ["13:20", "19:00"]],
-    Tuesday: [["07:45", "08:30"], ["13:20", "19:00"]],
-    Wednesday: [["07:45", "08:30"], ["13:20", "19:00"]],
-    Thursday: [["07:45", "08:30"], ["13:20", "19:00"]],
-    Friday: [["07:45", "08:30"], ["13:20", "EOD"]],
-    Saturday: [["SOD", "EOD"]],
-    Sunday: [["SOD", "19:00"]]
+const nonTradingHoursPattern = {
+    Monday: [
+        ['07:45', '08:30'],
+        ['13:20', '19:00']
+    ],
+    Tuesday: [
+        ['07:45', '08:30'],
+        ['13:20', '19:00']
+    ],
+    Wednesday: [
+        ['07:45', '08:30'],
+        ['13:20', '19:00']
+    ],
+    Thursday: [
+        ['07:45', '08:30'],
+        ['13:20', '19:00']
+    ],
+    Friday: [
+        ['07:45', '08:30'],
+        ['13:20', 'EOD']
+    ],
+    Saturday: [['SOD', 'EOD']],
+    Sunday: [['SOD', '19:00']]
 };
 
 // create discontinous date range
-const dates = d3.timeMinute.range(new Date(2018, 0, 1), new Date(2018, 0, 8))
+const dates = d3.timeMinute
+    .range(new Date(2018, 0, 1), new Date(2018, 0, 8))
     .filter(dt => {
         const dow = dt.getDay();
         switch (dow) {
@@ -22,11 +37,13 @@ const dates = d3.timeMinute.range(new Date(2018, 0, 1), new Date(2018, 0, 8))
             case 3:
             case 4:
             case 5:
-                if ((dt.getHours() === 7 && dt.getMinutes() >= 45)
-                    || (dt.getHours() === 8 && dt.getMinutes() < 30)
-                    || (dt.getHours() === 13 && dt.getMinutes() >= 20)
-                    || (dt.getHours() >= 14 && dow != 5 && dt.getHours() < 19)
-                    || (dt.getHours() >= 14 && dow === 5)) {
+                if (
+                    (dt.getHours() === 7 && dt.getMinutes() >= 45) ||
+                    (dt.getHours() === 8 && dt.getMinutes() < 30) ||
+                    (dt.getHours() === 13 && dt.getMinutes() >= 20) ||
+                    (dt.getHours() >= 14 && dow !== 5 && dt.getHours() < 19) ||
+                    (dt.getHours() >= 14 && dow === 5)
+                ) {
                     return false;
                 }
                 return true;
@@ -41,10 +58,9 @@ const dates = d3.timeMinute.range(new Date(2018, 0, 1), new Date(2018, 0, 8))
 const data = fc.randomFinancial()(dates.length);
 
 for (let i = 0; i < dates.length; i++) {
-    //console.log(`Changing: ${data[i].date} to ${dates[i]}`);
+    // console.log(`Changing: ${data[i].date} to ${dates[i]}`);
     data[i].date = dates[i];
 }
-
 
 // use the date to determine the x extent, padding by one day at each end
 const xExtent = fc
@@ -69,7 +85,9 @@ const multi = fc.seriesSvgMulti().series([gridlines, candlestick]);
 // adapt the d3 time scale in a discontinuous scale that skips weekends
 const skipWeeklyPatternScale = fc
     .scaleDiscontinuous(d3.scaleTime())
-    .discontinuityProvider(fc.discontinuitySkipWeeklyPattern(nonTradingHoursPattern));
+    .discontinuityProvider(
+        fc.discontinuitySkipWeeklyPattern(nonTradingHoursPattern)
+    );
 
 function renderChart() {
     // create a chart
@@ -80,6 +98,7 @@ function renderChart() {
         )
         .xDomain(xExtent(data))
         .yDomain(yExtent(data))
+        .xTicks(30)
         .svgPlotArea(multi);
 
     // render the chart
