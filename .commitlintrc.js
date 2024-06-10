514a88a4cbd14ec444169918a5444f970ac50dca
Beth Pritchard <bpritchard@scottlogic.com>
2024-06-10 14:34:39 +0100

    fix: use dependabot sign off to match lint ignore

--- .commitlintrc.js
+++ .commitlintrc.js
@@ -1,5 +1,4 @@
 module.exports = {
-    extends: ['@commitlint/config-conventional'],
-    ignores: [(message) => /^Bumps \[.+]\(.+\) from .+ to .+\.$/m.test(message)],
-  }
-  
+  extends: ['@commitlint/config-conventional'],
+  ignores: [(message) => /Signed-off-by: dependabot\[bot]\s+<support@github\.com>$/m.test(message)],
+}
