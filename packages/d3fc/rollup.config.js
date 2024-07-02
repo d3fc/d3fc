2ff79e684298bb3206f352f611c8678346da87a8
Beth Pritchard <bpritchard@scottlogic.com>
2024-07-02 14:23:22 +0100

    chore: update babel

--- packages/d3fc/rollup.config.js
+++ packages/d3fc/rollup.config.js
@@ -1,4 +1,4 @@
-import babel from 'rollup-plugin-babel';
+import babel from '@rollup/plugin-babel';
 import nodeResolve from '@rollup/plugin-node-resolve';
 import terser from '@rollup/plugin-terser';
 import serve from 'rollup-plugin-serve';
