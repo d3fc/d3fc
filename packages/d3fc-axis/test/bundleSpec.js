a718bdccf2fbcf5969329ad0cc461e67db3a3500
Beth Pritchard <bpritchard@scottlogic.com>
2024-07-03 11:24:36 +0100

    fix: axis test

--- packages/d3fc-axis/test/bundleSpec.js
+++ packages/d3fc-axis/test/bundleSpec.js
@@ -1,23 +1,41 @@
-import jsdom from 'jsdom';
+import {JSDOM, VirtualConsole} from 'jsdom';
+import fs from 'fs';
 
 describe('bundle', () => {
     it('should correctly wire-up all the dependencies via their UMD-exposed globals', (done) => {
-        jsdom.env({
-            html: '<html></html>',
-            virtualConsole: jsdom.createVirtualConsole().sendTo({
-                error: done
-            }),
-            scripts: [
-                require.resolve('d3/dist/d3.js'),
-                require.resolve('../../../node_modules/@d3fc/d3fc-data-join/build/d3fc-data-join.js'),
-                require.resolve('../../../node_modules/@d3fc/d3fc-rebind/build/d3fc-rebind.js'),
-                require.resolve('../build/d3fc-axis.js')
-            ],
-            done: (_, win) => {
-                const axis = win.fc.axisBottom();
+
+        const virtualConsole = new VirtualConsole().sendTo({
+            error: done
+        });
+        const dom = new JSDOM('<html></html>', { virtualConsole, runScripts: 'dangerously' },);
+
+        const { window } = dom;
+
+        const loadScript = (filePath) => {
+            const scriptContent = fs.readFileSync(filePath, 'utf-8');
+            const scriptElement = window.document.createElement('script');
+            scriptElement.textContent = scriptContent;
+            window.document.head.appendChild(scriptElement);
+        };
+
+        const scripts = [
+            require.resolve('d3/dist/d3.js'),
+            require.resolve('../../../node_modules/@d3fc/d3fc-data-join/build/d3fc-data-join.js'),
+            require.resolve('../../../node_modules/@d3fc/d3fc-rebind/build/d3fc-rebind.js'),
+            require.resolve('../build/d3fc-axis.js')
+        ];
+
+        scripts.forEach(loadScript);
+
+        window.onload = () => {
+            try {
+                const axis = window.fc.axisBottom();
                 expect(axis).not.toBeUndefined();
                 done();
+            } catch (err) {
+                done(err);
             }
-        });
-    });
+        };
+    }
+);
 });
