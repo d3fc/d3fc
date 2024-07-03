e37697498867244b57bc09dc811b28085f8ea79c
Beth Pritchard <bpritchard@scottlogic.com>
2024-07-03 11:32:17 +0100

    fix: technical indicator test

--- packages/d3fc-technical-indicator/test/bundleSpec.js
+++ packages/d3fc-technical-indicator/test/bundleSpec.js
@@ -1,24 +1,42 @@
-import jsdom from 'jsdom';
+import { JSDOM, VirtualConsole } from 'jsdom';
+import fs from 'fs';
 
 describe('bundle', function() {
     it('should corectly wire-up all the dependencies via their UMD-exposed globals', function(done) {
-        jsdom.env({
-            html: '<html></html>',
-            virtualConsole: jsdom.createVirtualConsole().sendTo({
-                error: done
-            }),
-            scripts: [
-                require.resolve('d3/dist/d3.js'),
-                require.resolve('../../../node_modules/@d3fc/d3fc-rebind/build/d3fc-rebind.js'),
-                require.resolve('../build/d3fc-technical-indicator.js')
-            ],
-            done: (_, win) => {
+    
+        const virtualConsole = new VirtualConsole().sendTo({
+            error: done
+        });
+        const dom = new JSDOM('<html></html>', { virtualConsole, runScripts: 'dangerously' });
+
+        const { window } = dom;
+
+        const loadScript = (filePath) => {
+            const scriptContent = fs
+                .readFileSync(filePath, 'utf-8');   
+            const scriptElement = window.document.createElement('script');
+            scriptElement.textContent = scriptContent;
+            window.document.head.appendChild(scriptElement);
+        };
+
+    
+        const scripts = [  require.resolve('d3/dist/d3.js'),
+            require.resolve('../../../node_modules/@d3fc/d3fc-rebind/build/d3fc-rebind.js'),
+            require.resolve('../build/d3fc-technical-indicator.js')
+        ];
+
+        scripts.forEach(loadScript);
+
+        window.onload = () => {
+            try {
+                const indicator = window.fc.indicatorMovingAverage();
                 const data = [0, 1, 6, 4, 8];
-                const indicator = win.fc.indicatorMovingAverage();
                 const transformed = indicator(data);
                 expect(transformed).not.toBeUndefined();
                 done();
+            } catch (err) {
+                done(err);
             }
-        });
+        };
     });
 });
